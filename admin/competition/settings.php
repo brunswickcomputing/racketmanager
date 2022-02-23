a7e25a4786088fb5b6f18f11e64ec306fa94d3ee
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-02-23 14:13:21 +0000

    reformat

--- admin/competition/settings.php
+++ admin/competition/settings.php
@@ -38,48 +38,47 @@ if ( !current_user_can( 'manage_racketmanager' ) ) {
 			active: <?php echo $tab ?>
 		});
 	});
-</script>
-<div class="wrap">
+	</script>
+	<div class="wrap">
 
-	<form action="" method="post">
-		<?php wp_nonce_field( 'racketmanager_manage-competition-options' ) ?>
+		<form action="" method="post">
+			<?php wp_nonce_field( 'racketmanager_manage-competition-options' ) ?>
 
-		<div class="theme-settings-blocks form" id="tabs">
-			<input type="hidden" class="active-tab" name="active-tab" value="<?php echo $tab ?>" ?>
+			<div class="theme-settings-blocks form" id="tabs">
+				<input type="hidden" class="active-tab" name="active-tab" value="<?php echo $tab ?>" ?>
 
-			<ul id="tablist" style="display: none";>
-				<li><a href="#general"><?php _e( 'General', 'racketmanager' ) ?></a></li>
-				<li><a href="#standings"><?php _e( 'Standings Table', 'racketmanager' ) ?></a></li>
-				<li><a href="#advanced"><?php _e( 'Advanced', 'racketmanager' ) ?></a></li>
-			</ul>
+				<ul id="tablist" style="display: none";>
+					<li><a href="#general"><?php _e( 'General', 'racketmanager' ) ?></a></li>
+					<li><a href="#standings"><?php _e( 'Standings Table', 'racketmanager' ) ?></a></li>
+					<li><a href="#advanced"><?php _e( 'Advanced', 'racketmanager' ) ?></a></li>
+				</ul>
 
-			<div id='general' class='settings-block-container'>
-				<h2><?php _e( 'General', 'racketmanager' ) ?></h2>
-				<div class="settings-block">
-					<?php include('include/settings-general.php'); ?>
+				<div id='general' class='settings-block-container'>
+					<h2><?php _e( 'General', 'racketmanager' ) ?></h2>
+					<div class="settings-block">
+						<?php include('include/settings-general.php'); ?>
+					</div>
+				</div>
+
+				<div id='standings' class='settings-block-container'>
+					<h2><?php _e( 'Standings Table', 'racketmanager' ) ?></h2>
+					<div class="settings-block">
+						<?php include('include/settings-standings.php'); ?>
+					</div>
+				</div>
+
+				<div id='advanced' class="settings-block-container">
+					<h2><?php _e( 'Advanced', 'racketmanager' ) ?></h2>
+					<div class="settings-block">
+						<?php include('include/settings-advanced.php'); ?>
+					</div>
+				</div>
+
 				</div>
 			</div>
-
-			<div id='standings' class='settings-block-container'>
-				<h2><?php _e( 'Standings Table', 'racketmanager' ) ?></h2>
-				<div class="settings-block">
-					<?php include('include/settings-standings.php'); ?>
-				</div>
-			</div>
-
-			<div id='advanced' class="settings-block-container">
-				<h2><?php _e( 'Advanced', 'racketmanager' ) ?></h2>
-				<div class="settings-block">
-					<?php include('include/settings-advanced.php'); ?>
-				</div>
-			</div>
-
-				</div>
-			</div>
-		</div>
-		<input type="hidden" name="competition_id" value="<?php echo $competition->id ?>" />
-		<p class="submit"><input type="submit" name="updateSettings" value="<?php _e( 'Save Preferences', 'racketmanager' ) ?>" class="button button-primary" /></p>
-	</form>
-</div>
+			<input type="hidden" name="competition_id" value="<?php echo $competition->id ?>" />
+			<p class="submit"><input type="submit" name="updateSettings" value="<?php _e( 'Save Preferences', 'racketmanager' ) ?>" class="button button-primary" /></p>
+		</form>
+	</div>
 
 <?php } ?>

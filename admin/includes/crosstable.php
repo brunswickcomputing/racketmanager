ca7cece39f708183fd10e236855244633a4e305d
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-02-25 22:21:34 +0000

    reformat indents

--- admin/includes/crosstable.php
+++ admin/includes/crosstable.php
@@ -1,26 +1,26 @@
 
 <table class='widefat crosstable' summary='' title='<?php echo __( 'Crosstable', 'racketmanager' )." ".$league->title ?>'>
 	<thead>
-	<tr>
-		<th colspan='2' style='text-align: center;'><?php _e( 'Club', 'racketmanager' ) ?></th>
-<?php for ( $i = 1; $i <= count($teams); $i++ ) { ?>
-		<th style='text-align: center;'><?php echo $i ?></th>
-<?php } ?>
-	</tr>
+		<tr>
+			<th colspan='2' style='text-align: center;'><?php _e( 'Club', 'racketmanager' ) ?></th>
+			<?php for ( $i = 1; $i <= count($teams); $i++ ) { ?>
+				<th style='text-align: center;'><?php echo $i ?></th>
+			<?php } ?>
+		</tr>
 	</thead>
 	<tbody>
-<?php foreach ( $teams AS $rank => $team ) {
-    $team = get_leagueteam($team);
-	if ( $team->home == 1 ) $team->title = '<strong>'.$team->title.'</strong>'; ?>
-		<tr class='<?php echo $team->class ?>'>
-			<th scope='row' class='rank'><?php echo $rank + 1 ?></th>
+		<?php foreach ( $teams AS $rank => $team ) {
+			$team = get_leagueteam($team);
+			if ( $team->home == 1 ) $team->title = '<strong>'.$team->title.'</strong>'; ?>
+			<tr class='<?php echo $team->class ?>'>
+				<th scope='row' class='rank'><?php echo $rank + 1 ?></th>
 				<td>
 					<?php echo $team->title ?>
 				</td>
-                <?php for ( $i = 1; $i <= count($teams); $i++ ) { ?>
-                    <?php echo $league->getCrosstableField($team->id, $teams[$i-1]->id, $team->home); ?>
-                <?php } ?>
-		</tr>
-<?php } ?>
+				<?php for ( $i = 1; $i <= count($teams); $i++ ) { ?>
+					<?php echo $league->getCrosstableField($team->id, $teams[$i-1]->id, $team->home); ?>
+				<?php } ?>
+			</tr>
+		<?php } ?>
 	</tbody>
 </table>

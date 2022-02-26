fe740f352ca1ce3224cf30f7c2a35f7867aac793
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-02-26 01:03:54 +0000

    reformat indents

--- admin/includes/teams/playerteam.php
+++ admin/includes/teams/playerteam.php
@@ -1,77 +1,77 @@
 <?php
 /**
- * Player Team administration panel
- *
- */
- namespace ns;
+* Player Team administration panel
+*
+*/
+namespace ns;
 ?>
 
 <div class="wrap league-block">
-    <p class="racketmanager_breadcrumb"><a href="admin.php?page=racketmanager"><?php _e( 'RacketManager', 'racketmanager' ) ?></a><?php if ( !$noleague ) { ?> &raquo; <a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league->id ?>"><?php echo $league->title ?></a><?php } ?> &raquo; <?php echo $form_title ?></p>
-    <h1><?php printf( "%s - %s",  $league->title, $form_title ); ?></h1>
-    <form action="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league_id ?>&amp;season=<?php echo $season ?>" method="post" enctype="multipart/form-data" name="team_edit" id="teamPlayerFrm" >
+  <p class="racketmanager_breadcrumb"><a href="admin.php?page=racketmanager"><?php _e( 'RacketManager', 'racketmanager' ) ?></a><?php if ( !$noleague ) { ?> &raquo; <a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league->id ?>"><?php echo $league->title ?></a><?php } ?> &raquo; <?php echo $form_title ?></p>
+  <h1><?php printf( "%s - %s",  $league->title, $form_title ); ?></h1>
+  <form action="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league_id ?>&amp;season=<?php echo $season ?>" method="post" enctype="multipart/form-data" name="team_edit" id="teamPlayerFrm" >
 
-        <?php wp_nonce_field( 'racketmanager_manage-teams' ) ?>
+    <?php wp_nonce_field( 'racketmanager_manage-teams' ) ?>
 
-        <div class="form-group">
-            <label for="team"><?php _e( 'Team', 'racketmanager' ) ?></label>
-            <div class="input">
-                <input type="text" id="team" name="team" value="<?php echo $team->title ?>" size="50" disabled />
-            </div>
+    <div class="form-group">
+      <label for="team"><?php _e( 'Team', 'racketmanager' ) ?></label>
+      <div class="input">
+        <input type="text" id="team" name="team" value="<?php echo $team->title ?>" size="50" disabled />
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="teamPlayer1"><?php _e( 'Player 1', 'racketmanager' ) ?></label>
+      <div class="input">
+        <input type="text" name="teamPlayer1" id="teamPlayer1" value="<?php echo isset($team->player['1']) ? $team->player['1'] : '' ?>" size="50" /><input type="hidden" name="teamPlayerId1" id="teamPlayerId1" value="<?php echo isset($team->playerId['1']) ? $team->playerId['1'] : '' ?>" />
+      </div>
+    </div>
+    <?php if ( substr($league->type,1,1) == 'D'  ) { ?>
+      <div class="form-group">
+        <label for="teamPlayer2"><?php _e( 'Player 2', 'racketmanager' ) ?></label>
+        <div class="input">
+          <input type="text" name="teamPlayer2" id="teamPlayer2" value="<?php echo isset($team->player['2']) ? $team->player['2'] : '' ?>" size="50" /><input type="hidden" name="teamPlayerId2" id="teamPlayerId2" value="<?php echo isset($team->playerId['2']) ? $team->playerId['2'] : '' ?>" />
         </div>
-        <div class="form-group">
-            <label for="teamPlayer1"><?php _e( 'Player 1', 'racketmanager' ) ?></label>
-            <div class="input">
-                <input type="text" name="teamPlayer1" id="teamPlayer1" value="<?php echo isset($team->player['1']) ? $team->player['1'] : '' ?>" size="50" /><input type="hidden" name="teamPlayerId1" id="teamPlayerId1" value="<?php echo isset($team->playerId['1']) ? $team->playerId['1'] : '' ?>" />
-            </div>
-        </div>
-        <?php if ( substr($league->type,1,1) == 'D'  ) { ?>
-            <div class="form-group">
-                <label for="teamPlayer2"><?php _e( 'Player 2', 'racketmanager' ) ?></label>
-                <div class="input">
-                    <input type="text" name="teamPlayer2" id="teamPlayer2" value="<?php echo isset($team->player['2']) ? $team->player['2'] : '' ?>" size="50" /><input type="hidden" name="teamPlayerId2" id="teamPlayerId2" value="<?php echo isset($team->playerId['2']) ? $team->playerId['2'] : '' ?>" />
-                </div>
-            </div>
-        <?php } ?>
-        <div class="form-group">
-            <label for="affiliatedclub"><?php _e( 'Affiliated Club', 'racketmanager' ) ?></label>
-            <div class="input">
-                <select size="1" name="affiliatedclub" id="affiliatedclub" >
-                    <option value=""><?php _e( 'Select club' , 'racketmanager') ?></option>
-                    <?php foreach ( $clubs AS $club ) { ?>
-                    <option value="<?php echo $club->id ?>"<?php if(isset($team->affiliatedclub)) selected($club->id, $team->affiliatedclub ) ?>><?php echo $club->name ?></option>
-                    <?php } ?>
-                </select>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="captain"><?php _e( 'Captain', 'racketmanager' ) ?></label>
-            <div class="input">
-                <input type="text" name="captain" id="captain" autocomplete="name off" value="<?php echo $team->captain ?>" size="40" disabled /><input type="hidden" name="captainId" id="captainId" value="<?php echo $team->captainId ?>" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="contactno"><?php _e( 'Contact Number', 'racketmanager' ) ?></label>
-            <div class="input">
-                <input type="tel" name="contactno" id="contactno" autocomplete="tel" value="<?php echo $team->contactno ?>" size="20" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="contactemail"><?php _e( 'Contact Email', 'racketmanager' ) ?></label>
-            <div class="input">
-                <input type="email" name="contactemail" id="contactemail" autocomplete="email" value="<?php echo $team->contactemail ?>" size="60" />
-            </div>
-        </div>
+      </div>
+    <?php } ?>
+    <div class="form-group">
+      <label for="affiliatedclub"><?php _e( 'Affiliated Club', 'racketmanager' ) ?></label>
+      <div class="input">
+        <select size="1" name="affiliatedclub" id="affiliatedclub" >
+          <option value=""><?php _e( 'Select club' , 'racketmanager') ?></option>
+          <?php foreach ( $clubs AS $club ) { ?>
+            <option value="<?php echo $club->id ?>"<?php if(isset($team->affiliatedclub)) selected($club->id, $team->affiliatedclub ) ?>><?php echo $club->name ?></option>
+          <?php } ?>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="captain"><?php _e( 'Captain', 'racketmanager' ) ?></label>
+      <div class="input">
+        <input type="text" name="captain" id="captain" autocomplete="name off" value="<?php echo $team->captain ?>" size="40" disabled /><input type="hidden" name="captainId" id="captainId" value="<?php echo $team->captainId ?>" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="contactno"><?php _e( 'Contact Number', 'racketmanager' ) ?></label>
+      <div class="input">
+        <input type="tel" name="contactno" id="contactno" autocomplete="tel" value="<?php echo $team->contactno ?>" size="20" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="contactemail"><?php _e( 'Contact Email', 'racketmanager' ) ?></label>
+      <div class="input">
+        <input type="email" name="contactemail" id="contactemail" autocomplete="email" value="<?php echo $team->contactemail ?>" size="60" />
+      </div>
+    </div>
 
-        <?php do_action( 'team_edit_form', $team ) ?>
-        <?php do_action( 'team_edit_form_'.(isset($league->sport) ? ($league->sport) : '' ), $team ) ?>
+    <?php do_action( 'team_edit_form', $team ) ?>
+    <?php do_action( 'team_edit_form_'.(isset($league->sport) ? ($league->sport) : '' ), $team ) ?>
 
-        <input type="hidden" name="team_id" id="team_id" value="<?php echo $team->id ?>" />
-        <input type="hidden" name="league_id" value="<?php echo $league_id ?>" />
-        <input type="hidden" name="updateLeague" value="teamPlayer" />
-        <input type="hidden" name="season" value="<?php echo $season ?>" />
+    <input type="hidden" name="team_id" id="team_id" value="<?php echo $team->id ?>" />
+    <input type="hidden" name="league_id" value="<?php echo $league_id ?>" />
+    <input type="hidden" name="updateLeague" value="teamPlayer" />
+    <input type="hidden" name="season" value="<?php echo $season ?>" />
 
-        <p class="submit"><input type="submit" id="actionPlayerTeam" name="action" value="<?php echo $form_action ?>" class="button button-primary" /></p>
-    </form>
-    <div id="errorMsg" style="display:none;"></div>
+    <p class="submit"><input type="submit" id="actionPlayerTeam" name="action" value="<?php echo $form_action ?>" class="button button-primary" /></p>
+  </form>
+  <div id="errorMsg" style="display:none;"></div>
 </div>
--- admin/includes/teams/team.php
+++ admin/includes/teams/team.php
@@ -1,107 +1,107 @@
 <?php
 /**
- * Team administration panel
- *
- */
- namespace ns;
+* Team administration panel
+*
+*/
+namespace ns;
 ?>
 
 <div class="wrap league-block">
-    <p class="racketmanager_breadcrumb"><a href="admin.php?page=racketmanager"><?php _e( 'RacketManager', 'racketmanager' ) ?></a><?php if ( !$noleague ) { ?> &raquo; <a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league->id ?>"><?php echo $league->title ?></a><?php } ?> &raquo; <?php echo $form_title ?></p>
-    <?php if ( !$noleague ) { ?>
+  <p class="racketmanager_breadcrumb"><a href="admin.php?page=racketmanager"><?php _e( 'RacketManager', 'racketmanager' ) ?></a><?php if ( !$noleague ) { ?> &raquo; <a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league->id ?>"><?php echo $league->title ?></a><?php } ?> &raquo; <?php echo $form_title ?></p>
+  <?php if ( !$noleague ) { ?>
     <h1><?php printf( "%s - %s",  $league->title, $form_title ); ?></h1>
-    <?php } else { ?>
+  <?php } else { ?>
     <h1><?php printf(  $form_title ); ?></h1>
-    <?php }
-if ( !$noleague ) { ?>
+  <?php }
+  if ( !$noleague ) { ?>
     <form action="index.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league_id ?>&amp;season=<?php echo $season ?>" method="post" enctype="multipart/form-data" name="team_edit">
-<?php } else { ?>
-    <form action="admin.php?page=racketmanager&amp;view=teams<?php if ( $clubId !== '' ) { ?>&amp;club_id=<?php echo $clubId ?> <?php } ?>" method="post" enctype="multipart/form-data" name="team_edit">
-<?php } ?>
-        <?php wp_nonce_field( 'racketmanager_manage-teams' ) ?>
+    <?php } else { ?>
+      <form action="admin.php?page=racketmanager&amp;view=teams<?php if ( $clubId !== '' ) { ?>&amp;club_id=<?php echo $clubId ?> <?php } ?>" method="post" enctype="multipart/form-data" name="team_edit">
+      <?php } ?>
+      <?php wp_nonce_field( 'racketmanager_manage-teams' ) ?>
 
+      <div class="form-group">
+        <label for="team"><?php _e( 'Team', 'racketmanager' ) ?></label>
+        <div class="input">
+          <input type="text" id="team" name="team" readonly value="<?php echo $team->title ?>" size="30" placeholder="<?php _e( 'Add Team', 'racketmanager' ) ?>""/>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="affiliatedclub"><?php _e( 'Affiliated Club', 'racketmanager' ) ?></label>
+        <div class="input">
+          <select size="1" name="affiliatedclub" id="affiliatedclub" >
+            <option><?php _e( 'Select club' , 'racketmanager') ?></option>
+            <?php foreach ( $clubs AS $club ) { ?>
+              <option value="<?php echo $club->id ?>"<?php if ( isset($team->affiliatedclub) ) selected($team->affiliatedclub, $club->id) ?>><?php echo $club->name ?></option>
+            <?php } ?>
+          </select>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="team_type"><?php _e( 'Type', 'racketmanager' ) ?></label>
+        <div class="input">
+          <select size='1' required="required" name='team_type' id='team_type'>
+            <option><?php _e( 'Select', 'racketmanager') ?></option>
+            <option value='WS' <?php if ( isset($team->type) ) selected($team->type, 'WS') ?>><?php _e( 'Ladies Singles', 'racketmanager') ?></option>
+            <option value='WD' <?php if ( isset($team->type) ) selected($team->type, 'WD') ?>><?php _e( 'Ladies Doubles', 'racketmanager') ?></option>
+            <option value='MD' <?php if ( isset($team->type) ) selected($team->type, 'MD') ?>><?php _e( 'Mens Doubles', 'racketmanager') ?></option>
+            <option value='MS' <?php if ( isset($team->type) ) selected($team->type, 'MS') ?>><?php _e( 'Mens Singles', 'racketmanager') ?></option>
+            <option value='XD' <?php if ( isset($team->type) ) selected($team->type, 'XD') ?>><?php _e( 'Mixed Doubles', 'racketmanager') ?></option>
+          </select>
+        </div>
+      </div>
+
+      <?php if ( !$noleague ) { ?>
         <div class="form-group">
-            <label for="team"><?php _e( 'Team', 'racketmanager' ) ?></label>
-            <div class="input">
-                <input type="text" id="team" name="team" readonly value="<?php echo $team->title ?>" size="30" placeholder="<?php _e( 'Add Team', 'racketmanager' ) ?>""/>
-            </div>
+          <label for="captain"><?php _e( 'Captain', 'racketmanager' ) ?></label>
+          <div class="input">
+            <input type="text" name="captain" id="captain" autocomplete="name off" value="<?php echo $team->captain ?>" size="30" /><input type="hidden" name="captainId" id="captainId" value="<?php echo $team->captainId ?>" />
+          </div>
         </div>
         <div class="form-group">
-            <label for="affiliatedclub"><?php _e( 'Affiliated Club', 'racketmanager' ) ?></label>
-            <div class="input">
-                <select size="1" name="affiliatedclub" id="affiliatedclub" >
-                    <option><?php _e( 'Select club' , 'racketmanager') ?></option>
-                    <?php foreach ( $clubs AS $club ) { ?>
-                    <option value="<?php echo $club->id ?>"<?php if ( isset($team->affiliatedclub) ) selected($team->affiliatedclub, $club->id) ?>><?php echo $club->name ?></option>
-                    <?php } ?>
-                </select>
-            </div>
+          <label for="contactno"><?php _e( 'Contact Number', 'racketmanager' ) ?></label>
+          <div class="input">
+            <input type="tel" name="contactno" id="contactno" autocomplete="tel" value="<?php echo $team->contactno ?>" size="20" />
+          </div>
         </div>
         <div class="form-group">
-            <label for="team_type"><?php _e( 'Type', 'racketmanager' ) ?></label>
-            <div class="input">
-                <select size='1' required="required" name='team_type' id='team_type'>
-                    <option><?php _e( 'Select', 'racketmanager') ?></option>
-                    <option value='WS' <?php if ( isset($team->type) ) selected($team->type, 'WS') ?>><?php _e( 'Ladies Singles', 'racketmanager') ?></option>
-                    <option value='WD' <?php if ( isset($team->type) ) selected($team->type, 'WD') ?>><?php _e( 'Ladies Doubles', 'racketmanager') ?></option>
-                    <option value='MD' <?php if ( isset($team->type) ) selected($team->type, 'MD') ?>><?php _e( 'Mens Doubles', 'racketmanager') ?></option>
-                    <option value='MS' <?php if ( isset($team->type) ) selected($team->type, 'MS') ?>><?php _e( 'Mens Singles', 'racketmanager') ?></option>
-                    <option value='XD' <?php if ( isset($team->type) ) selected($team->type, 'XD') ?>><?php _e( 'Mixed Doubles', 'racketmanager') ?></option>
-                </select>
-            </div>
+          <label for="contactemail"><?php _e( 'Contact Email', 'racketmanager' ) ?></label>
+          <div class="input">
+            <input type="email" name="contactemail" id="contactemail" autocomplete="email" value="<?php echo $team->contactemail ?>" size="30" /></td>
+          </div>
         </div>
+        <div class="form-group">
+          <label for="matchtime"><?php _e( 'Match Time', 'racketmanager' ) ?></label>
+          <div class="input">
+            <input type="time" name="matchtime" id="matchtime" value="<?php echo $team->match_time ?>" size="5" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="matchday"><?php _e( 'Match Day', 'racketmanager' ) ?></label>
+          <div class="input">
+            <select size="1" name="matchday" id="matchday" >
+              <option><?php _e( 'Select match day' , 'racketmanager') ?></option>
+              <?php foreach ( $matchdays AS $matchday ) { ?>
+                <option value="<?php echo $matchday ?>"<?php if(isset($team->match_day)) selected($matchday, $team->match_day ) ?>><?php echo $matchday ?></option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
+      <?php } ?>
+      <?php do_action( 'team_edit_form', $team ) ?>
+      <?php do_action( 'team_edit_form_'.(isset($league->sport) ? ($league->sport) : '' ), $team ) ?>
 
-        <?php if ( !$noleague ) { ?>
-            <div class="form-group">
-                <label for="captain"><?php _e( 'Captain', 'racketmanager' ) ?></label>
-                <div class="input">
-                    <input type="text" name="captain" id="captain" autocomplete="name off" value="<?php echo $team->captain ?>" size="30" /><input type="hidden" name="captainId" id="captainId" value="<?php echo $team->captainId ?>" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="contactno"><?php _e( 'Contact Number', 'racketmanager' ) ?></label>
-                    <div class="input">
-                    <input type="tel" name="contactno" id="contactno" autocomplete="tel" value="<?php echo $team->contactno ?>" size="20" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="contactemail"><?php _e( 'Contact Email', 'racketmanager' ) ?></label>
-                <div class="input">
-                    <input type="email" name="contactemail" id="contactemail" autocomplete="email" value="<?php echo $team->contactemail ?>" size="30" /></td>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="matchtime"><?php _e( 'Match Time', 'racketmanager' ) ?></label>
-                <div class="input">
-                    <input type="time" name="matchtime" id="matchtime" value="<?php echo $team->match_time ?>" size="5" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="matchday"><?php _e( 'Match Day', 'racketmanager' ) ?></label>
-                <div class="input">
-                    <select size="1" name="matchday" id="matchday" >
-                        <option><?php _e( 'Select match day' , 'racketmanager') ?></option>
-                        <?php foreach ( $matchdays AS $matchday ) { ?>
-                        <option value="<?php echo $matchday ?>"<?php if(isset($team->match_day)) selected($matchday, $team->match_day ) ?>><?php echo $matchday ?></option>
-                        <?php } ?>
-                    </select>
-                </div>
-            </div>
-        <?php } ?>
-        <?php do_action( 'team_edit_form', $team ) ?>
-        <?php do_action( 'team_edit_form_'.(isset($league->sport) ? ($league->sport) : '' ), $team ) ?>
-
-        <input type="hidden" name="team_id" id="team_id" value="<?php echo $team->id ?>" />
-        <input type="hidden" name="league_id" value="<?php echo $league_id ?>" />
-        <input type="hidden" name="updateLeague" value="team" />
-        <input type="hidden" name="season" value="<?php echo $season ?>" />
-        <?php if ( isset($clubId) ) { ?>
+      <input type="hidden" name="team_id" id="team_id" value="<?php echo $team->id ?>" />
+      <input type="hidden" name="league_id" value="<?php echo $league_id ?>" />
+      <input type="hidden" name="updateLeague" value="team" />
+      <input type="hidden" name="season" value="<?php echo $season ?>" />
+      <?php if ( isset($clubId) ) { ?>
         <input type="hidden" name="clubId" value="<?php echo $clubId ?>" />
-        <?php } ?>
-        <?php if ( $edit ) { ?>
+      <?php } ?>
+      <?php if ( $edit ) { ?>
         <input type="hidden" name="editTeam" value="team" />
-        <?php } ?>
+      <?php } ?>
 
-        <p class="submit"><input type="submit" name="action" value="<?php echo $form_action ?>" class="button button-primary" /></p>
+      <p class="submit"><input type="submit" name="action" value="<?php echo $form_action ?>" class="button button-primary" /></p>
     </form>
-</div>
+  </div>

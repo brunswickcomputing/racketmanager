561974dcce8fb72c79768d5f2e71aba3ac9f89b8
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-02 23:49:29 +0000

    make default page just list of competitions by type

--- admin/index.php
+++ admin/index.php
@@ -1,26 +1,12 @@
 <?php
 
 ?>
-<script type='text/javascript'>
-jQuery(function() {
-	jQuery("#tabs").tabs({
-		active: <?php echo $tab ?>
-	});
-});
-</script>
-<div class="wrap"  style="margin-bottom: 1em;">
+<div class="container">
 
-	<h1><?php _e( 'Racketmanager', 'racketmanager' ) ?></h1>
+	<h1><?php _e( 'Racketmanager Competitions', 'racketmanager' ) ?></h1>
 
-	<div id="tabs" class="racketmanager-blocks">
-		<ul id="tablist" style="display: none;">
-			<li><a href="#competitions-table"><?php _e( 'Competitions', 'racketmanager' ) ?></a></li>
-		</ul>
-
-		<div id="competitions-table" class="league-block-container">
-			<h2 class="header"><?php _e( 'Competitions', 'racketmanager' ) ?></h2>
-			<?php include('main/competitions.php'); ?>
-		</div>
-		<?php include(RACKETMANAGER_PATH . '/admin/includes/match-modal.php'); ?>
+	<div id="competitions-table" class="league-block-container">
+		<?php include('main/competitions.php'); ?>
 	</div>
+	<?php include(RACKETMANAGER_PATH . '/admin/includes/match-modal.php'); ?>
 </div>

28ee06fe5348ca158e4c8feb51c2db395d563fab
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-07 17:50:03 +0000

    use correct class on form

--- admin/championship/preliminary.php
+++ admin/championship/preliminary.php
@@ -1,5 +1,5 @@
 <div class="championship-block">
-  <form action="" method="post" style="display: inline;">
+  <form action="" method="post" class="form-control">
     <input type="hidden" name="league-tab" value="preliminary" />
     <p><?php _e( 'After adding the teams and arranging the rankings then ', 'racketmanager' ) ?></p><input type="submit" class="btn btn-primary" value="<?php _e( 'Proceed to Final Rounds', 'racketmanager' ) ?>" name="startFinals" />
     <p><?php _e( 'Afterwards changes to rankings will NOT affect the final results', 'racketmanager' ) ?></p>

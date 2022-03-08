7c3fd4e09b125648ac28e943beb28c17dc3a3c96
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-08 22:28:07 +0000

    use bootstrap tabs and replace table with bootstrap styling

--- admin/includes/competitions-list.php
+++ admin/includes/competitions-list.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<div class="wrap league-block">
+<div class="container league-block">
   <p class="racketmanager_breadcrumb"><a href="admin.php?page=racketmanager"><?php _e( 'RacketManager', 'racketmanager' ) ?></a> &raquo; <?php echo $season->name ?> &raquo; <?php echo 'Add Competitions to Season' ?></p>
   <h1><?php printf( "%s - %s",  $season->name, 'Add Competitions to Season' ); ?></h1>
   <legend>Select Competitions to Add</legend>
@@ -10,8 +10,8 @@
     });
   });
   </script>
-  <div id="tabs-competitions" class="racketmanager-blocks">
-    <form action="admin.php?page=racketmanager" method="post" enctype="multipart/form-data" name="competitions_add">
+  <div class="container">
+    <form action="admin.php?page=racketmanager-admin" method="post" enctype="multipart/form-data" name="competitions_add">
       <?php wp_nonce_field( 'racketmanager_add-seasons-competitions-bulk' ) ?>
       <input type="hidden" name="season_id" value="<?php echo $season->id ?>" />
       <input type="hidden" name="season" value="<?php echo $season->name ?>" />
@@ -19,52 +19,59 @@
         <label for="num_match_days"><?php _e( 'Number of Match Days', 'racketmanager' ) ?></label>
         <input type="number" min="1" step="1" required="required" class="small-text" name="num_match_days" id="num_match_days" size="2" />
       </div>
-      <ul id="tablist">
-        <li><a href="#competitions-cup"><?php _e( 'Cups', 'racketmanager' ) ?></a></li>
-        <li><a href="#competitions-league"><?php _e( 'Leagues', 'racketmanager' ) ?></a></li>
-        <li><a href="#competitions-tournament"><?php _e( 'Tournaments', 'racketmanager' ) ?></a></li>
-      </ul>
+      <div class="container">
+        <!-- Nav tabs -->
+        <ul class="nav nav-pills" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="competitionscup-tab" data-bs-toggle="pill" data-bs-target="#competitionscup" type="button" role="tab" aria-controls="competitionscup" aria-selected="true"><?php _e( 'Cups', 'racketmanager' ) ?></button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="competitionsleague-tab" data-bs-toggle="pill" data-bs-target="#competitionsleague" type="button" role="tab" aria-controls="competitionsleague" aria-selected="false"><?php _e( 'Leagues', 'racketmanager' ) ?></button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="competitionstournament-tab" data-bs-toggle="pill" data-bs-target="#competitionstournament" type="button" role="tab" aria-controls="competitionstournament" aria-selected="false"><?php _e( 'Tournaments', 'racketmanager' ) ?></button>
+          </li>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <div class="tablenav">
+            <!-- Bulk Actions -->
+            <select name="action" size="1">
+              <option value="addCompetitionsToSeason"><?php _e('Add')?></option>
+            </select>
+            <input type="submit" value="<?php _e('Apply'); ?>" name="doaddCompetitionsToSeason" id="doaddCompetitionsToSeason" class="btn btn-primary action" />
+          </div>
 
-      <div class="tablenav">
-        <!-- Bulk Actions -->
-        <select name="action" size="1">
-          <option value="addCompetitionsToSeason"><?php _e('Add')?></option>
-        </select>
-        <input type="submit" value="<?php _e('Apply'); ?>" name="doaddCompetitionsToSeason" id="doaddCompetitionsToSeason" class="btn btn-primary action" />
-      </div>
+          <div class="row table-header">
+            <div class="col-1 check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('competitions-filter'));" /></div>
+            <div class="col-2 column-num">ID</div>
+            <div class="col-4"><?php _e( 'Name', 'racketmanager' ) ?></div>
+          </div>
 
-      <table class="widefat" summary="" title="RacketManager Competitions">
-        <thead>
-          <tr>
-            <th scope="col" class="check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('competitions-filter'));" /></th>
-            <th scope="col" class="column-num">ID</th>
-            <th scope="col"><?php _e( 'Name', 'racketmanager' ) ?></th>
-          </tr>
-
-          <?php $prevCompType = '';
-          if ( $competitions = $racketmanager->getCompetitions(array('orderby' => array("competitiontype" => "ASC", "name" => "ASC"))) ) {
-            $class = '';
-            foreach ( $competitions AS $competition ) {
-              $class = ( 'alternate' == $class ) ? '' : 'alternate';
-              if ( $competition->competitiontype != $prevCompType ) {
-                if ( $prevCompType != '' ) { ?>
-                </tbody>
-              <?php }
-              $prevCompType = $competition->competitiontype; ?>
-              <tbody id="competitions-<?php echo $prevCompType ?>" style="display: contents">
+          <?php $competitionTypes = $this->getCompetitionTypes();
+          $i = 0;
+          foreach ( $competitionTypes AS $competitionType ) { $i ++; ?>
+            <div id="competitions<?php echo $competitionType ?>" class="tab-pane table-pane <?php if ( $i == 1 )  { echo 'active show';} ?> fade" role="tabpanel" aria-labelledby="competitions<?php echo $competitionType ?>-tab">
+              <?php $competitionQuery = array( 'type' => $competitionType );
+              $competitions = $racketmanager->getCompetitions( $competitionQuery );
+              $class = ''; ?>
+              <?php foreach ( $competitions AS $competition ) {
+                $competition = get_competition($competition);
+                $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
+                <div class="row table-row <?php echo $class ?>">
+                  <div class="col-1 check-column">
+                    <?php if ( !is_numeric(array_search($season->name,array_column($competition->seasons, 'name') ,true)) ) { ?>
+                      <input type="checkbox" value="<?php echo $competition->id ?>" name="competition[<?php echo $competition->id ?>]" />
+                    <?php } ?>
+                  </div>
+                  <div class="col-2 column-num"><?php echo $competition->id ?></div>
+                  <div class="col-4"><?php echo $competition->name ?></div>
+                </div>
               <?php } ?>
-              <tr class="<?php echo $class ?>">
-                <th scope="row" class="check-column">
-                  <?php if ( !array_search($season->name,array_column($competition->seasons, 'name') ,true) ) { ?>
-                    <input type="checkbox" value="<?php echo $competition->id ?>" name="competition[<?php echo $competition->id ?>]" />
-                  <?php } ?>
-                </th>
-                <td class="column-num"><?php echo $competition->id ?></td>
-                <td><?php echo $competition->name ?></td>
-              </tr>
-            <?php } ?>
+            </div>
           <?php } ?>
-        </tbody>
-      </table>
+        </div>
+      </div>
     </form>
   </div>
+</div>

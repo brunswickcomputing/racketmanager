1ed3f45af3682f9baf45a2384ea0d3b140e03a74
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-09 15:42:50 +0000

    replace table with bootstrap styling

--- admin/includes/competitions.php
+++ admin/includes/competitions.php
@@ -14,54 +14,51 @@
       </div>
     <?php } ?>
 
-    <table class="widefat" summary="" title="RacketManager Competitions">
-      <thead>
-        <tr>
-          <?php if ( !$type ) { ?>
-            <th scope="col" class="check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('competitions-filter'));" /></th>
-          <?php } ?>
-          <th scope="col" class="column-num">ID</th>
-          <th scope="col"><?php _e( 'Competition', 'racketmanager' ) ?></th>
-          <?php if ( !$type ) { ?>
-            <th scope="col" class="column-num"><?php _e( 'Number of Seasons', 'racketmanager' ) ?></th>
-          <?php } ?>
-          <th scope="col" class="column-num"><?php _e( 'Leagues', 'racketmanager' ) ?></th>
-          <th scope="col" class="column-num"><?php _e( 'Number of Sets', 'racketmanager' ) ?></th>
-          <th scope="col" class="column-num"><?php _e( 'Number of Rubbers', 'racketmanager' ) ?></th>
-          <th scope="col" class="centered"><?php _e( 'Type', 'racketmanager' ) ?></th>
-        </tr>
-      </thead>
-      <tbody id="the-list">
-        <?php $competitions = $racketmanager->getCompetitions( $competitionQuery );
-        $class = '';
-        foreach ( $competitions AS $competition ) {
-          $competition = get_competition($competition);
-          $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
-          <tr class="<?php echo $class ?>">
-            <?php if ( !$type ) { ?>
-              <th scope="row" class="check-column"><input type="checkbox" value="<?php echo $competition->id ?>" name="competition[<?php echo $competition->id ?>]" /></th>
-            <?php } ?>
-            <td class="column-num"><?php echo $competition->id ?></td>
-            <td><a href="admin.php?page=racketmanager&amp;subpage=show-competition&amp;competition_id=<?php echo $competition->id ?>"><?php echo $competition->name ?></a></td>
-            <?php if ( !$type ) { ?>
-              <td class="column-num"><?php echo $competition->num_seasons ?></td>
-            <?php } ?>
-            <td class="column-num"><?php echo $competition->num_leagues ?></td>
-            <td class="column-num"><?php echo $competition->num_sets ?></td>
-            <td class="column-num"><?php echo $competition->num_rubbers ?></td>
-            <td>
-              <?php switch ($competition->type) {
-                case 'WS': _e( 'Ladies Singles', 'racketmanager' ); break;
-                case 'WD': _e( 'Ladies Doubles', 'racketmanager' ); break;
-                case 'MS': _e( 'Mens Singles', 'racketmanager' ); break;
-                case 'MD': _e( 'Mens Doubles', 'racketmanager' ); break;
-                case 'XD': _e( 'Mixed Doubles', 'racketmanager' ); break;
-                case 'LD': _e( 'The League', 'racketmanager' ); break;
-              } ?>
-            </td>
-          </tr>
+    <div class="container">
+      <div class="row table-header">
+
+        <?php if ( !$type ) { ?>
+          <div class="col-12 col-md-1 check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('competitions-filter'));" /></div>
         <?php } ?>
-      </tbody>
-    </table>
+        <div class="col-12 col-md-1 column-num">ID</div>
+        <div class="col-12 col-md-3"><?php _e( 'Competition', 'racketmanager' ) ?></div>
+        <?php if ( !$type ) { ?>
+          <div class="col-12 col-md-1 column-num"><?php _e( 'Number of Seasons', 'racketmanager' ) ?></div>
+        <?php } ?>
+        <div class="col-12 col-md-1 column-num"><?php _e( 'Leagues', 'racketmanager' ) ?></div>
+        <div class="col-12 col-md-1 column-num"><?php _e( 'Number of Sets', 'racketmanager' ) ?></div>
+        <div class="col-12 col-md-1 column-num"><?php _e( 'Number of Rubbers', 'racketmanager' ) ?></div>
+        <div class="col-12 col-md-1 centered"><?php _e( 'Type', 'racketmanager' ) ?></div>
+      </div>
+      <?php $competitions = $racketmanager->getCompetitions( $competitionQuery );
+      $class = '';
+      foreach ( $competitions AS $competition ) {
+        $competition = get_competition($competition);
+        $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
+        <div class="row table-row <?php echo $class ?>">
+          <?php if ( !$type ) { ?>
+            <div class="col-12 col-md-1 check-column"><input type="checkbox" value="<?php echo $competition->id ?>" name="competition[<?php echo $competition->id ?>]" /></div>
+          <?php } ?>
+          <div class="col-12 col-md-1 column-num"><?php echo $competition->id ?></div>
+          <div class="col-12 col-md-3 column-num"><a href="admin.php?page=racketmanager&amp;subpage=show-competition&amp;competition_id=<?php echo $competition->id ?>"><?php echo $competition->name ?></a></div>
+          <?php if ( !$type ) { ?>
+            <div class="col-12 col-md-1 column-num"><?php echo $competition->num_seasons ?></div>
+          <?php } ?>
+          <div class="col-12 col-md-1 column-num"><?php echo $competition->num_leagues ?></div>
+          <div class="col-12 col-md-1 column-num"><?php echo $competition->num_sets ?></div>
+          <div class="col-12 col-md-1 column-num"><?php echo $competition->num_rubbers ?></div>
+          <div class="col-12 col-md-2 centered">
+            <?php switch ($competition->type) {
+              case 'WS': _e( 'Ladies Singles', 'racketmanager' ); break;
+              case 'WD': _e( 'Ladies Doubles', 'racketmanager' ); break;
+              case 'MS': _e( 'Mens Singles', 'racketmanager' ); break;
+              case 'MD': _e( 'Mens Doubles', 'racketmanager' ); break;
+              case 'XD': _e( 'Mixed Doubles', 'racketmanager' ); break;
+              case 'LD': _e( 'The League', 'racketmanager' ); break;
+            } ?>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
   </form>
 </div>

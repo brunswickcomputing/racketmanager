7db516c0362108bc86f38545cca28f5a640c2b09
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-09 17:56:23 +0000

    replace table with bootstrap styling

--- admin/includes/crosstable.php
+++ admin/includes/crosstable.php
@@ -1,26 +1,35 @@
 
-<table class='widefat crosstable' summary='' title='<?php echo __( 'Crosstable', 'racketmanager' )." ".$league->title ?>'>
-	<thead>
-		<tr>
-			<th colspan='2' style='text-align: center;'><?php _e( 'Club', 'racketmanager' ) ?></th>
-			<?php for ( $i = 1; $i <= count($teams); $i++ ) { ?>
-				<th style='text-align: center;'><?php echo $i ?></th>
-			<?php } ?>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach ( $teams AS $rank => $team ) {
-			$team = get_leagueteam($team);
-			if ( $team->home == 1 ) $team->title = '<strong>'.$team->title.'</strong>'; ?>
-			<tr class='<?php echo $team->class ?>'>
-				<th scope='row' class='rank'><?php echo $rank + 1 ?></th>
-				<td>
-					<?php echo $team->title ?>
-				</td>
+<div class="container">
+	<div class="row table-header">
+		<div class="col-auto"> </div>
+		<div class="col-2"><?php _e( 'Club', 'racketmanager' ) ?></div>
+		<div class="col-9 container">
+			<div class="row align-items-center">
 				<?php for ( $i = 1; $i <= count($teams); $i++ ) { ?>
-					<?php echo $league->getCrosstableField($team->id, $teams[$i-1]->id, $team->home); ?>
+					<div class="col-2 fixture"><?php echo $i ?></div>
 				<?php } ?>
-			</tr>
-		<?php } ?>
-	</tbody>
-</table>
+			</div>
+		</div>
+	</div>
+	<?php foreach ( $teams AS $rank => $team ) {
+		$team = get_leagueteam($team);
+		if ( $team->home == 1 ) {
+			$team->title = '<strong>'.$team->title.'</strong>';
+		} ?>
+		<div class="row table-row <?php echo $team->class ?>">
+			<div class="col-auto rank">
+				<?php echo $rank + 1 ?>
+			</div>
+			<div class="col-2">
+				<?php echo $team->title ?>
+			</div>
+			<div class="col-9 container">
+				<div class="row align-items-center">
+					<?php for ( $i = 1; $i <= count($teams); $i++ ) { ?>
+						<div class="col-2 fixture"><?php echo $league->getCrosstableField($team->id, $teams[$i-1]->id, $team->home); ?></div>
+					<?php } ?>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
+</div>

9fe0955911c538cc68483eff1278f30fdc3d7df5
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-10 16:49:59 +0000

    replace table with bootstrap styling

--- admin/competition/leagues.php
+++ admin/competition/leagues.php
@@ -1,62 +1,70 @@
-<form id="leagues-filter" method="post" action="">
-	<?php wp_nonce_field( 'leagues-bulk' ) ?>
+<div class="container">
+	<div class="container">
+		<form id="leagues-filter" method="post" action="">
+			<?php wp_nonce_field( 'leagues-bulk' ) ?>
 
-	<input type="hidden" name="competition_id" value="<?php echo $competition_id ?>" />
-	<div class="tablenav">
-		<!-- Bulk Actions -->
-		<select name="action" size="1">
-			<option value="-1" selected="selected"><?php _e('Bulk Actions') ?></option>
-			<option value="delete"><?php _e('Delete')?></option>
-		</select>
-		<input type="submit" value="<?php _e('Apply'); ?>" name="doactionleague" id="doactionleague" class="btn btn-secondary action" />
-	</div>
+			<input type="hidden" name="competition_id" value="<?php echo $competition_id ?>" />
+			<div class="tablenav">
+				<!-- Bulk Actions -->
+				<select name="action" size="1">
+					<option value="-1" selected="selected"><?php _e('Bulk Actions') ?></option>
+					<option value="delete"><?php _e('Delete')?></option>
+				</select>
+				<input type="submit" value="<?php _e('Apply'); ?>" name="doactionleague" id="doactionleague" class="btn btn-secondary action" />
+			</div>
+			
+			<div class="row table-header">
+				<div class="col-1 check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('leagues-filter'));" /></div>
+				<div class="col-1 column-num">ID</div>
+				<div class="col-3"><?php _e( 'League', 'racketmanager' ) ?></div>
+				<div class="col-1 column-num"><?php _e( 'Teams', 'racketmanager' ) ?></div>
+				<div class="col-1 column-num"><?php _e( 'Matches', 'racketmanager' ) ?></div>
+			</div>
 
-	<table class="widefat" summary="" title="RacketManager">
-		<thead>
-			<tr>
-				<th scope="col" class="check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('leagues-filter'));" /></th>
-				<th scope="col" class="column-num">ID</th>
-				<th scope="col"><?php _e( 'League', 'racketmanager' ) ?></th>
-				<th scope="col" class="column-num"><?php _e( 'Teams', 'racketmanager' ) ?></th>
-				<th scope="col" class="column-num"><?php _e( 'Matches', 'racketmanager' ) ?></th>
-			</tr>
-			<tbody id="the-list">
-				<?php
-				if ( $leagues = $competition->getLeagues( array('competition' => $competition_id)) ) {
-					$class = '';
-					foreach ( $leagues AS $league ) {
-						$league = get_league($league);
-						$class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
-						<tr class="<?php echo $class ?>">
-							<th scope="row" class="check-column"><input type="checkbox" value="<?php echo $league->id ?>" name="league[<?php echo $league->id ?>]" /></th>
-							<td class="column-num"><?php echo $league->id ?></td>
-							<td><a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league->id ?>"><?php echo $league->title ?></a></td>
-							<td class="column-num"><?php echo $league->num_teams_total ?></td>
-							<td class="column-num"><?php echo $league->num_matches_total ?></td>
-						</tr>
-					<?php } ?>
+			<?php
+			if ( $leagues = $competition->getLeagues( array('competition' => $competition_id)) ) {
+				$class = '';
+				foreach ( $leagues AS $league ) {
+					$league = get_league($league);
+					$class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
+					<div class="row table-row <?php echo $class ?>">
+						<div class="col-1 check-column"><input type="checkbox" value="<?php echo $league->id ?>" name="league[<?php echo $league->id ?>]" /></div>
+						<div class="col-1 column-num"><?php echo $league->id ?></div>
+						<div class="col-3"><a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $league->id ?>"><?php echo $league->title ?></a></div>
+						<div class="col-1 column-num"><?php echo $league->num_teams_total ?></div>
+						<div class="col-1 column-num"><?php echo $league->num_matches_total ?></div>
+						<div class="col-1"><a href="admin.php?page=racketmanager&amp;subpage=show-competition&amp;competition_id=<?php echo $competition->id ?>&amp;editleague=<?php echo $league->id ?>"><?php _e( 'Edit', 'racketmanager' ) ?></a></div>
+					</div>
 				<?php } ?>
-			</tbody>
-		</table>
-	</form>
-
+			<?php } ?>
+		</form>
+	</div>
 	<!-- Add New League -->
-	<h3><?php _e( 'Add League', 'racketmanager' ) ?></h3>
-	<form action="" method="post" class="form-control">
-		<?php wp_nonce_field( 'racketmanager_add-league' ) ?>
-		<input type="hidden" name="competition_id" value="<?php echo $competition_id ?>" />
-		<input type="hidden" name="league_id" value="<?php echo $league_id ?>" />
-		<div class="form-group">
-			<div class="form-label">
-				<label for="league_title"><?php _e( 'League', 'racketmanager' ) ?></label>
+	<?php if ( !$league_id ) {
+		$action = __( 'Add League', 'racketmanager' );
+	} else {
+		$action = __( 'Update League', 'racketmanager' );
+	} ?>
+	<div class="container">
+
+		<h3><?php echo $action ?></h3>
+		<form action="" method="post" class="form-control">
+			<?php wp_nonce_field( 'racketmanager_add-league' ) ?>
+			<input type="hidden" name="competition_id" value="<?php echo $competition_id ?>" />
+			<input type="hidden" name="league_id" value="<?php echo $league_id ?>" />
+			<div class="form-group">
+				<div class="form-label">
+					<label for="league_title"><?php _e( 'League', 'racketmanager' ) ?></label>
+				</div>
+				<div class="form-input">
+					<input type="text" required="required" placeholder="<?php _e( 'Enter new league name', 'racketmanager') ?>"name="league_title" id="league_title" value="<?php echo $league_title ?>" size="30" />
+				</div>
 			</div>
-			<div class="form-input">
-				<input type="text" required="required" placeholder="<?php _e( 'Enter new league name', 'racketmanager') ?>"name="league_title" id="league_title" value="<?php echo $league_title ?>" size="30" />
+			<div class="form-group">
+				<div class="form-input">
+					<input type="submit" name="addLeague" value="<?php echo $action ?>" class="btn btn-primary" />
+				</div>
 			</div>
-		</div>
-		<div class="form-group">
-			<div class="form-input">
-				<input type="submit" name="addLeague" value="<?php if ( !$league_id ) _e( 'Add League', 'racketmanager' ); else _e( 'Update League', 'racketmanager' ); ?>" class="btn btn-primary" />
-			</div>
-		</div>
-	</form>
+		</form>
+	</div>
+</div>

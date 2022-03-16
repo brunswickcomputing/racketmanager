55c36bc19d509a9154d6cb60380b9a50e87d83e5
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-16 00:04:22 +0000

    reformat indents

--- admin/admin/seasons.php
+++ admin/admin/seasons.php
@@ -1,56 +1,52 @@
-<div class="container">
-	<!-- View Seasons -->
+<!-- View Seasons -->
+<form id="seasons-filter" method="post" action="">
+	<?php wp_nonce_field( 'seasons-bulk' ) ?>
+
+	<div class="tablenav">
+		<!-- Bulk Actions -->
+		<select name="action" size="1">
+			<option value="-1" selected="selected"><?php _e('Bulk Actions') ?></option>
+			<option value="delete"><?php _e('Delete')?></option>
+		</select>
+		<input type="submit" value="<?php _e('Apply'); ?>" name="doSeasonDel" id="doSeasonDel" class="btn btn-secondary action" />
+	</div>
+
 	<div class="container">
-		<form id="seasons-filter" method="post" action="">
-			<?php wp_nonce_field( 'seasons-bulk' ) ?>
-
-			<div class="tablenav">
-				<!-- Bulk Actions -->
-				<select name="action" size="1">
-					<option value="-1" selected="selected"><?php _e('Bulk Actions') ?></option>
-					<option value="delete"><?php _e('Delete')?></option>
-				</select>
-				<input type="submit" value="<?php _e('Apply'); ?>" name="doSeasonDel" id="doSeasonDel" class="btn btn-secondary action" />
-			</div>
-
-			<div class="container">
-				<div class="row table-header">
-					<div class="col-12 col-md-1 check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('seasons-filter'));" /></div>
-					<div class="col-12 col-md-1 column-num">ID</div>
-					<div class="col-12 col-md-2"><?php _e( 'Name', 'racketmanager' ) ?></div>
-					<div class="col-12 col-md-3"><?php _e( 'Action', 'racketmanager' ) ?></div>
+		<div class="row table-header">
+			<div class="col-12 col-md-1 check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('seasons-filter'));" /></div>
+			<div class="col-12 col-md-1 column-num">ID</div>
+			<div class="col-12 col-md-2"><?php _e( 'Name', 'racketmanager' ) ?></div>
+			<div class="col-12 col-md-3"><?php _e( 'Action', 'racketmanager' ) ?></div>
+		</div>
+		<?php if ( $seasons = $racketmanager->getSeasons() ) {
+			$class = '';
+			foreach ( $seasons AS $season ) { ?>
+				<?php $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
+				<div class="row table-row <?php echo $class ?>">
+					<div class="col-12 col-md-1 check-column">
+						<input type="checkbox" value="<?php echo $season->id ?>" name="season[<?php echo $season->id ?>]" />
+					</div>
+					<div class="col-12 col-md-1 column-num"><?php echo $season->id ?></div>
+					<div class="col-12 col-md-2"><?php echo $season->name ?></div>
+					<div class="col-12 col-md-3"><a class="btn btn-secondary" href="admin.php?page=racketmanager-admin&amp;subpage=competitions&amp;season=<?php echo $season->name ?>">Add Competitions</a></div>
 				</div>
-				<?php if ( $seasons = $racketmanager->getSeasons() ) {
-					$class = '';
-					foreach ( $seasons AS $season ) { ?>
-						<?php $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
-						<div class="row table-row <?php echo $class ?>">
-							<div class="col-12 col-md-1 check-column">
-								<input type="checkbox" value="<?php echo $season->id ?>" name="season[<?php echo $season->id ?>]" />
-							</div>
-							<div class="col-12 col-md-1 column-num"><?php echo $season->id ?></div>
-							<div class="col-12 col-md-2"><?php echo $season->name ?></div>
-							<div class="col-12 col-md-3"><a class="btn btn-secondary" href="admin.php?page=racketmanager-admin&amp;subpage=competitions&amp;season=<?php echo $season->name ?>">Add Competitions</a></div>
-						</div>
-					<?php } ?>
-				<?php } ?>
-			</div>
-		</form>
+			<?php } ?>
+		<?php } ?>
 	</div>
-	<!-- Add New Season -->
-	<div clas="container">
-		<h2><?php _e( 'Add Season', 'racketmanager' ) ?></h2>
-		<form action="" method="post" class="form-control">
-			<?php wp_nonce_field( 'racketmanager_add-season' ) ?>
-			<div class="form-group">
-				<label for="seasonName"><?php _e( 'Name', 'racketmanager' ) ?></label>
-				<div class="input">
-					<input required="required" placeholder="<?php _e( 'Enter season name', 'racketmanager') ?>" type="text" name="seasonName" id="seasonName" value=""  />
-				</div>
+</form>
+<!-- Add New Season -->
+<div clas="container">
+	<h2><?php _e( 'Add Season', 'racketmanager' ) ?></h2>
+	<form action="" method="post" class="form-control">
+		<?php wp_nonce_field( 'racketmanager_add-season' ) ?>
+		<div class="form-group">
+			<label for="seasonName"><?php _e( 'Name', 'racketmanager' ) ?></label>
+			<div class="input">
+				<input required="required" placeholder="<?php _e( 'Enter season name', 'racketmanager') ?>" type="text" name="seasonName" id="seasonName" value=""  />
 			</div>
-			<input type="hidden" name="addSeason" value="season" />
-			<input type="submit" name="addSeason" value="<?php _e( 'Add Season','racketmanager' ) ?>" class="btn btn-primary" />
+		</div>
+		<input type="hidden" name="addSeason" value="season" />
+		<input type="submit" name="addSeason" value="<?php _e( 'Add Season','racketmanager' ) ?>" class="btn btn-primary" />
 
-		</form>
-	</div>
+	</form>
 </div>
--- admin/results/results.php
+++ admin/results/results.php
@@ -5,49 +5,47 @@ $prev_league = 0;
 ?>
 <div class="container">
   <?php wp_nonce_field( 'results-update' ) ?>
-  <div class="container">
-    <div class="row table-header">
-      <div class="col-12 col-md-1 check-column"><?php _e( 'ID', 'racketmanager' ) ?></div>
-      <div class="col-12 col-md-2"><?php _e( 'Date','racketmanager' ) ?></div>
-      <div class="col-12 col-md-3"><?php _e( 'Match','racketmanager' ) ?></div>
-      <div class="col-12 col-md-2"><?php _e( 'Location','racketmanager' ) ?></div>
-      <div class="col-12 col-md-1"><?php _e( 'Begin','racketmanager' ) ?></div>
-      <div class="col-12 col-md-2"><?php _e( 'Score', 'racketmanager' ) ?></div>
-      <div class="col-12 col-md-1"></div>
-    </div>
-    <?php if ( $matches ) { $class = '';
-      foreach ( $matches AS $match ) {
-        $match = get_match($match);
-        if ( $match->league->is_championship ) {
-          $matchLink = 'final='.$match->final_round.'&amp;league-tab=matches';
-        } else {
-          $matchLink = 'match_day='.$match->match_day;
-        }
-        $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
-
-        <div class="row table-row <?php echo $class ?>">
-          <?php if ( $prev_league != $match->league_id) {
-            $prev_league = $match->league_id; ?>
-            <div class="col-12"><?php echo $match->league->title ?></div>
-          <?php } ?>
-          <div class="col-12 col-md-1 check-column"><?php echo $match->id ?>
-            <input type="hidden" name="matches[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->id ?>" />
-            <input type="hidden" name="home_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->home_team ?>" />
-            <input type="hidden" name="away_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->away_team ?>" />
-          </div>
-          <div class="col-12 col-md-2"><?php echo ( substr($match->date, 0, 10) == '0000-00-00' ) ? 'N/A' : mysql2date($this->date_format, $match->date) ?></div>
-          <div class="col-12 col-md-3 match-title"><a href="admin.php?page=racketmanager&amp;subpage=match&amp;league_id=<?php echo $match->league_id ?>&amp;edit=<?php echo $match->id ?>&amp;season=<?php echo $match->season ?>"><?php echo $match->match_title ?></a></div>
-          <div class="col-12 col-md-2"><?php echo ( empty($match->location) ) ? 'N/A' : $match->location ?></div>
-          <div class="col-12 col-md-1"><?php echo ( '00:00' == $match->hour.":".$match->minutes ) ? 'N/A' : mysql2date($this->time_format, $match->date) ?></div>
-          <div class="col-12 col-md-1">
-            <?php echo $match->score ?>
-          </div>
-          <div class="col-12 col-md-2"><a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $match->league->id ?>&amp;season=<?php echo $match->season ?>&amp;<?php echo $matchLink ?> " class="btn btn-secondary"><?php _e('View match', 'racketmanager') ?></a>
-          </div>
-        </div>
-      <?php }
-    } else { ?>
-      <div class="col-auto"><?php _e('No matches with pending results', 'racketmanager') ?></div>
-    <?php } ?>
+  <div class="row table-header">
+    <div class="col-12 col-md-1 check-column"><?php _e( 'ID', 'racketmanager' ) ?></div>
+    <div class="col-12 col-md-2"><?php _e( 'Date','racketmanager' ) ?></div>
+    <div class="col-12 col-md-3"><?php _e( 'Match','racketmanager' ) ?></div>
+    <div class="col-12 col-md-2"><?php _e( 'Location','racketmanager' ) ?></div>
+    <div class="col-12 col-md-1"><?php _e( 'Begin','racketmanager' ) ?></div>
+    <div class="col-12 col-md-2"><?php _e( 'Score', 'racketmanager' ) ?></div>
+    <div class="col-12 col-md-1"></div>
   </div>
+  <?php if ( $matches ) { $class = '';
+    foreach ( $matches AS $match ) {
+      $match = get_match($match);
+      if ( $match->league->is_championship ) {
+        $matchLink = 'final='.$match->final_round.'&amp;league-tab=matches';
+      } else {
+        $matchLink = 'match_day='.$match->match_day;
+      }
+      $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
+
+      <div class="row table-row <?php echo $class ?>">
+        <?php if ( $prev_league != $match->league_id) {
+          $prev_league = $match->league_id; ?>
+          <div class="col-12"><?php echo $match->league->title ?></div>
+        <?php } ?>
+        <div class="col-12 col-md-1 check-column"><?php echo $match->id ?>
+          <input type="hidden" name="matches[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->id ?>" />
+          <input type="hidden" name="home_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->home_team ?>" />
+          <input type="hidden" name="away_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->away_team ?>" />
+        </div>
+        <div class="col-12 col-md-2"><?php echo ( substr($match->date, 0, 10) == '0000-00-00' ) ? 'N/A' : mysql2date($this->date_format, $match->date) ?></div>
+        <div class="col-12 col-md-3 match-title"><a href="admin.php?page=racketmanager&amp;subpage=match&amp;league_id=<?php echo $match->league_id ?>&amp;edit=<?php echo $match->id ?>&amp;season=<?php echo $match->season ?>"><?php echo $match->match_title ?></a></div>
+        <div class="col-12 col-md-2"><?php echo ( empty($match->location) ) ? 'N/A' : $match->location ?></div>
+        <div class="col-12 col-md-1"><?php echo ( '00:00' == $match->hour.":".$match->minutes ) ? 'N/A' : mysql2date($this->time_format, $match->date) ?></div>
+        <div class="col-12 col-md-1">
+          <?php echo $match->score ?>
+        </div>
+        <div class="col-12 col-md-2"><a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $match->league->id ?>&amp;season=<?php echo $match->season ?>&amp;<?php echo $matchLink ?> " class="btn btn-secondary"><?php _e('View match', 'racketmanager') ?></a>
+        </div>
+      </div>
+    <?php }
+  } else { ?>
+    <div class="col-auto"><?php _e('No matches with pending results', 'racketmanager') ?></div>
+  <?php } ?>
 </div>

b5652d96d70d8b9fc89b0427063ef44f6a34a53d
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-03-21 21:53:59 +0000

    use bootstrap accordion

--- templates/teams-list.php
+++ templates/teams-list.php
@@ -3,20 +3,20 @@
 Template page for Team List
 
 The following variables are usable:
-	
-	$league league object
-	$teams: all teams of league
-	
-	You can check the content of a variable when you insert the tag <?php var_dump($variable) ?>
+
+$league league object
+$teams: all teams of league
+
+You can check the content of a variable when you insert the tag <?php var_dump($variable) ?>
 */
 ?>
 <?php if ( $teams ) { ?>
 
-<div class="teamlist jquery-ui-accordion">
-<?php while ( have_teams() ) {
-    the_team();
-    include('team.php');
-} ?>
-</div>
+  <div class="accordion accordion-flush" id="teamsList">
+    <?php while ( have_teams() ) {
+      the_team();
+      include('team.php');
+    } ?>
+  </div>
 
 <?php } ?>

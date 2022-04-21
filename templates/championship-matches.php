54b50df406c18206db7488cbf3fbb82d63adc939
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-04-21 11:59:42 +0100

    fix tab handling for multiple leagues

--- templates/championship-matches.php
+++ templates/championship-matches.php
@@ -1,9 +1,9 @@
 <!-- Nav tabs -->
-<ul class="nav nav-tabs frontend" id="myTab" role="tablist">
+<ul class="nav nav-tabs frontend" id="myTab-<?php echo $league->id ?>" role="tablist">
   <?php $i = 0;
   foreach ( $finals AS $final ) { ?>
     <li class="nav-item" role="presentation">
-      <button class="nav-link <?php if ( $i == 0 ) { echo 'active'; } ?>" id="final-<?php echo $final->key ?>-tab" data-bs-toggle="pill" data-bs-target="#final-<?php echo $final->key ?>" type="button" role="tab" aria-controls="final-<?php echo $final->key ?>" aria-selected="true"><?php echo $final->name ?></button>
+      <button class="nav-link <?php if ( $i == 0 ) { echo 'active'; } ?>" id="final-<?php echo $final->key ?>-<?php echo $league->id ?>-tab" data-bs-toggle="pill" data-bs-target="#final-<?php echo $final->key ?>-<?php echo $league->id ?>" type="button" role="tab" aria-controls="final-<?php echo $final->key ?>-<?php echo $league->id ?>" aria-selected="true"><?php echo $final->name ?></button>
     </li>
     <?php $i ++;
     } ?>
@@ -12,7 +12,7 @@
 <div class="tab-content">
   <?php $i = 0;
   foreach ( $finals AS $final ) { ?>
-	<div class="tab-pane fade <?php if ( $i == 0 ) { echo 'show active'; } ?>" id="final-<?php echo $final->key ?>" role="tabpanel" aria-labelledby="final-<?php echo $final->key ?>-tab">
+	<div class="tab-pane fade <?php if ( $i == 0 ) { echo 'show active'; } ?>" id="final-<?php echo $final->key ?>-<?php echo $league->id ?>" role="tabpanel" aria-labelledby="final-<?php echo $final->key ?>-<?php echo $league->id ?>-tab">
     <?php $matches = $final->matches; ?>
     <?php include('matches-tennis-scores.php'); ?>
 	</div>
--- templates/championship.php
+++ templates/championship.php
@@ -14,12 +14,12 @@ You can check the content of a variable when you insert the tag <?php var_dump($
 global $wp_query;
 $postID = $wp_query->post->ID;
 $archive = true;
-$tab = 'draw';
+$tab = 'draw-'.$league->id;
 ?>
 <script type='text/javascript'>
 var tab = '<?php echo $tab ?>;'
 var hash = window.location.hash.substr(1);
-if (hash == 'teams') tab = 'teams';
+if (hash == 'teams') tab = 'teams-' + <?php echo $league->id ?>;
 jQuery(function() {
 	activaTab('<?php echo $tab ?>');
 });
@@ -27,36 +27,36 @@ jQuery(function() {
 <!-- Nav tabs -->
 <ul class="nav nav-tabs frontend" id="myTab" role="tablist">
 	<li class="nav-item" role="presentation">
-		<button class="nav-link" id="draw-tab" data-bs-toggle="pill" data-bs-target="#draw" type="button" role="tab" aria-controls="draw" aria-selected="true"><?php _e( 'Draw', 'racketmanager' ) ?></button>
+		<button class="nav-link" id="draw-<?php echo $league->id ?>-tab" data-bs-toggle="pill" data-bs-target="#draw-<?php echo $league->id ?>" type="button" role="tab" aria-controls="draw-<?php echo $league->id ?>" aria-selected="true"><?php _e( 'Draw', 'racketmanager' ) ?></button>
 	</li>
 	<li class="nav-item" role="presentation">
-		<button class="nav-link" id="matches-tab" data-bs-toggle="pill" data-bs-target="#matches" type="button" role="tab" aria-controls="matches" aria-selected="false"><?php _e( 'Matches', 'racketmanager' ) ?></button>
+		<button class="nav-link" id="matches-<?php echo $league->id ?>-tab" data-bs-toggle="pill" data-bs-target="#matches-<?php echo $league->id ?>" type="button" role="tab" aria-controls="matches-<?php echo $league->id ?>" aria-selected="false"><?php _e( 'Matches', 'racketmanager' ) ?></button>
 	</li>
 	<li class="nav-item" role="presentation">
-		<button class="nav-link" id="teams-tab" data-bs-toggle="pill" data-bs-target="#teams" type="button" role="tab" aria-controls="teams" aria-selected="false"><?php _e( 'Teams', 'racketmanager' ) ?></button>
+		<button class="nav-link" id="teams-<?php echo $league->id ?>-tab" data-bs-toggle="pill" data-bs-target="#teams-<?php echo $league->id ?>" type="button" role="tab" aria-controls="teams-<?php echo $league->id ?>" aria-selected="false"><?php _e( 'Teams', 'racketmanager' ) ?></button>
 	</li>
 	<?php if ( !isset($league->entryType) || $league->entryType != 'player' ) { ?>
 	<li class="nav-item" role="presentation">
-		<button class="nav-link" id="players-tab" data-bs-toggle="pill" data-bs-target="#players" type="button" role="tab" aria-controls="players" aria-selected="false"><?php _e( 'Players', 'racketmanager' ) ?></button>
+		<button class="nav-link" id="players-<?php echo $league->id ?>-tab" data-bs-toggle="pill" data-bs-target="#players-<?php echo $league->id ?>" type="button" role="tab" aria-controls="players-<?php echo $league->id ?>" aria-selected="false"><?php _e( 'Players', 'racketmanager' ) ?></button>
 	</li>
 		<?php } ?>
 </ul>
 <!-- Tab panes -->
 <div class="tab-content">
-	<div class="tab-pane fade" id="draw" role="tabpanel" aria-labelledby="draw-tab">
+	<div class="tab-pane fade" id="draw-<?php echo $league->id ?>" role="tabpanel" aria-labelledby="draw-tab">
 		<h3 class="header"><?php _e('Draw', 'racketmanager') ?></h3>
 		<?php include('championship-results.php'); ?>
 	</div>
-	<div class="tab-pane fade" id="matches" role="tabpanel" aria-labelledby="matches-tab">
+	<div class="tab-pane fade" id="matches-<?php echo $league->id ?>" role="tabpanel" aria-labelledby="matches-tab">
 		<h3 class="header"><?php _e('Matches', 'racketmanager') ?></h3>
 		<?php include('championship-matches.php'); ?>
 	</div>
-	<div class="tab-pane fade" id="teams" role="tabpanel" aria-labelledby="teams-tab">
+	<div class="tab-pane fade" id="teams-<?php echo $league->id ?>" role="tabpanel" aria-labelledby="teams-tab">
 		<h3 class="header"><?php _e('Teams', 'racketmanager') ?></h3>
 		<?php racketmanager_teams( $league->id, array('season' => get_current_season(), 'template' => 'list') ) ?>
 	</div>
 	<?php if ( !isset($league->entryType) || $league->entryType != 'player' ) { ?>
-	<div class="tab-pane fade" id="players" role="tabpanel" aria-labelledby="players-tab">
+	<div class="tab-pane fade" id="players-<?php echo $league->id ?>" role="tabpanel" aria-labelledby="players-tab">
 		<h3 class="header"><?php _e('Players', 'racketmanager') ?></h3>
 			<?php racketmanager_players( $league->id, array('season' => get_current_season()) ) ?>	</div>
 	<?php } ?>

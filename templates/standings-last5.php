1ee73242c82f76c70dc798acc47640ba238457eb
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-05-16 14:31:54 +0100

    hide columns on small screens

--- templates/standings-last5.php
+++ templates/standings-last5.php
@@ -12,7 +12,7 @@ namespace ns;
       <thead class="table-dark">
         <tr>
           <th class="num"><?php echo _e( 'Pos', 'racketmanager' ) ?></th>
-          <?php if ( show_standings('status') ) {      ?><th class="num">&#160;</th><?php } ?>
+          <?php if ( show_standings('status') ) {      ?><th class="num d-none d-md-table-cell">&#160;</th><?php } ?>
           <th class="team"><?php _e( 'Team', 'racketmanager' ) ?></th>
           <?php if ( show_standings('pld') ) {         ?><th class="num"><?php _e( 'Pld', 'racketmanager' ) ?></th><?php } ?>
           <?php if ( show_standings('won') ) {         ?><th class="num"><?php _e( 'W','racketmanager' ) ?></th><?php } ?>
@@ -20,7 +20,7 @@ namespace ns;
           <?php if ( show_standings('lost') ) {        ?><th class="num"><?php  _e( 'L','racketmanager' ) ?></th><?php } ?>
           <?php if ( show_standings('winPercent') ) {  ?><th class="num"><?php  _e( 'PCT','racketmanager' ) ?></th><?php } ?>
           <?php the_standings_header(); ?>
-          <th class="num"><?php _e( 'Pts Adjust', 'racketmanager' ) ?></th>
+          <th class="num d-none d-md-table-cell"><?php _e( 'Pts Adjust', 'racketmanager' ) ?></th>
           <th class="num"><?php _e( 'Pts', 'racketmanager' ) ?></th>
           <?php if ( show_standings('last5') ) {       ?><th class="last5 d-none d-md-table-cell"><?php _e( 'Last 5', 'racketmanager' ) ?></th><?php } ?>
         </tr>
@@ -31,7 +31,7 @@ namespace ns;
           ?>
           <tr class='<?php the_team_class(); ?>'>
             <td class='num'><span class="rank"><?php the_team_rank(); ?></span></td>
-            <?php if ( show_standings('status') ) {      ?><td class="num"><?php the_team_status(); ?></td><?php } ?>
+            <?php if ( show_standings('status') ) {      ?><td class="num d-none d-md-table-cell"><?php the_team_status(); ?></td><?php } ?>
             <td><?php the_team_name_url(show_standings('team_link')) ?></td>
             <?php if ( show_standings('pld') ) {         ?><td class='num'><?php num_done_matches(); ?></td><?php } ?>
             <?php if ( show_standings('won') ) {         ?><td class='num'><?php num_won_matches(); ?></td><?php } ?>
@@ -39,7 +39,7 @@ namespace ns;
             <?php if ( show_standings('lost') ) {        ?><td class='num'><?php num_lost_matches(); ?></td><?php } ?>
             <?php if ( show_standings('winPercent') ) {  ?><td class="num"><?php win_percentage() ?></td><?php } ?>
             <?php the_standings_columns(); ?>
-            <td class='num'><?php the_team_points_adjust(); ?></td>
+            <td class='num d-none d-md-table-cell'><?php the_team_points_adjust(); ?></td>
             <td class='num'><?php the_team_points(); ?></td>
             <?php if ( show_standings('last5') ) {       ?><td class="last5Icon last5 d-none d-md-table-cell"><?php the_last5_matches(show_standings('team_link')); ?></td><?php } ?>
           </tr>

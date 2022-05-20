c9da807f78ccab48056b57b07a60433cee576850
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-05-20 21:08:43 +0100

    * UPDATE: tidy up results page

--- admin/results/results.php
+++ admin/results/results.php
@@ -1,16 +1,13 @@
 <?php
 global $racketmanager;
-$matches = $racketmanager->getMatches( array('confirmed' => true) );
+$matches = $racketmanager->getMatches( array('confirmed' => true, 'order' => array( 'updated' => 'ASC', 'id' => 'ASC')) );
 $prev_league = 0;
 ?>
 <div class="container">
   <?php wp_nonce_field( 'results-update' ) ?>
   <div class="row table-header">
-    <div class="col-12 col-md-1 check-column"><?php _e( 'ID', 'racketmanager' ) ?></div>
     <div class="col-12 col-md-2"><?php _e( 'Date','racketmanager' ) ?></div>
-    <div class="col-12 col-md-3"><?php _e( 'Match','racketmanager' ) ?></div>
-    <div class="col-12 col-md-2"><?php _e( 'Location','racketmanager' ) ?></div>
-    <div class="col-12 col-md-1"><?php _e( 'Begin','racketmanager' ) ?></div>
+    <div class="col-12 col-md-4"><?php _e( 'Match','racketmanager' ) ?></div>
     <div class="col-12 col-md-2"><?php _e( 'Score', 'racketmanager' ) ?></div>
     <div class="col-12 col-md-1"></div>
   </div>
@@ -29,15 +26,11 @@ $prev_league = 0;
           $prev_league = $match->league_id; ?>
           <div class="col-12"><?php echo $match->league->title ?></div>
         <?php } ?>
-        <div class="col-12 col-md-1 check-column"><?php echo $match->id ?>
-          <input type="hidden" name="matches[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->id ?>" />
-          <input type="hidden" name="home_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->home_team ?>" />
-          <input type="hidden" name="away_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->away_team ?>" />
-        </div>
+        <input type="hidden" name="matches[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->id ?>" />
+        <input type="hidden" name="home_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->home_team ?>" />
+        <input type="hidden" name="away_team[<?php echo $match->league->id ?>][<?php echo $match->id ?>]" value="<?php echo $match->away_team ?>" />
         <div class="col-12 col-md-2"><?php echo ( substr($match->date, 0, 10) == '0000-00-00' ) ? 'N/A' : mysql2date($this->date_format, $match->date) ?></div>
-        <div class="col-12 col-md-3 match-title"><a href="admin.php?page=racketmanager&amp;subpage=match&amp;league_id=<?php echo $match->league_id ?>&amp;edit=<?php echo $match->id ?>&amp;season=<?php echo $match->season ?>"><?php echo $match->match_title ?></a></div>
-        <div class="col-12 col-md-2"><?php echo ( empty($match->location) ) ? 'N/A' : $match->location ?></div>
-        <div class="col-12 col-md-1"><?php echo ( '00:00' == $match->hour.":".$match->minutes ) ? 'N/A' : mysql2date($this->time_format, $match->date) ?></div>
+        <div class="col-12 col-md-4 match-title"><?php echo $match->match_title ?></div>
         <div class="col-12 col-md-1">
           <?php echo $match->score ?>
         </div>

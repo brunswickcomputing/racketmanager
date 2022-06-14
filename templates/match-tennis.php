f47d573be13ca1a8482a42b4147aec6e38b98964
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-06-14 11:21:38 +0100

    * UPDATE: show login prompt on match details when not logged in

--- templates/match-tennis.php
+++ templates/match-tennis.php
@@ -17,21 +17,29 @@ $postID = $wp_query->post->ID;
 		<h1 class="header"><?php echo $match->match_title ?></h1>
 		<?php include('league-selections.php'); ?>
 
-		<div id="viewMatchRubbers">
-				<div id="splash" class="d-none">
-						<section id="waitingMatch">
-								<p>Please wait</p>
-								<div class="spinnerMatch"></div>
-						</section>
+		<?php if ( is_user_logged_in() ) { ?>
+			<div id="viewMatchRubbers">
+					<div id="splash" class="d-none">
+							<section id="waitingMatch">
+									<p>Please wait</p>
+									<div class="spinnerMatch"></div>
+							</section>
+					</div>
+					<div id="showMatchRubbers">
+						<?php if (isset($match->league->num_rubbers) && $match->league->num_rubbers > 0 ) {
+							$racketmanager->showRubbersScreen($match);
+						} else {
+							$racketmanager->showMatchScreen($match);
+						} ?>
+					</div>
+			</div>
+		<?php } else { ?>
+			<div class="row justify-content-center">
+				<div class="col-auto">
+					<?php _e('You need to ','racketmanager') ?><a href="<?php echo wp_login_url( $_SERVER['REQUEST_URI'] ); ?>"><?php _e('login', 'racketmanager') ?></a> <?php _e('to enter match information', 'racketmanager') ?>
 				</div>
-				<div id="showMatchRubbers">
-					<?php if (isset($match->league->num_rubbers) && $match->league->num_rubbers > 0 ) {
-						$racketmanager->showRubbersScreen($match);
-					} else {
-						$racketmanager->showMatchScreen($match);
-					} ?>
-				</div>
-		</div>
+			</div>
+		<?php } ?>
 	</div>
 
 <?php }  ?>

21f5b529b69dd41ee2eb3d615f0bab3e3f664d75
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-06-17 14:43:41 +0100

    * UPDATE: change button spacing on outstanding results screen

--- admin/results/pending-results.php
+++ admin/results/pending-results.php
@@ -23,10 +23,10 @@ $prev_league = 0;
         <div class="col-9 col-sm-6 col-lg-4 match-title">
           <a href="admin.php?page=racketmanager&amp;subpage=show-league&amp;league_id=<?php echo $match->league->id ?>&amp;season=<?php echo $match->season ?>&amp;<?php echo $matchLink ?> "><?php echo $match->match_title ?></a>
         </div>
-        <div class="col-12 col-md-auto">
+        <div class="col-auto">
           <a href="admin.php?page=racketmanager-results&amp;subpage=match&amp;match_id=<?php echo $match->id ?>&amp;referrer=pendingresults" class="btn btn-primary"><?php _e('Enter result', 'racketmanager') ?></a>
         </div>
-        <div class="col-12 col-md-auto">
+        <div class="col-auto">
           <a class="btn btn-secondary" onclick="Racketmanager.chaseMatchResult('<?php echo ($match->id) ?>');">
           <?php _e( 'Chase result', 'racketmanager' ) ?></a>
         </div>

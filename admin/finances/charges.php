ef3484bcaf606a74b782872e7894ce02538105d3
brunswickcomputing <paul@brunswickcomputing.co.uk>
2022-12-15 17:59:14 +0000

    tidy up code

--- admin/finances/charges.php
+++ admin/finances/charges.php
@@ -2,21 +2,23 @@
 ?>
 <div class="container">
 
-	<form id="charges-filter" method="post" action="" class="form-control mb-3">
+	<form id="charges-action" method="post" action="" class="form-control mb-3">
 		<?php wp_nonce_field( 'charges-bulk' ) ?>
 
-		<div class="tablenav">
+		<div class="row justify-content-between mb-3">
 			<!-- Bulk Actions -->
-			<select name="action" size="1">
-				<option value="-1" selected="selected"><?php _e('Bulk Actions') ?></option>
-				<option value="delete"><?php _e('Delete')?></option>
-			</select>
-			<input type="submit" value="<?php _e('Apply'); ?>" name="doChargesDel" id="doChargesDel" class="btn btn-secondary action" />
+			<div class="col-auto">
+				<select name="action" size="1">
+					<option value="-1" selected="selected"><?php _e('Bulk Actions') ?></option>
+					<option value="delete"><?php _e('Delete')?></option>
+				</select>
+				<button name="doChargesDel" id="doChargesDel" class="btn btn-secondary"><?php _e('Apply') ?></button>
+			</div>
 		</div>
 
-		<div class="container">
+		<div class="container striped">
 			<div class="row table-header">
-				<div class="col-2 col-lg-1 check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('charges-filter'));" /></div>
+				<div class="col-2 col-lg-1 check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('charges-action'));" /></div>
 				<div class="d-none d-lg-1 col-1 column-num">ID</div>
 				<div class="col-6"><?php _e( 'Name', 'racketmanager' ) ?></div>
 				<div class="col-3"><?php _e( 'Status', 'racketmanager' ) ?></div>
@@ -24,10 +26,8 @@
 
 			<?php
 			if ( $charges = $racketmanager->getCharges() ) {
-				$class = '';
-				foreach ( $charges AS $charge ) {
-					$class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
-					<div class="row table-row <?php echo $class ?>">
+				foreach ( $charges AS $charge ) { ?>
+					<div class="row table-row">
 						<div class="col-2 col-lg-1 check-column"><input type="checkbox" value="<?php echo $charge->id ?>" name="charge[<?php echo $charge->id ?>]" /></div>
 						<div class="d-none d-lg-1 col-1 column-num"><?php echo $charge->id ?></div>
 						<div class="col-6"><a href="admin.php?page=racketmanager-finances&amp;subpage=charges&amp;charges=<?php echo $charge->id ?>"><?php echo $charge->season.' '.ucfirst($charge->type).' '.ucfirst($charge->competitionType) ?></a></div>

5a7d0e9f13739db7e224c77942c3b777bb1f55a3
brunswickcomputing <paul@brunswickcomputing.co.uk>
2023-05-10 13:59:12 +0100

    use correct fieldname

--- admin/includes/settings/players.php
+++ admin/includes/settings/players.php
@@ -1,7 +1,7 @@
 <div class="form-control">
       <div class="form-floating mb-3">
-        <input type="number" class="form-control" name='rosterLeadTime' id='rosterLeadTime' value='<?php echo isset($options['checks']['rosterLeadTime']) ? $options['checks']['rosterLeadTime'] : '' ?>' />
-        <label for='rosterLeadTime'><?php _e( 'Player Registration Lead Time (days)', 'racketmanager' ) ?></label>
+        <input type="number" class="form-control" name='playerLeadTime' id='playerLeadTime' value='<?php echo isset($options['checks']['playerLeadTime']) ? $options['checks']['playerLeadTime'] : '' ?>' />
+        <label for='playerLeadTime'><?php _e( 'Player Registration Lead Time (days)', 'racketmanager' ) ?></label>
       </div>
     <div class="form-floating mb-3">
         <input type="number" class="form-control" name='playedRounds' id='playedRounds' value='<?php echo isset($options['checks']['playedRounds']) ? $options['checks']['playedRounds'] : '' ?>' />

be455e87b33f2dda3275f5c0eef26ca1384e9119
brunswickcomputing <paul@brunswickcomputing.co.uk>
2024-02-27 00:25:35 +0000

    display championship matches correctly

--- templates/event.php
+++ templates/event.php
@@ -277,7 +277,20 @@ if ( empty( $tab ) ) {
 						</div>
 						<div class="module__content">
 							<div class="module-container">
-								<?php require 'championship-matches.php'; ?>
+								<?php
+								foreach ( $event->leagues as $league ) {
+									?>
+									<h4 class="header">
+										<?php echo esc_html( $league->title ); ?>
+									</h4>
+									<?php
+									$finals   = $league->finals;
+									$champion = null;
+									require RACKETMANAGER_PATH . 'templates/championship-matches.php';
+									?>
+									<?php
+								}
+								?>
 							</div>
 						</div>
 					</div>

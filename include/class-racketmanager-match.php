da0ca444fd49698c8186c61849ace66cde422603
Paul Moffat <paul@brunswickcomputing.co.uk>
2024-04-09 13:58:04 +0100

    add extra rubbers when reverse rubber option

--- include/class-racketmanager-match.php
+++ include/class-racketmanager-match.php
@@ -526,6 +526,8 @@ final class Racketmanager_Match {
 			$max_rubbers = $this->league->num_rubbers;
 			if ( $this->league->is_championship && ! empty( $this->league->current_season['homeAway'] && 'true' === $this->league->current_season['homeAway'] ) && ! empty( $this->leg ) && 2 === $this->leg && 'MPL' === $this->league->event->scoring ) {
 				++$max_rubbers;
+			} elseif ( '1' === $this->league->event->reverse_rubbers ) {
+				$max_rubbers = $max_rubbers * 2;
 			}
 		}
 		$sql = $wpdb->prepare(

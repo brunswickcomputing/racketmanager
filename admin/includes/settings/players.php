f06110d416d684ab0af314c77d42586631bcbed3
brunswickcomputing <paul@brunswickcomputing.co.uk>
2024-05-10 15:13:43 +0100

    format file correctly

--- admin/includes/settings/players.php
+++ admin/includes/settings/players.php
@@ -1,14 +1,24 @@
+<?php
+/**
+ * Player options administration panel
+ *
+ * @package Racketmanager/Admin/Templates
+ */
+
+namespace Racketmanager;
+
+?>
 <div class="form-control">
       <div class="form-floating mb-3">
-        <input type="number" class="form-control" name='playerLeadTime' id='playerLeadTime' value='<?php echo isset($options['checks']['playerLeadTime']) ? $options['checks']['playerLeadTime'] : '' ?>' />
-        <label for='playerLeadTime'><?php _e( 'Player Registration Lead Time (days)', 'racketmanager' ) ?></label>
+		<input type="number" class="form-control" name='playerLeadTime' id='playerLeadTime' value='<?php echo esc_html( isset( $options['checks']['rosterLeadTime'] ) ? $options['checks']['rosterLeadTime'] : '' ); ?>' />
+		<label for='playerLeadTime'><?php esc_html_e( 'Player Registration Lead Time (hours)', 'racketmanager' ); ?></label>
       </div>
     <div class="form-floating mb-3">
-        <input type="number" class="form-control" name='playedRounds' id='playedRounds' value='<?php echo isset($options['checks']['playedRounds']) ? $options['checks']['playedRounds'] : '' ?>' />
-        <label for='playedRounds'><?php _e( 'End of season eligibility (Match Days)', 'racketmanager' ) ?></label>
+		<input type="number" class="form-control" name='playedRounds' id='playedRounds' value='<?php echo esc_html( isset( $options['checks']['playedRounds'] ) ? $options['checks']['playedRounds'] : '' ); ?>' />
+		<label for='playedRounds'><?php esc_html_e( 'End of season eligibility (Match Days)', 'racketmanager' ); ?></label>
     </div>
     <div class="form-floating mb-3">
-        <input type="number" class="form-control" name='playerLocked' id='playerLocked' value='<?php echo isset($options['checks']['playerLocked']) ? $options['checks']['playerLocked'] : '' ?>' />
-        <label for='playerLocked'><?php _e( 'How many matches lock a player', 'racketmanager' ) ?></label>
+		<input type="number" class="form-control" name='playerLocked' id='playerLocked' value='<?php echo esc_html( isset( $options['checks']['playerLocked'] ) ? $options['checks']['playerLocked'] : '' ); ?>' />
+		<label for='playerLocked'><?php esc_html_e( 'How many matches lock a player', 'racketmanager' ); ?></label>
     </div>
 </div>

1809b783cea8f6011af03050628ce9f00d76b148
brunswickcomputing <paul@brunswickcomputing.co.uk>
2024-05-10 12:54:03 +0100

    handle match id only passed

--- include/class-racketmanager-shortcodes-competition.php
+++ include/class-racketmanager-shortcodes-competition.php
@@ -676,57 +676,62 @@ class Racketmanager_Shortcodes_Competition extends Racketmanager_Shortcodes {
 		$template = $args['template'];
 		// Get Match ID from shortcode or $_GET.
 		if ( ! $match_id ) {
+			$match_id = get_query_var( 'match_id' );
+		}
+		if ( ! $match_id ) {
 			$league_name = un_seo_url( get_query_var( 'league_name' ) );
 			if ( $league_name ) {
 				$league = get_league( $league_name, 'name' );
 			}
-			$home_team_name = un_seo_url( get_query_var( 'teamHome' ) );
-			$away_team_name = un_seo_url( get_query_var( 'teamAway' ) );
-			$match_day      = get_query_var( 'match_day' );
-			$season         = get_query_var( 'season' );
-			$round          = get_query_var( 'round' );
-			$leg            = get_query_var( 'leg' );
-			$match_args     = array();
-			if ( $home_team_name ) {
-				if ( 'player' === $league->event->competition->entry_type ) {
-					$home_team_name = $this->get_player_team_name( $home_team_name );
+			if ( $league ) {
+				$home_team_name = un_seo_url( get_query_var( 'teamHome' ) );
+				$away_team_name = un_seo_url( get_query_var( 'teamAway' ) );
+				$match_day      = get_query_var( 'match_day' );
+				$season         = get_query_var( 'season' );
+				$round          = get_query_var( 'round' );
+				$leg            = get_query_var( 'leg' );
+				$match_args     = array();
+				if ( $home_team_name ) {
+					if ( 'player' === $league->event->competition->entry_type ) {
+						$home_team_name = $this->get_player_team_name( $home_team_name );
+					}
+					$home_team               = $racketmanager->getteam_id( $home_team_name );
+					$match_args['home_team'] = $home_team;
 				}
-				$home_team               = $racketmanager->getteam_id( $home_team_name );
-				$match_args['home_team'] = $home_team;
-			}
-			if ( $away_team_name ) {
-				if ( 'player' === $league->event->competition->entry_type ) {
-					$away_team_name = $this->get_player_team_name( $away_team_name );
+				if ( $away_team_name ) {
+					if ( 'player' === $league->event->competition->entry_type ) {
+						$away_team_name = $this->get_player_team_name( $away_team_name );
+					}
+					$away_team               = $racketmanager->getteam_id( $away_team_name );
+					$match_args['away_team'] = $away_team;
 				}
-				$away_team               = $racketmanager->getteam_id( $away_team_name );
-				$match_args['away_team'] = $away_team;
-			}
-			if ( $match_day ) {
-				$match_args['match_day'] = $match_day;
-			}
-			if ( $round ) {
-				if ( $league->is_championship ) {
-					$match_args['final'] = $round;
-				} else {
-					$match_args['match_day'] = $round;
+				if ( $match_day ) {
+					$match_args['match_day'] = $match_day;
 				}
-			}
-			if ( $season ) {
-				$match_args['season'] = $season;
-			}
-			if ( $leg ) {
-				$match_args['leg'] = $leg;
-			}
-			$matches = $league->get_matches( $match_args );
-			if ( ! $matches ) {
-				return esc_html_e( 'Match not found', 'racketmanager' );
-			}
-			$num_matches = count( $matches );
-			if ( 1 === $num_matches ) {
-				$match_id = $matches[0]->id;
-			}
-			if ( $player ) {
-				$match->player = get_player( $player );
+				if ( $round ) {
+					if ( $league->is_championship ) {
+						$match_args['final'] = $round;
+					} else {
+						$match_args['match_day'] = $round;
+					}
+				}
+				if ( $season ) {
+					$match_args['season'] = $season;
+				}
+				if ( $leg ) {
+					$match_args['leg'] = $leg;
+				}
+				$matches = $league->get_matches( $match_args );
+				if ( ! $matches ) {
+					return esc_html_e( 'Match not found', 'racketmanager' );
+				}
+				$num_matches = count( $matches );
+				if ( 1 === $num_matches ) {
+					$match_id = $matches[0]->id;
+				}
+				if ( $player ) {
+					$match->player = get_player( $player );
+				}
 			}
 		}
 		if ( $match_id ) {
@@ -739,6 +744,8 @@ class Racketmanager_Shortcodes_Competition extends Racketmanager_Shortcodes {
 			if ( ! empty( $match->league->num_rubbers ) ) {
 				$template = 'teams';
 			} elseif ( 'tournament' === $match->league->event->competition->type ) {
+				$template = 'tournament';
+			} elseif ( $match->league->event->is_box ) {
 				$template = 'tournament';
 			}
 			if ( empty( $template ) && $this->check_template( 'match-' . $match->league->sport ) ) {

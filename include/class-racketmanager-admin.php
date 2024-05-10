64c297191d7b882ad7118f701e77806988c65f43
brunswickcomputing <paul@brunswickcomputing.co.uk>
2024-05-10 15:17:06 +0100

    remove debug statements

--- include/class-racketmanager-admin.php
+++ include/class-racketmanager-admin.php
@@ -3814,8 +3814,6 @@ final class RacketManager_Admin extends RacketManager {
 				$options['checks']['playedRounds']             = isset( $_POST['playedRounds'] ) ? intval( $_POST['playedRounds'] ) : null;
 				$options['checks']['playerLocked']             = isset( $_POST['playerLocked'] ) ? sanitize_text_field( wp_unslash( $_POST['playerLocked'] ) ) : null;
 				$competition_types                             = Racketmanager_Util::get_competition_types();
-				debug_to_console( $options['checks']['leadTimeCheck'] );
-				debug_to_console( $options['checks']['rosterLeadTime'] );
 				foreach ( $competition_types as $competition_type ) {
 					$options[ $competition_type ]['matchCapability']         = isset( $_POST[ $competition_type ]['matchCapability'] ) ? sanitize_text_field( wp_unslash( $_POST[ $competition_type ]['matchCapability'] ) ) : null;
 					$options[ $competition_type ]['resultConfirmation']      = isset( $_POST[ $competition_type ]['resultConfirmation'] ) ? sanitize_text_field( wp_unslash( $_POST[ $competition_type ]['resultConfirmation'] ) ) : null;

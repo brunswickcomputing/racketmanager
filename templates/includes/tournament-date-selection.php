c02c17f9b2b0cd6694af146ff084eb8305d280e8
Paul Moffat <paul@brunswickcomputing.co.uk>
2024-06-25 14:52:55 +0100

    separate out date selection and show select on small screens

---
+++ templates/includes/tournament-date-selection.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template for tournament matches
+ *
+ * @package Racketmanager/Templates
+ */
+
+namespace Racketmanager;
+
+?>
+
+<div class="form-wrapper">
+    <div class="col-6">
+        <form id="tournament-match-date-form" action="">
+            <input type="hidden" name="tournament_id" id="tournament_id" value="<?php echo esc_html( $tournament->name ); ?>" />
+            <select class="form-select" name="match_date" id="match_date">
+                <?php
+                foreach ( $tournament->match_dates as $match_date ) {
+                    ?>
+                    <option value="<?php echo esc_html( $match_date ); ?>" <?php selected( $current_match_date, $match_date ); ?>>
+                        <?php echo esc_html( mysql2date( 'D j M', $match_date ) ); ?>
+                    </option>
+                    <?php
+                }
+                ?>
+            </select>
+        </form>
+    </div>
+</div>
--- templates/tournament/matches.php
+++ templates/tournament/matches.php
@@ -22,53 +22,39 @@ namespace Racketmanager;
 				<div class="container">
 					<?php
 					if ( count( $tournament->match_dates ) > 8 ) {
+						require RACKETMANAGER_PATH . 'templates/includes/tournament-date-selection.php';
 						?>
-						<div class="form-wrapper">
-							<div class="col-6">
-								<form id="tournament-match-date-form" action="">
-									<input type="hidden" name="tournament_id" id="tournament_id" value="<?php echo esc_html( $tournament->name ); ?>" />
-									<select class="form-select" name="match_date" id="match_date">
-										<?php
-										foreach ( $tournament->match_dates as $match_date ) {
-											?>
-											<option value="<?php echo esc_html( $match_date ); ?>" <?php selected( $current_match_date, $match_date ); ?>>
-												<?php echo esc_html( mysql2date( 'D j M', $match_date ) ); ?>
-											</option>
-											<?php
-										}
-										?>
-									</select>
-								</form>
-							</div>
-						</div>
 						<?php
 					} else {
 						?>
-						<ul class="nav nav-tabs frontend match-date-list" id="match_date_tabs" role="tablist">
-						<?php
-						foreach ( $tournament->match_dates as $match_date ) {
-							if ( $current_match_date === $match_date ) {
-								$selected_class = 'is_selected';
-							} else {
-								$selected_class = '';
+						<div class="d-block d-md-none">
+							<?php require RACKETMANAGER_PATH . 'templates/includes/tournament-date-selection.php'; ?>
+						</div>
+						<ul class="d-none d-md-flex nav nav-tabs frontend match-date-list" id="match_date_tabs" role="tablist">
+							<?php
+							foreach ( $tournament->match_dates as $match_date ) {
+								if ( $current_match_date === $match_date ) {
+									$selected_class = 'is_selected';
+								} else {
+									$selected_class = '';
+								}
+								?>
+								<li class="nav-item nav-link <?php echo esc_html( $selected_class ); ?>" role="presentation">
+									<a href="/tournament/<?php echo esc_html( seo_url( $tournament->name ) ); ?>/matches/<?php echo esc_html( $match_date ); ?>/" data-value="<?php echo esc_html( $match_date ); ?>" class="nav-link__value">
+										<span class="date__weekday">
+											<?php echo esc_html( mysql2date( 'D', $match_date ) ); ?>
+										</span>
+										<span class="date__day">
+											<?php echo esc_html( mysql2date( 'j', $match_date ) ); ?>
+										</span>
+										<span class="date__month">
+											<?php echo esc_html( mysql2date( 'M', $match_date ) ); ?>
+										</span>
+									</a>
+								</li>
+								<?php
 							}
 							?>
-							<li class="nav-item nav-link <?php echo esc_html( $selected_class ); ?>" role="presentation">
-								<a href="/tournament/<?php echo esc_html( seo_url( $tournament->name ) ); ?>/matches/<?php echo esc_html( $match_date ); ?>/" data-value="<?php echo esc_html( $match_date ); ?>" class="nav-link__value">
-									<span class="date__weekday">
-										<?php echo esc_html( mysql2date( 'D', $match_date ) ); ?>
-									</span>
-									<span class="date__day">
-										<?php echo esc_html( mysql2date( 'j', $match_date ) ); ?>
-									</span>
-									<span class="date__month">
-										<?php echo esc_html( mysql2date( 'M', $match_date ) ); ?>
-									</span>
-								</a>
-							</li>
-							<?php
-						}
-						?>
 						</ul>
 						<?php
 					}

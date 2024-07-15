fb6d9616303fb1a520bc76eec1f13015bd9c8876
Paul Moffat <paul@brunswickcomputing.co.uk>
2024-07-15 11:22:02 +0100

    new file

---
+++ templates/includes/match-modal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template for match modal
+ *
+ * @package Racketmanager/Templates
+ */
+
+namespace Racketmanager;
+
+?>
+<div class="modal" id="matchModal" tabindex="-1">
+	<div class="modal-dialog modal-dialog-centered modal-lg">
+		<div class="modal-content">
+			<div class="modal-header modal__header"></div>
+			<div class="modal-body"></div>
+			<div class="modal-footer"></div>
+		</div>
+	</div>
+</div>

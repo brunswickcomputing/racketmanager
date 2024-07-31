922de32e69d72ac7cf46291b3405902b0ca59130
Paul Moffat <paul@brunswickcomputing.co.uk>
2024-07-31 20:34:26 +0100

    use withdrawn flag

--- templates/includes/matches-teams-match.php
+++ templates/includes/matches-teams-match.php
@@ -91,7 +91,7 @@ if ( empty( $match->winner_id ) ) {
 				<span class="nav--link">
 					<span class="nav-link__value">
 						<?php
-						if ( $match->teams['home']->is_withdrawn ) {
+						if ( ! empty( $match->teams['home']->is_withdrawn ) ) {
 							$title_text = $match->teams['home']->title . ' ' . __( 'has withdrawn', 'racketmanager' );
 							?>
 							<s aria-label="<?php echo esc_attr( $title_text ); ?>" data-bs-toggle="tooltip" data-bs-placement="right" title="<?php echo esc_attr( $title_text ); ?>">
@@ -100,7 +100,7 @@ if ( empty( $match->winner_id ) ) {
 						?>
 						<?php echo esc_html( $match->teams['home']->title ); ?>
 						<?php
-						if ( $match->teams['home']->is_withdrawn ) {
+						if ( ! empty( $match->teams['home']->is_withdrawn ) ) {
 							?>
 							</s> 
 							<?php
@@ -133,7 +133,7 @@ if ( empty( $match->winner_id ) ) {
 				<span class="nav--link">
 					<span class="nav-link__value">
 						<?php
-						if ( $match->teams['away']->is_withdrawn ) {
+						if ( ! empty( $match->teams['away']->is_withdrawn ) ) {
 							$title_text = $match->teams['away']->title . ' ' . __( 'has withdrawn', 'racketmanager' );
 							?>
 							<s aria-label="<?php echo esc_attr( $title_text ); ?>" data-bs-toggle="tooltip" data-bs-placement="left" title="<?php echo esc_attr( $title_text ); ?>">
@@ -142,7 +142,7 @@ if ( empty( $match->winner_id ) ) {
 						?>
 						<?php echo esc_html( $match->teams['away']->title ); ?>
 						<?php
-						if ( $match->teams['away']->is_withdrawn ) {
+						if ( ! empty( $match->teams['away']->is_withdrawn ) ) {
 							?>
 							</s> 
 							<?php

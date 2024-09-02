10f785c5a89ac2da573643bbc9ec5e6de812e996
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2024-09-02 17:41:51 +0100

    * UPDATE: add reset password modal

--- templates/forms/login-page.php
+++ templates/forms/login-page.php
@@ -8,6 +8,12 @@
 namespace Racketmanager;
 
 ?>
+<div class="alert_rm mt-3" id="loginAlert" style="display:none;">
+	<div class="alert__body">
+		<div class="alert__body-inner" id="loginAlertResponse">
+		</div>
+	</div>
+</div>
 <form method="post" action="<?php echo esc_url( wp_login_url() ); ?>">
 	<?php wp_nonce_field( 'racketmanager_login', 'racketmanager_login_nonce' ); ?>
 	<div class="mt-3">
@@ -63,7 +69,7 @@ namespace Racketmanager;
 	</div>
 	<div class="row">
 		<div class="col-6">
-			<a class="forgot-password" href="<?php echo esc_url( wp_lostpassword_url() ); ?>">
+			<a class="forgot-password" href="<?php echo esc_url( wp_lostpassword_url() ); ?>" data-bs-toggle="modal" data-bs-target="#resetPasswordModal">
 				<?php esc_html_e( 'Forgot your password?', 'racketmanager' ); ?>
 			</a>
 		</div>
@@ -77,3 +83,53 @@ namespace Racketmanager;
 		</div>
 	</div>
 </form>
+<!-- Modal -->
+<div class="modal fade" id="resetPasswordModal" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form id="lostpasswordform" action="<?php echo esc_url( wp_lostpassword_url() ); ?>" method="post">
+				<?php wp_nonce_field( 'reset_password', 'racketmanager_nonce' ); ?>
+				<div class="modal-header modal__header">
+					<h2 class="modal-title"><?php esc_html_e( 'Reset password', 'racketmanager' ); ?></h2>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<div class="form-info"><?php esc_html_e( "Enter your email address and click 'Send'. You will receive a link to create a new password", 'racketmanager' ); ?></div>
+					<div class="alert_rm" id="resetAlert" style="display:none;">
+						<div class="alert__body">
+							<div class="alert__body-inner" id="resetAlertResponse">
+							</div>
+						</div>
+					</div>
+					<div class="form-floating mb-3">
+						<input type="email" class="form-control
+						<?php
+						if ( $email_error ) {
+							echo ' is-invalid';
+						}
+						?>
+						" placeholder="<?php esc_html_e( 'Email', 'racketmanager' ); ?>" name="user_login" id="user_login" aria-describedby="emailFeedback" />
+						<label class="" for="user_login"><?php esc_html_e( 'Email', 'racketmanager' ); ?></label>
+						<div id="emailFeedback" class="
+						<?php
+						if ( $email_error ) {
+							echo 'invalid-feedback';
+						}
+						?>
+						">
+							<?php
+							if ( $email_error ) {
+								echo esc_html( $email_message );
+							}
+							?>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-plain" data-bs-dismiss="modal"><?php esc_html_e( 'Cancel', 'racketmanager' ); ?></button>
+					<button type="button" class="btn btn-primary" onclick="Racketmanager.resetPassword(this)"><?php esc_html_e( 'Send', 'racketmanager' ); ?></button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>

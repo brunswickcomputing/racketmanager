e9f638e7a76c962d264013ae487eefa3c0042daf
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2024-09-10 19:24:58 +0100

    use header file

--- templates/competition.php
+++ templates/competition.php
@@ -22,20 +22,6 @@ if ( empty( $tab ) ) {
 		$tab = 'overview'; //phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 	}
 }
-switch ( $competition->type ) {
-	case 'league':
-		$image = 'images/bootstrap-icons.svg#table';
-		break;
-	case 'cup':
-		$image = 'images/bootstrap-icons.svg#trophy-fill';
-		break;
-	case 'tournament':
-		$image = 'images/lta-icons.svg#icon-bracket';
-		break;
-	default:
-		$image = null;
-		break;
-}
 ?>
 <script type="text/javascript">
 var tab = '<?php echo esc_html( $tab ); ?>;'
@@ -43,75 +29,8 @@ jQuery(function() {
 	activaTab('<?php echo esc_html( $tab ); ?>');
 });
 </script>
+<?php require 'includes/competition-header.php'; ?>
 <div class="container">
-	<div class="page-subhead competition">
-		<div class="media competition-head">
-			<div class="media__wrapper">
-				<div class="media__img">
-					<svg width="16" height="16" class="media__img-element--icon">
-						<use xlink:href="<?php echo esc_url( RACKETMANAGER_URL . $image ); ?>"></use>
-					</svg>
-				</div>
-				<div class="media__content">
-					<h1 class="media__title"><?php echo esc_html( $competition->name ); ?></h1>
-					<div class="media__content-subinfo">
-						<?php
-						if ( ! empty( $competition_season['venue_name'] ) ) {
-							?>
-							<small class="media__subheading">
-								<span class="nav--link">
-									<span class="nav-link__value">
-										<?php echo esc_html( $competition_season['venue_name'] ); ?>
-									</span>
-								</span>
-							</small>
-							<?php
-						}
-						?>
-						<?php
-						if ( ! empty( $competition_season['dateStart'] ) && ! empty( $competition_season['dateEnd'] ) ) {
-							?>
-							<small class="media__subheading">
-								<span class="nav--link">
-									<span class="nav-link__value">
-										<?php racketmanager_the_svg( 'icon-calendar' ); ?>
-										<?php echo esc_html( mysql2date( 'j M', $competition_season['dateStart'] ) ); ?> <?php esc_html_e( 'to', 'racketmanager' ); ?> <?php echo esc_html( mysql2date( 'j M', $competition_season['dateEnd'] ) ); ?>
-									</span>
-								</span>
-							</small>
-							<?php
-						}
-						?>
-					</div>
-				</div>
-				<div class="media__aside">
-					<form method="get" action="<?php echo esc_html( get_permalink( $post_id ) ); ?>" id="racketmanager_competititon_archive" class="season-select">
-						<input type="hidden" name="page_id" value="<?php echo esc_html( $post_id ); ?>" />
-						<input type="hidden" name="pagename" id="pagename" value="<?php echo esc_html( $pagename ); ?>" />
-						<div class="row g-1 align-items-center">
-							<div class="col-md">
-								<div class="form-floating">
-									<select class="form-select" size="1" name="season" id="season">
-										<?php
-										foreach ( array_reverse( $competition->seasons ) as $key => $season ) {
-											$option_name = $season['name'];
-											?>
-											<option value="<?php echo esc_html( $season['name'] ); ?>" <?php selected( $season['name'], $competition_season['name'] ); ?>>
-												<?php echo esc_html( $option_name ); ?>
-											</option>
-											<?php
-										}
-										?>
-									</select>
-									<label for="season"><?php esc_html_e( 'Seasons', 'racketmanager' ); ?></label>
-								</div>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
 		<nav class="navbar navbar-expand-lg">
 			<div class="">
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

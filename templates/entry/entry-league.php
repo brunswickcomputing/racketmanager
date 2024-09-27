bfc1bde1a6ac25399fb4b22a7562be576640ccb2
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2024-09-27 16:17:36 +0100

    remove code warning

--- templates/entry/entry-league.php
+++ templates/entry/entry-league.php
@@ -253,7 +253,7 @@ $match_days = Racketmanager_Util::get_match_days();
 									printf( __( 'I agree to abide by %s.', 'racketmanager' ), $rules_link ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 									?>
 								</label>
-								<input class="form-check-input switch" id="acceptance" name="acceptance" type="checkbox" role="switch">
+								<input class="form-check-input switch" id="acceptance" name="acceptance" type="checkbox" role="switch" aria-checked="false">
 							</div>
 						</div>
 					</div>

daef9bcc32d308a2ea7aa8d789c11f2ea23f2090
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2024-12-02 14:49:19 +0000

    create event matches template

---
+++ templates/event/matches.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template for event matches
+ *
+ * @package Racketmanager/Templates
+ */
+
+namespace Racketmanager;
+
+?>
+<div class="module module--card">
+	<div class="module__banner">
+		<h3 class="module__title"><?php esc_html_e( 'Matches', 'racketmanager' ); ?></h3>
+	</div>
+	<div class="module__content">
+		<div class="module-container">
+			<?php
+			foreach ( $event->leagues as $league ) {
+				?>
+				<h4 class="header">
+					<?php echo esc_html( $league->title ); ?>
+				</h4>
+				<?php
+				$finals   = $league->finals;
+				$champion = null;
+				require RACKETMANAGER_PATH . 'templates/championship-matches.php';
+				?>
+				<?php
+			}
+			?>
+		</div>
+	</div>
+</div>

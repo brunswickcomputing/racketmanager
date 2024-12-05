583815129a61c6f1bd9f527ed0060d175253dd69
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2024-12-05 12:13:03 +0000

    remove code warnings

--- templates/includes/loading.php
+++ templates/includes/loading.php
@@ -1,9 +1,19 @@
-		<div id="splash" class="container d-none">
-			<div class="module module--card">
-				<div class="spinner-loading d-flex justify-content-center">
-					<div class="spinner-border" role="status">
-					<span class="visually-hidden">Loading...</span>
-					</div>
-				</div>
+<?php
+/**
+ * Template page for loading view
+ *
+ * @package Racketmanager/Templates
+ */
+
+namespace Racketmanager;
+
+?>
+<div id="splash" class="container d-none">
+	<div class="module module--card">
+		<div class="spinner-loading d-flex justify-content-center">
+			<div class="spinner-border" role="status">
+			<span class="visually-hidden">Loading...</span>
 			</div>
 		</div>
+	</div>
+</div>

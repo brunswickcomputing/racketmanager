a4d95aa0bf7363057b4f5d763b79221e6db15e8b
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-01-03 15:29:52 +0000

    handle number of rounds correctly before championship active

--- include/class-racketmanager-championship.php
+++ include/class-racketmanager-championship.php
@@ -170,6 +170,13 @@ final class Racketmanager_Championship extends RacketManager {
 				);
 				$this->num_advance = pow( 2, $league->current_season['num_match_days'] );
 				if ( $league->event->competition->is_active && $num_teams ) {
+					$use_teams = true;
+				} elseif ( $num_teams > $this->num_advance ) {
+					$use_teams = true;
+				} else {
+					$use_teams = false;
+				}
+				if ( $use_teams ) {
 					$this->num_rounds            = ceil( log( $num_teams, 2 ) );
 					$this->num_teams_first_round = pow( 2, $this->num_rounds );
 				} else {

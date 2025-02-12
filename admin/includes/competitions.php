dc4e83fb1239ce77d547b8f30a308ff98ba288c1
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-02-12 18:16:37 +0000

    allow filtering by age group

--- admin/includes/competitions.php
+++ admin/includes/competitions.php
@@ -7,8 +7,36 @@
 
 namespace Racketmanager;
 
-$competitions = $this->get_competitions( $competition_query );
+$age_group_select               = isset( $_GET['age_group'] ) ? sanitize_text_field( wp_unslash( $_GET['age_group'] ) ) : '';
+$competition_query['age_group'] = isset( $age_group_select ) ? $age_group_select : null;
+$orderby['age_group']           = 'ASC';
+$orderby['type']                = 'ASC';
+$orderby['name']                = 'ASC';
+$competition_query['orderby']   = $orderby;
+$competitions                   = $this->get_competitions( $competition_query );
+$age_groups                     = Racketmanager_Util::get_age_groups();
+$page_name                      = isset( $_GET['page'] ) ? sanitize_text_field( wp_unslash( $_GET['page'] ) ) : 'racketmanager';
 ?>
+<div class="container">
+	<div class="row justify-content-between mb-3">
+		<form id="competitions-list-filter" method="get" action="" class="form-control">
+			<input type="hidden" name="page" value="<?php echo esc_attr( $page_name ); ?>" />
+			<div class="col-auto">
+				<select class="form-select-1" name="age_group" id="age_group">
+					<option value=""><?php esc_html_e( 'All age groups', 'racketmanager' ); ?></option>
+					<?php
+					foreach ( $age_groups as $age_group => $age_group_desc ) {
+						?>
+						<option value="<?php echo esc_attr( $age_group ); ?>" <?php selected( $age_group, $age_group_select ); ?>><?php echo esc_html( $age_group_desc ); ?></option>
+						<?php
+					}
+					?>
+				</select>
+				<button class="btn btn-primary"><?php esc_html_e( 'Filter', 'racketmanager' ); ?></button>
+			</div>
+		</form>
+	</div>
+</div>
 <div class="form-control" id="competitions">
 	<form id="competitions-filter" method="post" action="">
 		<?php wp_nonce_field( 'competitions-bulk' ); ?>

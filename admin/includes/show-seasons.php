10f3b8610cc90453f329c7c8c10981c3adf5fd99
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-03-07 09:38:53 +0000

    use table for layout

--- admin/includes/show-seasons.php
+++ admin/includes/show-seasons.php
@@ -8,42 +8,41 @@
 namespace Racketmanager;
 
 ?>
-<div>
+<div class="container">
 	<div class="alert_rm" id="alert-season" style="display:none;">
 		<div class="alert__body">
 			<div class="alert__body-inner" id="alert-season-response">
 			</div>
 		</div>
 	</div>
-<div class="container">
-	<div class="row justify-content-end">
-		<div class="col-auto racketmanager_breadcrumb">
-			<a href="admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s"><?php echo esc_html( ucfirst( $competition->type ) ); ?>s</a> &raquo; <?php echo esc_html( $competition->name ); ?>
+	<div class="container">
+		<div class="row justify-content-end">
+			<div class="col-auto racketmanager_breadcrumb">
+				<a href="admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s"><?php echo esc_html( ucfirst( $competition->type ) ); ?>s</a> &raquo; <?php echo esc_html( $competition->name ); ?>
+			</div>
+		</div>
+		<div class="row justify-content-between">
+			<div class="col-auto">
+				<h1><?php echo esc_html( $competition->name ); ?></h1>
+			</div>
+		</div>
+		<div class="row mb-3">
+			<nav class="navbar navbar-expand-lg bg-body-tertiary">
+				<ul class="nav nav-pills">
+					<li class="nav-item">
+						<div class="nav-link active" href="#" role="tab"><?php esc_html_e( 'Seasons', 'racketmanager' ); ?></div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s&view=config&competition_id=<?php echo esc_attr( $competition->id ); ?>" type="button" role="tab"><?php esc_html_e( 'Configuration', 'racketmanager' ); ?></a>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	</div>
-	<div class="row justify-content-between">
-		<div class="col-auto">
-			<h1><?php echo esc_html( $competition->name ); ?></h1>
-		</div>
-	</div>
-	<div class="row mb-3">
-		<nav class="navbar navbar-expand-lg bg-body-tertiary">
-			<ul class="nav nav-pills">
-				<li class="nav-item">
-					<div class="nav-link active" href="#" role="tab"><?php esc_html_e( 'Seasons', 'racketmanager' ); ?></div>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s&view=config&competition_id=<?php echo esc_attr( $competition->id ); ?>" type="button" role="tab"><?php esc_html_e( 'Configuration', 'racketmanager' ); ?></a>
-				</li>
-			</ul>
-		</nav>
-	</div>
-
 	<form id="seasons-filter" action="" method="post" class="form-control mb-3">
 		<?php wp_nonce_field( 'seasons-bulk', 'racketmanager_nonce' ); ?>
-
 		<input type="hidden" name="competition_id" value="<?php echo esc_html( $competition_id ); ?>" />
-		<div class="tablenav">
+		<div class="mb-2">
 			<!-- Bulk Actions -->
 			<select name="action" size="1">
 				<option value="-1" selected="selected"><?php esc_html_e( 'Bulk Actions', 'racketmanager' ); ?></option>
@@ -51,64 +50,60 @@ namespace Racketmanager;
 			</select>
 			<input type="submit" value="<?php esc_html_e( 'Apply', 'racketmanager' ); ?>" name="doactionseason" id="doactionseason" class="btn btn-secondary action" />
 		</div>
-
-		<div class=container>
-			<div class="row table-header">
-				<div class="col-1 col-md-auto me-1 check-column"><input type="checkbox" id="check-all-seasons" onclick="Racketmanager.checkAll(document.getElementById('seaons-filter'));" /></div>
-				<div class="col-2 col-lg-1"><?php esc_html_e( 'Season', 'racketmanager' ); ?></div>
-				<div class="col-4 col-lg-2"><?php esc_html_e( 'Start', 'racketmanager' ); ?></div>
-				<div class="col-4 col-lg-1"><?php esc_html_e( 'End', 'racketmanager' ); ?></div>
-			</div>
-			<?php
-			if ( ! empty( $competition->seasons ) ) {
-				$class = '';
+		<table class="table table-striped">
+			<thead class="table-dark">
+				<tr>
+					<th class="check-column"><input type="checkbox" id="check-all-seasons" onclick="Racketmanager.checkAll(document.getElementById('seaons-filter'));" /></th>
+					<th><?php esc_html_e( 'Season', 'racketmanager' ); ?></th>
+					<th><?php esc_html_e( 'Start', 'racketmanager' ); ?></th>
+					<th><?php esc_html_e( 'End', 'racketmanager' ); ?></th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
 				foreach ( array_reverse( $competition->seasons ) as $season ) {
-					$class = ( 'alternate' === $class ) ? '' : 'alternate';
 					$key   = $season['name'];
 					?>
-					<div class="row table-row <?php echo esc_html( $class ); ?>">
-						<div class="col-1 col-md-auto me-1 check-column"><input type="checkbox" value="<?php echo esc_html( $key ); ?>" name="del_season[<?php echo esc_html( $key ); ?>]" /></div>
-						<div class="col-2 col-lg-1"><a href="admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=overview&amp;competition_id=<?php echo esc_html( $competition->id ); ?>&amp;season=<?php echo esc_html( $key ); ?>"><?php echo esc_html( $season['name'] ); ?></a></div>
-						<div class="col-4 col-lg-2"><?php echo empty( $season['date_start'] ) ? null : esc_html( $season['date_start'] ); ?></div>
-						<div class="col-4 col-lg-1"><?php echo empty( $season['date_end'] ) ? null : esc_html( $season['date_end'] ); ?></div>
-						<?php
-						$today = gmdate( 'Y-m-d' );
-						if ( ! empty( $season['date_end'] ) && $today > $season['date_end'] ) {
-							$competition_code = isset( $season->competition_code ) ? $season->competition_code : null;
-							if ( empty( $competition_code ) ) {
-								$competition_code = empty( $competition->competition_code ) ? null : $competition->competition_code;
-							}
-							if ( ! empty( $competition_code ) ) {
-								?>
-								<div class="col-auto">
+					<tr>
+						<td class="check-column"><input type="checkbox" value="<?php echo esc_html( $key ); ?>" name="del_season[<?php echo esc_html( $key ); ?>]" /></td>
+						<td><a href="admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=overview&amp;competition_id=<?php echo esc_html( $competition->id ); ?>&amp;season=<?php echo esc_html( $key ); ?>"><?php echo esc_html( $season['name'] ); ?></a></td>
+						<td><?php echo empty( $season['date_start'] ) ? null : esc_html( $season['date_start'] ); ?></td>
+						<td><?php echo empty( $season['date_end'] ) ? null : esc_html( $season['date_end'] ); ?></td>
+						<td>
+							<?php
+							$today = gmdate( 'Y-m-d' );
+							if ( ! empty( $season['date_end'] ) && $today > $season['date_end'] ) {
+								$competition_code = isset( $season->competition_code ) ? $season->competition_code : null;
+								if ( empty( $competition_code ) ) {
+									$competition_code = empty( $competition->competition_code ) ? null : $competition->competition_code;
+								}
+								if ( ! empty( $competition_code ) ) {
+									?>
 									<a href="/index.php?competition_id=<?php echo esc_html( $competition->id ); ?>&season=<?php echo esc_html( $key ); ?>&competition_code=<?php echo esc_attr( $competition_code ); ?>&racketmanager_export=report_results" class="btn btn-secondary btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php esc_html_e( 'Report results', 'racketmanager' ); ?>" >
 										<span class="nav-link__value text-uppercase">
 											<?php esc_html_e( 'Report results', 'racketmanager' ); ?>
 										</span>
 									</a>
-								</div>
-								<?php
-							}
-						} elseif ( ! empty( $season['date_closing'] ) && $today <= $season['date_closing'] && ! empty( $season['date_open'] ) && $today >= $season['date_open'] ) {
-							?>
-							<div class="col-auto">
-								<button href="" /index.php?competition_id=<?php echo esc_html( $competition->id ); ?>&season=<?php echo esc_html( $key ); ?> class="btn btn-secondary btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php esc_html_e( 'Notify open', 'racketmanager' ); ?>" onclick="Racketmanager.notify_open(event, <?php echo esc_html( $competition->id ) . ',' . esc_html( $key ); ?> )">
+									<?php
+								}
+							} elseif ( ! empty( $season['date_closing'] ) && $today <= $season['date_closing'] && ! empty( $season['date_open'] ) && $today >= $season['date_open'] ) {
+								?>
+								<button href="" /index.php?competition_id=<?php echo esc_html( $competition->id ); ?>&season=<?php echo esc_html( $key ); ?> class="btn btn-info btn-sm text-uppercase" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php esc_html_e( 'Notify open', 'racketmanager' ); ?>" onclick="Racketmanager.notify_open(event, <?php echo esc_html( $competition->id ) . ',' . esc_html( $key ); ?> )">
 									<?php esc_html_e( 'Notify open', 'racketmanager' ); ?>
 								</button>
 								<span class="notifymessage" id="notifyMessage-<?php echo esc_html( $key ); ?>"></span>
-							</div>
-							<?php
-						}
-						?>
-					</div>
+								<?php
+							}
+							?>
+						</td>
+					</tr>
 					<?php
 				}
 				?>
-				<?php
-			}
-			?>
-		</form>
-	</div>
+			</tbody>
+		</table>
+	</form>
 	<div class="mb-3">
 		<!-- Add New Season -->
 		<a href="admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=modify&amp;competition_id=<?php echo esc_attr( $competition->id ); ?>" class="btn btn-primary submit"><?php esc_html_e( 'Add Season', 'racketmanager' ); ?></a>

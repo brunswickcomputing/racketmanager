50d2189a5be1605fb59c392193fdbfbd55982f7c
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-03-07 10:21:02 +0000

    fix indentation

--- templates/competitions.php
+++ templates/competitions.php
@@ -9,31 +9,31 @@ namespace Racketmanager;
 
 ?>
 <div class="container">
-		<div class="module module--card">
-			<div class="module__banner">
-				<?php // translators: %s: type. ?>
-				<h2 class="module__title"><?php printf( esc_html__( 'My %s', 'racketmanager' ), esc_html( strtolower( $type ) ) ); ?></h2>
-			</div>
-			<div class="module__content">
-				<div class="module-container">
-					<ul class="list list--grid list--bordered">
-						<?php
-						if ( empty( $user_competitions ) ) {
-							?>
-							<?php // translators: %s: type. ?>
-							<li class="list__item col-12 text-center"><?php printf( esc_html__( 'This will show %s that you have played in.', 'racketmanager' ), esc_html( strtolower( $type ) ) ); ?></li>
-							<?php
-						} else {
-							$competition_list = $user_competitions;
-							require 'includes/competition-list.php';
-							?>
-							<?php
-						}
+	<div class="module module--card">
+		<div class="module__banner">
+			<?php // translators: %s: type. ?>
+			<h2 class="module__title"><?php printf( esc_html__( 'My %s', 'racketmanager' ), esc_html( strtolower( $type ) ) ); ?></h2>
+		</div>
+		<div class="module__content">
+			<div class="module-container">
+				<ul class="list list--grid list--bordered">
+					<?php
+					if ( empty( $user_competitions ) ) {
 						?>
-					</ul>
-				</div>
+						<?php // translators: %s: type. ?>
+						<li class="list__item col-12 text-center"><?php printf( esc_html__( 'This will show %s that you have played in.', 'racketmanager' ), esc_html( strtolower( $type ) ) ); ?></li>
+						<?php
+					} else {
+						$competition_list = $user_competitions;
+						require 'includes/competition-list.php';
+						?>
+						<?php
+					}
+					?>
+				</ul>
 			</div>
 		</div>
+	</div>
 	<div class="module module--card">
 		<div class="module__banner">
 			<h2 class="module__title"><?php echo esc_html( $type ); ?></h2>

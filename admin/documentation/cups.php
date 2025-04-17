e49415c382c89456bcf634377f4bb37dad3742f9
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-04-17 17:55:09 +0100

    fix php and wordpress coding errors

--- admin/documentation/cups.php
+++ admin/documentation/cups.php
@@ -2,7 +2,7 @@
   <a href="#top" class="alignright top-link"><?php _e( 'Top', 'racketmanager' ) ?></a>
   <p>The cups page shows all the cup competitions that have been created.</p>
   <p>Clicking on cup name takes the user to the cup competition page.</p>
-  <h3 id="notifycups"><?php _e( 'Notify Entry Open', 'racketmanager' ) ?></h3>
+  <h3 id="notifyCups"><?php _e( 'Notify Entry Open', 'racketmanager' ) ?></h3>
   <p>Match secretaries for all clubs can be notified when cup entries are allowed.</p>
-  <p>Selecting the season and cup type (summer or winter) and then pressing the "Notify cup entry open" button sends an email to match secretaries for all clubs with a link the the cup entry form.</p>
+  <p>Selecting the season and cup type (summer or winter) and then pressing the "Notify cup entry open" button sends an email to match secretaries for all clubs with a link the cup entry form.</p>
 </div>

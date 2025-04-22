95476287f475cfb1190e3927c16ed6ff4cbe1be5
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-04-22 17:03:20 +0100

    fix php and wordpress coding errors

--- admin/league/show-schedule.php
+++ admin/league/show-schedule.php
@@ -7,11 +7,15 @@
 
 namespace Racketmanager;
 
+global $racketmanager;
+/** @var object $competition */
+/** @var string $season */
+/** @var int $competition_id */
 ?>
 <div class="container">
 	<div class="row justify-content-end">
 		<div class="col-auto racketmanager_breadcrumb">
-			<a href="admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s"><?php echo esc_html( ucfirst( $competition->type ) ); ?>s</a> &raquo; <a href="admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s&amp;view=seasons&amp;competition_id=<?php echo esc_attr( $competition->id ); ?>"><?php echo esc_html( $competition->name ); ?></a> &raquo; <a href="admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s&amp;view=overview&amp;competition_id=<?php echo esc_attr( $competition->id ); ?>&amp;season=<?php echo esc_attr( $season ); ?>"><?php echo esc_html( $season ); ?></a> &raquo; <?php echo esc_html_e( 'Schedule', 'racketmanager' ); ?>
+			<a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s"><?php echo esc_html( ucfirst( $competition->type ) ); ?>s</a> &raquo; <a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s&amp;view=seasons&amp;competition_id=<?php echo esc_attr( $competition->id ); ?>"><?php echo esc_html( $competition->name ); ?></a> &raquo; <a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_html( $competition->type ); ?>s&amp;view=overview&amp;competition_id=<?php echo esc_attr( $competition->id ); ?>&amp;season=<?php echo esc_attr( $season ); ?>"><?php echo esc_html( $season ); ?></a> &raquo; <?php esc_html_e( 'Schedule', 'racketmanager' ); ?>
 		</div>
 	</div>
 	<div class="row justify-content-between">
@@ -35,12 +39,14 @@ namespace Racketmanager;
 			<div class="row gx-3 mb-2 align-items-center">
 				<!-- Bulk Actions -->
 				<div class="col-auto">
-					<select class="form-select" name="actionSchedule">
-						<option value="-1" disabled selected><?php esc_html_e( 'Bulk Actions', 'racketmanager' ); ?></option>
-						<option value="schedule"><?php esc_html_e( 'Schedule matches', 'racketmanager' ); ?></option>
-						<option value="delete"><?php esc_html_e( 'Delete matches', 'racketmanager' ); ?></option>
-					</select>
-				</div>
+                    <label>
+                        <select class="form-select" name="actionSchedule">
+                            <option value="-1" disabled selected><?php esc_html_e( 'Bulk Actions', 'racketmanager' ); ?></option>
+                            <option value="schedule"><?php esc_html_e( 'Schedule matches', 'racketmanager' ); ?></option>
+                            <option value="delete"><?php esc_html_e( 'Delete matches', 'racketmanager' ); ?></option>
+                        </select>
+                    </label>
+                </div>
 				<div class="col-auto">
 					<button name="scheduleAction" id="scheduleAction" class="btn btn-secondary"><?php esc_html_e( 'Apply', 'racketmanager' ); ?></button>
 				</div>
@@ -48,7 +54,7 @@ namespace Racketmanager;
 			<table class="table table-striped">
 				<thead class="table-dark">
 					<tr>
-						<th class="check-column"><input type="checkbox" onclick="Racketmanager.checkAll(document.getElementById('schedule-filter'));" /></th>
+						<th class="check-column"><label for="event-all"></label><input type="checkbox" id="event-all" onclick="Racketmanager.checkAll(document.getElementById('schedule-filter'));" /></th>
 						<th class=""><?php esc_html_e( 'Event', 'racketmanager' ); ?></th>
 						<th class=""></th>
 					</tr>
@@ -77,13 +83,13 @@ namespace Racketmanager;
 							$event_link             = 'admin.php?page=racketmanager-' . $competition->type . 's&amp;view=event&amp;competition_id=' . $competition->id . '&amp;event_id=' . $event->id . '&amp;season=' . $event->get_season();
 							?>
 							<tr>
-								<td class="check-column"><input type="checkbox" value="<?php echo esc_html( $event->id ); ?>" id="event-<?php echo esc_attr( $event->id ); ?>" name="event[<?php echo esc_html( $event->id ); ?>]" onclick="Racketmanager.checkAll(document.getElementById('schedule-filter'));" /></td>
+								<td class="check-column"><label for="event-<?php echo esc_attr( $event->id ); ?>"></label><input type="checkbox" value="<?php echo esc_html( $event->id ); ?>" id="event-<?php echo esc_attr( $event->id ); ?>" name="event[<?php echo esc_html( $event->id ); ?>]" onclick="Racketmanager.checkAll(document.getElementById('schedule-filter'));" /></td>
 								<td><a href="<?php echo esc_html( $event_link ); ?>"><?php echo esc_html( $event->name ); ?></a></td>
 								<td>
 									<?php
 									if ( ! empty( $match_count ) ) {
 										?>
-										<a class="btn btn-secondary" href="admin.php?page=racketmanager&amp;subpage=show-event&amp;event_id=<?php echo esc_html( $event->id ); ?>&amp;view=matches"><?php esc_html_e( 'View matches', 'racketmanager' ); ?></a>
+										<a class="btn btn-secondary" href="/wp-admin/admin.php?page=racketmanager&amp;subpage=show-event&amp;event_id=<?php echo esc_html( $event->id ); ?>&amp;view=matches"><?php esc_html_e( 'View matches', 'racketmanager' ); ?></a>
 										<?php
 										if ( empty( $match_completion_count ) ) {
 											?>

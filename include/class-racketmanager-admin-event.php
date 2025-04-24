ecc5e47b0b43df12dfbed01826e84fdeec490e5b
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-04-24 11:49:53 +0100

    fix php and wordpress coding errors

--- include/class-racketmanager-admin-event.php
+++ include/class-racketmanager-admin-event.php
@@ -29,7 +29,7 @@ final class RacketManager_Admin_Event extends RacketManager_Admin {
 	 *
 	 * @return void
 	 */
-	public function display_config_page() {
+	public function display_config_page(): void {
 		global $racketmanager;
 		if ( ! current_user_can( 'edit_leagues' ) ) {
 			$racketmanager->set_message( __( 'You do not have sufficient permissions to access this page', 'racketmanager' ), true );
@@ -105,10 +105,8 @@ final class RacketManager_Admin_Event extends RacketManager_Admin {
 				if ( isset( $_POST['addEventConfig'] ) ) {
 					if ( ! isset( $_POST['racketmanager_nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['racketmanager_nonce'] ) ), 'racketmanager_manage-event-config' ) ) {
 						$racketmanager->set_message( __( 'Security token invalid', 'racketmanager' ), true );
-						$racketmanager->printMessage();
 					} elseif ( ! empty( $_POST['event_id'] ) ) {
 						$racketmanager->set_message( __( 'Event id invalid for new event', 'racketmanager' ), true );
-						$racketmanager->printMessage();
 					} else {
 						$config                     = new \stdClass();
 						$config->name               = isset( $_POST['event_name'] ) ? sanitize_text_field( wp_unslash( $_POST['event_name'] ) ) : null;
@@ -130,48 +128,42 @@ final class RacketManager_Admin_Event extends RacketManager_Admin {
 							$event->num_rubbers    = $config->num_rubbers;
 							$event->type           = $config->type;
 							$event                 = new RacketManager_Event( $event );
-							if ( $event ) {
-								?>
-								<script>
-								let url = new URL(window.location.href);
-								url.searchParams.append('event_id', <?php echo esc_attr( $event->id ); ?>);
-								history.pushState('', '', url.toString());
-								</script>
-								<?php
-								$event_season['name']           = $competition->current_season['name'];
-								$event_season['home_away']      = $competition->current_season['home_away'];
-								$event_season['num_match_days'] = $competition->current_season['num_match_days'];
-								$event_season['match_dates']    = $competition->current_season['match_dates'];
-								$event->add_season( $event_season );
-								$league_id = $event->add_league( $event->name );
-								if ( $league_id ) {
-									if ( $competition->is_championship ) {
-										$config->primary_league = $league_id;
-										$league_title = $event->name . ' ' . __( 'Plate', 'racketmanger' );
-										$event->add_league( $league_title );
-									}
+							?>
+                            <script>
+							let url = new URL(window.location.href);
+							url.searchParams.append('event_id', <?php echo esc_attr( $event->id ); ?>);
+							history.pushState('', '', url.toString());
+							</script>
+							<?php
+							$event_season['name']           = $competition->current_season['name'];
+							$event_season['home_away']      = $competition->current_season['home_away'];
+							$event_season['num_match_days'] = $competition->current_season['num_match_days'];
+							$event_season['match_dates']    = $competition->current_season['match_dates'];
+							$event->add_season( $event_season );
+							$league_id = $event->add_league( $event->name );
+							if ( $league_id ) {
+								if ( $competition->is_championship ) {
+									$config->primary_league = $league_id;
+									$league_title = $event->name . ' ' . __( 'Plate', 'racketmanager' );
+									$event->add_league( $league_title );
 								}
-								$event->config = $config;
-								$updates       = $event->set_config( $config );
-								if ( $updates ) {
-									$racketmanager->set_message( __( 'Event config updated', 'racketmanager' ) );
-								} elseif ( empty( $racketmanager->error_messages ) ) {
-									$racketmanager->set_message( __( 'No updates found', 'racketmanager' ), 'warning' );
-								} else {
-									$racketmanager->set_message( __( 'Errors found', 'racketmanager' ), true );
-								}
-								$racketmanager->printMessage();
+							}
+							$event->config = $config;
+							$updates       = $event->set_config( $config );
+							if ( $updates ) {
+								$racketmanager->set_message( __( 'Event config updated', 'racketmanager' ) );
+							} elseif ( empty( $racketmanager->error_messages ) ) {
+								$racketmanager->set_message( __( 'No updates found', 'racketmanager' ), 'warning' );
 							} else {
-								$racketmanager->set_message( __( 'Event creating event', 'racketmanager' ), true );
-								$racketmanager->printMessage();
+								$racketmanager->set_message( __( 'Errors found', 'racketmanager' ), true );
 							}
 						} else {
 							$racketmanager->error_messages[] = __( 'Name must be set', 'racketmanager' );
 							$racketmanager->error_fields[]   = 'name';
 							$racketmanager->set_message( __( 'Error creating event', 'racketmanager' ), true );
-							$racketmanager->printMessage();
 						}
 					}
+					$racketmanager->printMessage();
 				}
 			}
 			if ( empty( $tab ) ) {
@@ -180,32 +172,5 @@ final class RacketManager_Admin_Event extends RacketManager_Admin {
 			$is_invalid = false;
 			require RACKETMANAGER_PATH . 'admin/includes/event-config.php';
 		}
-	}
-	/**
-	 * Set match dates function
-	 *
-	 * @param object $season season details.
-	 * @return array of match dates.
-	 */
-	private function set_match_dates( $season ) {
-		$match_dates  = array();
-		$date_start   = $season->date_start;
-		$round_length = $season->round_length;
-		if ( $season->home_away ) {
-			$halfway = $season->num_match_days / 2;
-		} else {
-			$halfway = null;
-		}
-		for ( $i = 0; $i < $season->num_match_days; ++$i ) {
-			if ( $i === $halfway ) {
-				if ( $season->home_away_diff ) {
-					$days_diff  = $season->home_away_diff * 7;
-					$date_start = Racketmanager_Util::amend_date( $date_start, $days_diff );
-				}
-			}
-			$match_dates[ $i ] = $date_start;
-			$date_start        = Racketmanager_Util::amend_date( $date_start, $round_length );
-		}
-		return $match_dates;
 	}
 }

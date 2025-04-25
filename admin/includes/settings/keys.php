2d50600be5ead5c2965b526064d3e62b69726984
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-04-25 14:03:51 +0100

    fix php and wordpress coding errors

--- admin/includes/settings/keys.php
+++ admin/includes/settings/keys.php
@@ -1,14 +1,14 @@
 <div class="form-control">
-  <div class="form-floating mb-3">
-    <input type="text" class="form-control" name='googleMapsKey' id='googleMapsKey' value='<?php echo isset($options['keys']['googleMapsKey']) ? $options['keys']['googleMapsKey'] : '' ?>' />
-    <label for='googleMapsKey'><?php _e( 'Google Maps Key', 'racketmanager' ) ?></label>
-  </div>
-  <div class="form-floating mb-3">
-    <input type="text" class="form-control" name='recaptchaSiteKey' id='recaptchaSiteKey' value='<?php echo isset($options['keys']['recaptchaSiteKey']) ? $options['keys']['recaptchaSiteKey'] : '' ?>' />
-    <label for='recaptchaSiteKey'><?php _e( 'Recaptcha Site Key', 'racketmanager' ) ?></label>
-  </div>
-  <div class="form-floating mb-3">
-    <input type="text" class="form-control" name='recaptchaSecretKey' id='recaptchaSecretKey' value='<?php echo isset($options['keys']['recaptchaSecretKey']) ? $options['keys']['recaptchaSecretKey'] : '' ?>' />
-    <label for='recaptchaSecretKey'><?php _e( 'Recaptcha Secret Key', 'racketmanager' ) ?></label>
-  </div>
+    <div class="form-floating mb-3">
+        <input type="text" class="form-control" name='googleMapsKey' id='googleMapsKey' value="<?php echo $options['keys']['googleMapsKey'] ?? '' ?>" />
+        <label for='googleMapsKey'><?php _e( 'Google Maps Key', 'racketmanager' ) ?></label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="text" class="form-control" name='recaptchaSiteKey' id='recaptchaSiteKey' value="<?php echo $options['keys']['recaptchaSiteKey'] ?? '' ?>" />
+        <label for='recaptchaSiteKey'><?php _e( 'Recaptcha Site Key', 'racketmanager' ) ?></label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="text" class="form-control" name='recaptchaSecretKey' id='recaptchaSecretKey' value="<?php echo $options['keys']['recaptchaSecretKey'] ?? '' ?>" />
+        <label for='recaptchaSecretKey'><?php _e( 'Recaptcha Secret Key', 'racketmanager' ) ?></label>
+    </div>
 </div>

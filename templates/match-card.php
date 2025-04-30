969b0eb484b82c3dc334233e56ec3f41504288ed
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-04-30 17:24:25 +0100

    fix php and wordpress coding errors

--- templates/match-card.php
+++ templates/match-card.php
@@ -7,12 +7,13 @@
 
 namespace Racketmanager;
 
-$points_span = 2 + intval( $match->league->num_sets );
+/** @var object $match */
+/** @var object $sponsorhtml */
 ?>
-<div id="matchrubbers" class="rubber-block">
-	<div id="matchheader">
-		<div class="leaguetitle"><?php echo esc_html( $match->league->title ); ?></div>
-		<div class="matchdate"><?php echo esc_html( substr( $match->date, 0, 10 ) ); ?></div>
+<div id="matchRubbers" class="rubber-block">
+	<div id="matchHeader">
+		<div class="leagueTitle"><?php echo esc_html( $match->league->title ); ?></div>
+		<div class="matchDate"><?php echo esc_html( substr( $match->date, 0, 10 ) ); ?></div>
 		<div class="matchday">
 			<?php
 			if ( $match->league->event->competition->is_championship ) {
@@ -22,7 +23,7 @@ $points_span = 2 + intval( $match->league->num_sets );
 			}
 			?>
 		</div>
-		<div class="matchtitle">
+		<div class="matchTitle">
 			<?php
 			if ( ! $match->league->event->competition->is_championship ) {
 				echo esc_html( $match->match_title );
@@ -30,19 +31,19 @@ $points_span = 2 + intval( $match->league->num_sets );
 			?>
 		</div>
 	</div>
-	<form id="match-view" action="#" method="post"">
+	<form id="match-view" action="#" method="post">
 		<?php wp_nonce_field( 'rubbers-match' ); ?>
 
-		<table class="widefat">
+		<table class="table table-bordered">
 		<caption><?php esc_html_e( 'Match card', 'racketmanager' ); ?></caption>
-			<thead>
+			<thead class="table-dark">
 				<tr>
 					<th style="text-align: center;" colspan="1"><?php esc_html_e( 'Team', 'racketmanager' ); ?></th>
 					<th style="text-align: center;" colspan="<?php echo esc_html( $match->league->num_sets ); ?>"><?php esc_html_e( 'Sets', 'racketmanager' ); ?></th>
 					<th style="text-align: center;" colspan="1"><?php esc_html_e( 'Team', 'racketmanager' ); ?></th>
 				</tr>
 			</thead>
-			<tbody class="rtbody rubber-table" id="the-list-rubbers-<?php echo esc_html( $match->id ); ?>" >
+			<tbody class="rubber-table" id="the-list-rubbers-<?php echo esc_html( $match->id ); ?>" >
 
 				<tr class="rtr">
 					<td class="rtd">
@@ -51,9 +52,9 @@ $points_span = 2 + intval( $match->league->num_sets );
 
 					<?php for ( $i = 1; $i <= $match->league->num_sets; $i++ ) { ?>
 						<td class="rtd">
-							<input class="points" type="text" size="2" id="set_<?php echo esc_html( $i ); ?>_player1" name="custom[sets][<?php echo esc_html( $i ); ?>][player1]" />
+                            <label for="set_<?php echo esc_html( $i ); ?>_player1"></label><input class="points" type="text" size="2" id="set_<?php echo esc_html( $i ); ?>_player1" name="custom[sets][<?php echo esc_html( $i ); ?>][player1]" />
 							:
-							<input class="points" type="text" size="2" id="set_<?php echo esc_html( $i ); ?>_player2" name="custom[sets][<?php echo esc_html( $i ); ?>][player2]" />
+                            <label for="set_<?php echo esc_html( $i ); ?>_player2"></label><input class="points" type="text" size="2" id="set_<?php echo esc_html( $i ); ?>_player2" name="custom[sets][<?php echo esc_html( $i ); ?>][player2]" />
 						</td>
 					<?php } ?>
 
@@ -63,15 +64,15 @@ $points_span = 2 + intval( $match->league->num_sets );
 				</tr>
 				<tr>
 					<td class="rtd">
-						<input class="player" name="homesig" id="homesig" placeholder="Home Captain Signature" />
+                        <label for="home_sig"></label><input class="player" name="home_sig" id="home_sig" placeholder="Home Captain Signature" />
 					</td>
 					<td colspan="<?php echo intval( $match->league->num_sets ); ?>" class="rtd" style="text-align: center;">
-						<input class="points" type="text" size="2" id="home_points" name="home_points" />
+                        <label for="home_points"></label><input class="points" type="text" size="2" id="home_points" name="home_points" />
 						:
-						<input class="points" type="text" size="2" id="away_points" name="away_points" />
+                        <label for="away_points"></label><input class="points" type="text" size="2" id="away_points" name="away_points" />
 					</td>
 					<td class="rtd">
-						<input class="player" name="awaysig" id="awaysig" placeholder="Away Captain Signature" />
+                        <label for="away_sig"></label><input class="player" name="away_sig" id="away_sig" placeholder="Away Captain Signature" />
 					</td>
 				</tr>
 			</tbody>

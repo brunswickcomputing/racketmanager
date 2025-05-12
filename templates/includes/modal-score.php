a62678dbf41fa43f4905dbbd21c6de5e0d23faeb
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-05-12 16:09:53 +0100

    remove redundant fields

--- templates/includes/modal-score.php
+++ templates/includes/modal-score.php
@@ -15,21 +15,7 @@ namespace Racketmanager;
 				<h4 class="modal-title"><?php esc_html_e( 'Score status', 'racketmanager' ); ?></h4>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
-			<div class="modal-body">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-sm-6">
-							<select class="form-select">
-								<option value="" disabled selected><?php esc_html_e( 'Status', 'racketmanager' ); ?></option>
-							</select>
-						</div>
-						<div class="col-sm-6">
-							<ul class="list list--naked">
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
+			<div class="modal-body"></div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 				<button type="button" class="btn btn-primary">Save changes</button>

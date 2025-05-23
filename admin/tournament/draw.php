8f16dcef6a05ccc36beafd8eb6cde08697d43016
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-05-23 15:02:25 +0100

    fix Undefined symbol inspection error

--- admin/tournament/draw.php
+++ admin/tournament/draw.php
@@ -7,6 +7,9 @@
 
 namespace Racketmanager;
 
+/** @var string $tab */
+/** @var object $tournament */
+/** @var object $league */
 ?>
 <script type='text/javascript'>
 jQuery(document).ready(function(){
@@ -16,7 +19,7 @@ jQuery(document).ready(function(){
 <div class="container">
 	<div class='row justify-content-end'>
 		<div class='col-auto racketmanager_breadcrumb'>
-			<a href='admin.php?page=racketmanager-tournaments'><?php esc_html_e( 'RacketManager Tournaments', 'racketmanager' ); ?></a> &raquo; <a href='admin.php?page=racketmanager-tournaments&amp;view=tournament&amp;tournament=<?php echo esc_attr( $tournament->id ); ?>&amp;season=<?php echo esc_attr( $tournament->season ); ?>'><?php echo esc_html( $tournament->name ); ?></a> &raquo; <?php echo esc_html( $league->title ); ?>
+			<a href="/wp-admin/admin.php?page=racketmanager-tournaments"><?php esc_html_e( 'RacketManager Tournaments', 'racketmanager' ); ?></a> &raquo; <a href="/wp-admin/admin.php?page=racketmanager-tournaments&amp;view=tournament&amp;tournament=<?php echo esc_attr( $tournament->id ); ?>&amp;season=<?php echo esc_attr( $tournament->season ); ?>"><?php echo esc_html( $tournament->name ); ?></a> &raquo; <?php echo esc_html( $league->title ); ?>
 		</div>
 	</div>
 	<h1><?php echo esc_html( $league->title ); ?> - <?php echo esc_html( $tournament->name ); ?></h1>

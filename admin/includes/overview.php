7148ec5ba79602fa8fdd86cfa3176468785de817
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-05-23 18:29:43 +0100

    fix Undefined symbol inspection error

--- admin/includes/overview.php
+++ admin/includes/overview.php
@@ -7,6 +7,10 @@
 
 namespace Racketmanager;
 
+/** @var object $competition */
+/** @var string $season */
+/** @var object $current_season */
+$current_season = empty( $cup_season ) ? $current_season : $cup_season;
 ?>
 	<div class="row mb-3">
 		<div class="col-12 col-md-6">
@@ -66,13 +70,13 @@ namespace Racketmanager;
 	</div>
 	<div class="row mb-3">
 		<div class="col-auto">
-			<a role="button" class="btn btn-primary" href="admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=modify&amp;competition_id=<?php echo esc_html( $competition->id ); ?>&season=<?php echo esc_html( $season ); ?>"><?php esc_html_e( 'Edit season', 'racketmanager' ); ?></a>
+			<a role="button" class="btn btn-primary" href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=modify&amp;competition_id=<?php echo esc_html( $competition->id ); ?>&season=<?php echo esc_html( $season ); ?>"><?php esc_html_e( 'Edit season', 'racketmanager' ); ?></a>
 			<?php
-			if ( $current_season->is_open ) {
+//			if ( $current_season->is_open ) {
 				?>
-				<button class="btn btn-secondary" onclick="Racketmanager.notifyCupEntryOpen(event, '<?php echo esc_html( $competition->id ); ?>');"><?php esc_html_e( 'Notify open', 'racketmanager' ); ?></button>
+				<button class="btn btn-secondary" id="notifyOpen" data-competition-id="<?php echo esc_attr( $competition->id ); ?>" data-season="<?php echo esc_attr( $season ); ?>"><?php esc_html_e( 'Notify open', 'racketmanager' ); ?></button>
 				<?php
-			}
+//			}
 			if ( ! empty( $competition->competition_code ) && $competition->is_complete ) {
 				?>
 				<a href="/index.php?competition_id=<?php echo esc_html( $competition->id ); ?>&season=<?php echo esc_html( $season ); ?>&racketmanager_export=report_results" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php esc_html_e( 'Report results', 'racketmanager' ); ?>" >
@@ -85,9 +89,16 @@ namespace Racketmanager;
 			?>
 		</div>
 	</div>
-	<div class="alert_rm" id="alert-<?php echo esc_attr( $competition->type ); ?>s" style="display:none;">
+	<div class="alert_rm" id="alert-season" style="display:none;">
 		<div class="alert__body">
-			<div class="alert__body-inner" id="alert-<?php echo esc_attr( $competition->type ); ?>s-response">
+			<div class="alert__body-inner" id="alert-season-response">
 			</div>
 		</div>
 	</div>
+<script type="text/javascript">
+    document.getElementById('notifyOpen').addEventListener('click', function (e) {
+        let competitionId = this.dataset.competitionId;
+        let season = this.dataset.season;
+        Racketmanager.notify_open(e, competitionId, season)
+    });
+</script>

4dd6efb86c28e7708a26693a53e8d1903586b842
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-05-23 14:19:19 +0100

    fix Undefined symbol inspection error

--- templates/email/contact-teams.php
+++ templates/email/contact-teams.php
@@ -7,28 +7,27 @@
 
 namespace Racketmanager;
 
+/** @var string $intro */
+/** @var array  $body */
+/** @var string $closing */
 require 'email-header.php';
-?>
-			<?php
-			$title_align = 'center';
-			require 'components/title.php';
-			$paragraph_text = __( 'Dear Captain', 'racketmanager' );
-			require 'components/paragraph.php';
-			if ( $intro ) {
-				$paragraph_text = $intro;
-				require 'components/paragraph.php';
-			}
-			foreach ( $body as $i => $body_entry ) {
-				if ( $body_entry ) {
-					$paragraph_text = $body_entry;
-					require 'components/paragraph.php';
-				}
-			}
-			if ( $closing ) {
-				$paragraph_text = $closing;
-				require 'components/paragraph.php';
-			}
-			require 'components/closing.php';
-			?>
-<?php
+$title_align = 'center';
+require 'components/title.php';
+$paragraph_text = __( 'Dear Captain', 'racketmanager' );
+require 'components/paragraph.php';
+if ( $intro ) {
+	$paragraph_text = $intro;
+	require 'components/paragraph.php';
+}
+foreach ( $body as $i => $body_entry ) {
+	if ( $body_entry ) {
+		$paragraph_text = $body_entry;
+		require 'components/paragraph.php';
+	}
+}
+if ( $closing ) {
+	$paragraph_text = $closing;
+	require 'components/paragraph.php';
+}
+require 'components/closing.php';
 require 'email-footer.php';

ebd711b5d518598e0a7f86cd955e9b188562b644
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-05-25 15:34:06 +0100

    define variable used

--- admin/competition/include/settings/rules.php
+++ admin/competition/include/settings/rules.php
@@ -7,6 +7,7 @@
 
 namespace Racketmanager;
 
+/** @var array $rules_options */
 ksort( $rules_options );
 ?>
 <div class="form-control">
@@ -14,7 +15,7 @@ ksort( $rules_options );
 	foreach ( $rules_options as $option => $value ) {
 		?>
 		<div class="form-check">
-			<input type="checkbox" class="form-check-input" name="rules[<?php echo esc_html( $option ); ?>]" id="rules-<?php echo esc_html( $option ); ?>" value="1" <?php empty( $competition->config->rules[ $option ] ) ? null : checked( 1, $competition->config->rules[ $option ] ); ?> <?php echo empty( $value ) ? 'disabled' : null; ?> />
+			<input type="checkbox" class="form-check-input" name="rules[<?php echo esc_html( $option ); ?>]" id="rules-<?php echo esc_html( $option ); ?>" value="1" <?php checked( 1, empty( $competition->config->rules[ $option ] ) ? null : $competition->config->rules[ $option ] ); ?> <?php echo empty( $value ) ? 'disabled' : null; ?> />
 			<label class="form-check-label" for="rules-<?php echo esc_html( $option ); ?>"><?php echo esc_html( ucfirst( $option ) ); ?></label>
 		</div>
 		<?php

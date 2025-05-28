ef8f3d8fd532538db99796c88ecd4e251795878d
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-05-28 12:02:20 +0100

    fix PHP PHPDoc Code style isset can be replaced with coalesce inspection error

--- admin/includes/settings/billing.php
+++ admin/includes/settings/billing.php
@@ -19,13 +19,13 @@ namespace Racketmanager;
 		</div>
 		<div class="col-md-4 mb-3 mb-md-0">
 			<div class="form-floating">
-				<input type="number" class="form-control" name='invoiceNumber' id='invoiceNumber' value='<?php echo isset($options['billing']['invoiceNumber']) ? $options['billing']['invoiceNumber'] : '' ?>' />
+				<input type="number" class="form-control" name='invoiceNumber' id='invoiceNumber' value='<?php echo $options['billing']['invoiceNumber'] ?? '' ?>' />
 				<label for='invoiceNumber'><?php _e( 'Invoice Number', 'racketmanager' ) ?></label>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="form-floating">
-				<input type="number" class="form-control" name='paymentTerms' id='paymentTerms' value='<?php echo isset($options['billing']['paymentTerms']) ? $options['billing']['paymentTerms'] : '' ?>' />
+				<input type="number" class="form-control" name='paymentTerms' id='paymentTerms' value='<?php echo $options['billing']['paymentTerms'] ?? '' ?>' />
 				<label for='paymentTerms'><?php _e( 'Payment Terms (days)', 'racketmanager' ) ?></label>
 			</div>
 		</div>
@@ -34,19 +34,19 @@ namespace Racketmanager;
 		<legend><?php esc_html_e( 'Bank Account', 'racketmanager' ); ?></legend>
 		<div class="col-md-4 mb-3 mb-md-0">
 			<div class="form-floating">
-				<input type="text" class="form-control" name='bankName' id='bankName' value='<?php echo isset($options['billing']['bankName']) ? $options['billing']['bankName'] : '' ?>' />
+				<input type="text" class="form-control" name='bankName' id='bankName' value='<?php echo $options['billing']['bankName'] ?? '' ?>' />
 				<label for='bankName'><?php _e( 'Bank Name', 'racketmanager' ) ?></label>
 			</div>
 		</div>
 		<div class="col-md-4 mb-3 mb-md-0">
 			<div class="form-floating">
-				<input type="number" class="form-control" name='sortCode' id='sortCode' value='<?php echo isset($options['billing']['sortCode']) ? $options['billing']['sortCode'] : '' ?>' />
+				<input type="number" class="form-control" name='sortCode' id='sortCode' value='<?php echo $options['billing']['sortCode'] ?? '' ?>' />
 				<label for='sortCode'><?php _e( 'Sort Code', 'racketmanager' ) ?></label>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="form-floating">
-				<input type="number" class="form-control" name='accountNumber' id='accountNumber' value='<?php echo isset($options['billing']['accountNumber']) ? $options['billing']['accountNumber'] : '' ?>' />
+				<input type="number" class="form-control" name='accountNumber' id='accountNumber' value='<?php echo $options['billing']['accountNumber'] ?? '' ?>' />
 				<label for='accountNumber'><?php _e( 'Account Number', 'racketmanager' ) ?></label>
 			</div>
 		</div>
@@ -55,19 +55,19 @@ namespace Racketmanager;
 		<legend><?php esc_html_e( 'Billing Contact', 'racketmanager' ); ?></legend>
 		<div class="col-md-4 mb-3 mb-md-0">
 			<div class="form-floating">
-				<input type="email" class="form-control" name='billingEmail' id='billingEmail' value='<?php echo isset($options['billing']['billingEmail']) ? $options['billing']['billingEmail'] : '' ?>' />
+				<input type="email" class="form-control" name='billingEmail' id='billingEmail' value='<?php echo $options['billing']['billingEmail'] ?? '' ?>' />
 				<label for='billingEmail'><?php _e( 'Billing Email Address', 'racketmanager' ) ?></label>
 			</div>
 		</div>
 		<div class="col-md-4 mb-3 mb-md-0">
 			<div class="form-floating">
-				<input type="tel" class="form-control" name='billingTelephone' id='billingTelephone' value='<?php echo isset($options['billing']['billingTelephone']) ? $options['billing']['billingTelephone'] : '' ?>' />
+				<input type="tel" class="form-control" name='billingTelephone' id='billingTelephone' value='<?php echo $options['billing']['billingTelephone'] ?? '' ?>' />
 				<label for='billingTelephone'><?php _e( 'Billing Telephone', 'racketmanager' ) ?></label>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="form-floating">
-				<input type="text" class="form-control" name='billingAddress' id='billingAddress' value='<?php echo isset($options['billing']['billingAddress']) ? $options['billing']['billingAddress'] : '' ?>' />
+				<input type="text" class="form-control" name='billingAddress' id='billingAddress' value='<?php echo $options['billing']['billingAddress'] ?? '' ?>' />
 				<label for='billingAddress'><?php _e( 'Billing Address', 'racketmanager' ) ?></label>
 			</div>
 		</div>
@@ -86,19 +86,19 @@ namespace Racketmanager;
 			<legend><?php esc_html_e( 'Test keys', 'racketmanager' ); ?></legend>
 			<div class="col-md-4 mb-3 mb-md-0">
 				<div class="form-floating">
-					<input type="text" class="form-control" name='api_publishable_key_test' id='api_publishable_key_test' value='<?php echo isset($options['billing']['api_publishable_key_test']) ? $options['billing']['api_publishable_key_test'] : '' ?>' />
+					<input type="text" class="form-control" name='api_publishable_key_test' id='api_publishable_key_test' value='<?php echo $options['billing']['api_publishable_key_test'] ?? '' ?>' />
 					<label for='api_publishable_key_test'><?php _e( 'API Publishable key', 'racketmanager' ) ?></label>
 				</div>
 			</div>
 			<div class="col-md-4 mb-3 mb-md-0">
 				<div class="form-floating">
-					<input type="text" class="form-control" name='api_secret_key_test' id='api_secret_key_test' value='<?php echo isset($options['billing']['api_secret_key_test']) ? $options['billing']['api_secret_key_test'] : '' ?>' />
+					<input type="text" class="form-control" name='api_secret_key_test' id='api_secret_key_test' value='<?php echo $options['billing']['api_secret_key_test'] ?? '' ?>' />
 					<label for='api_secret_key_test'><?php _e( 'API Secret key', 'racketmanager' ) ?></label>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="form-floating">
-					<input type="text" class="form-control" name='api_endpoint_key_test' id='api_endpoint_key_test' value='<?php echo isset($options['billing']['api_endpoint_key_test']) ? $options['billing']['api_endpoint_key_test'] : '' ?>' />
+					<input type="text" class="form-control" name='api_endpoint_key_test' id='api_endpoint_key_test' value='<?php echo $options['billing']['api_endpoint_key_test'] ?? '' ?>' />
 					<label for='api_endpoint_key_test'><?php _e( 'API Endpoint secret key', 'racketmanager' ) ?></label>
 				</div>
 			</div>
@@ -107,19 +107,19 @@ namespace Racketmanager;
 			<legend><?php esc_html_e( 'Live keys', 'racketmanager' ); ?></legend>
 			<div class="col-md-4 mb-3 mb-md-0">
 				<div class="form-floating">
-					<input type="text" class="form-control" name='api_publishable_key_live' id='api_publishable_key_live' value='<?php echo isset($options['billing']['api_publishable_key_live']) ? $options['billing']['api_publishable_key_live'] : '' ?>' />
+					<input type="text" class="form-control" name='api_publishable_key_live' id='api_publishable_key_live' value='<?php echo $options['billing']['api_publishable_key_live'] ?? '' ?>' />
 					<label for='api_publishable_key_live'><?php _e( 'API Publishable key', 'racketmanager' ) ?></label>
 				</div>
 			</div>
 			<div class="col-md-4 mb-3 mb-md-0">
 				<div class="form-floating">
-					<input type="text" class="form-control" name='api_secret_key_live' id='api_secret_key_live' value='<?php echo isset($options['billing']['api_secret_key_live']) ? $options['billing']['api_secret_key_live'] : '' ?>' />
+					<input type="text" class="form-control" name='api_secret_key_live' id='api_secret_key_live' value='<?php echo $options['billing']['api_secret_key_live'] ?? '' ?>' />
 					<label for='api_secret_key_live'><?php _e( 'API Secret key', 'racketmanager' ) ?></label>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="form-floating">
-					<input type="text" class="form-control" name='api_endpoint_key_live' id='api_endpoint_key_live' value='<?php echo isset($options['billing']['api_endpoint_key_live']) ? $options['billing']['api_endpoint_key_live'] : '' ?>' />
+					<input type="text" class="form-control" name='api_endpoint_key_live' id='api_endpoint_key_live' value='<?php echo $options['billing']['api_endpoint_key_live'] ?? '' ?>' />
 					<label for='api_endpoint_key_live'><?php _e( 'API Endpoint secret key', 'racketmanager' ) ?></label>
 				</div>
 			</div>

3cf46d6f94b40ea89590b993ec8130bafba2ed85
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-05-30 21:33:16 +0100

    fix PHP Attributes NoReturn attribute can be added inspection error

--- include/class-racketmanager-exporter.php
+++ include/class-racketmanager-exporter.php
@@ -70,7 +70,7 @@ class Racketmanager_Exporter {
 	 *  days - defaults to 7
 	 *  competition
 	 */
-	#[NoReturn] public function results(): void {
+	#[NoReturn] #[NoReturn] public function results(): void {
 		global $racketmanager;
 		// phpcs:disable WordPress.Security.NonceVerification.Recommended
 		if ( isset( $_GET['club'] ) ) {

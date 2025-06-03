6a15383a622ef57fc4ecb1fbfdfaee6a83153119
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-03 16:29:38 +0100

    remove tennis specific files

--- templates/matches.php
+++ templates/matches.php
@@ -1,45 +1,60 @@
 <?php
 /**
- * Matches table template
+ * Template page for the match table in tennis
  *
  * @package Racketmanager/Templates
+ *
+ * The following variables are usable:
+ *  $league: contains data of current league
+ *  $matches: contains all matches for current league
+ *  $teams: contains teams of current league in an associative array
+ *  $season: current season
+ *
+ * You can check the content of a variable when you insert the tag <?php var_dump($variable)
  */
 
 namespace Racketmanager;
 
+/** @var object $league */
+/** @var array  $matches */
 ?>
-<div class="racketmanager-container">
-	<?php
-	if ( is_single_match() ) {
-		the_single_match();
-	} else {
-		require 'matches-selections.php';
-		if ( have_matches() ) {
-			?>
-			<table class='racketmanager matchtable' title='<?php esc_html_e( 'Match Plan', 'racketmanager' ); ?> <?php the_league_title(); ?>'>
-				<thead>
-					<tr>
-						<th class='match'><?php esc_html_e( 'Match', 'racketmanager' ); ?></th>
-						<th class='score'><?php esc_html_e( 'Score', 'racketmanager' ); ?></th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					while ( have_matches() ) {
-						the_match();
-						?>
-						<tr class='<?php the_match_class(); ?>'>
-							<td class='match'><?php the_match_date(); ?> <?php the_match_time( $match->start_time ); ?> <?php echo esc_html( $match->location ); ?><br /><?php the_match_title(); ?> <?php the_match_report(); ?></td>
-							<td class='score'><?php the_match_score(); ?></td>
-						</tr>
-						<?php
-					}
-					?>
-				</tbody>
-			</table>
-			<?php the_matches_pagination(); ?>
+<div class="module module--card">
+    <div class="module__banner">
+        <h3 class="module__title"><?php esc_html_e( 'Matches', 'racketmanager' ); ?></h3>
+    </div>
+    <div class="module__content">
+        <div class="module-container">
 			<?php
-		}
-	}
-	?>
+			if ( ! $league->event->is_box ) {
+				include 'matches-selections.php';
+			}
+			if ( $matches ) {
+				$show_header = false;
+				if ( $league->event->competition->is_team_entry ) {
+					if ( -1 === $league->match_day ) {
+						$matches_list = $matches;
+						$matches_key  = 'match_day';
+						require RACKETMANAGER_PATH . 'templates/includes/matches-team-list-group.php';
+					} else {
+						require RACKETMANAGER_PATH . 'templates/includes/matches-team-list.php';
+					}
+				} else {
+					?>
+                    <div class="tournament-matches">
+						<?php
+						foreach ( $matches as $no => $match ) {
+							?>
+							<?php require RACKETMANAGER_PATH . 'templates/tournament/match.php'; ?>
+							<?php
+						}
+						?>
+                    </div>
+					<?php
+				}
+			} else {
+				esc_html_e( 'No matches found', 'racketmanager' );
+			}
+			?>
+        </div>
+    </div>
 </div>

ae2bd940dffe01249372efedd894964c84d8c017
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-11 12:56:36 +0100

    use output element instead of role=status

--- templates/includes/loading.php
+++ templates/includes/loading.php
@@ -11,9 +11,9 @@ namespace Racketmanager;
 <div id="splash" class="container d-none">
 	<div class="module module--card">
 		<div class="spinner-loading d-flex justify-content-center">
-			<div class="spinner-border" role="status">
-			<span class="visually-hidden">Loading...</span>
-			</div>
+			<output class="spinner-border">
+			    <span class="visually-hidden">Loading...</span>
+			</output>
 		</div>
 	</div>
 </div>

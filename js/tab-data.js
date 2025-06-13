69edbff0c85a4c025d40aa9160de9816f633ef4f
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-13 14:45:47 +0100

    remove event listener before adding

--- js/tab-data.js
+++ js/tab-data.js
@@ -9,26 +9,34 @@ jQuery(document).ajaxComplete(function () {
 function tabData () {
 //* Add event listener for tab data links
     const tabData = document.querySelectorAll('.tabData');
-    tabData.forEach(el => el.addEventListener('click', function (e) {
-        let type = this.dataset.type;
-        let typeId = this.dataset.typeId;
-        let season = this.dataset.season;
-        let name = this.dataset.name;
-        let competitionType = this.dataset.competitionType;
-        Racketmanager.tabData(e,type,typeId,season,name,competitionType);
-    }));
+    tabData.forEach(function(el) {
+           el.removeEventListener('click', tabDataClick);
+           el.addEventListener('click', tabDataClick);
+    });
+}
+function tabDataClick (e) {
+    let type = this.dataset.type;
+    let typeId = this.dataset.typeId;
+    let season = this.dataset.season;
+    let name = this.dataset.name;
+    let competitionType = this.dataset.competitionType;
+    Racketmanager.tabData(e,type,typeId,season,name,competitionType);
 }
 function tabDataLinks () {
 //* Add event listener for tab data links
     const tabDataLinks = document.querySelectorAll('.tabDataLink');
-    tabDataLinks.forEach(el => el.addEventListener('click', function (e) {
-        let type = this.dataset.type;
-        let typeId = this.dataset.typeId;
-        let season = this.dataset.season;
-        let link = this.dataset.link;
-        let linkId = this.dataset.linkId;
-        let linkType = this.dataset.linkType;
-        Racketmanager.tabDataLink(e,type,typeId,season,link,linkId,linkType);
-    }));
+    tabDataLinks.forEach(function(el) {
+            el.removeEventListener('click', tabDataLinkClick);
+            el.addEventListener('click', tabDataLinkClick);
+    });
+}
+function tabDataLinkClick (e) {
+    let type = this.dataset.type;
+    let typeId = this.dataset.typeId;
+    let season = this.dataset.season;
+    let link = this.dataset.link;
+    let linkId = this.dataset.linkId;
+    let linkType = this.dataset.linkType;
+    Racketmanager.tabDataLink(e,type,typeId,season,link,linkId,linkType);
 }
 

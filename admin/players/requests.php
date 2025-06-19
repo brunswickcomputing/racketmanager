8037e8539747e04b13ea6fda6be30a635d288189
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-19 16:03:49 +0100

    replace tab character with spaces

--- admin/players/requests.php
+++ admin/players/requests.php
@@ -15,33 +15,33 @@ namespace Racketmanager;
 ?>
 <!-- Club Player Request Filter -->
 <form id="club-player-request-filter" method="get" action="" class="form-control mb-3">
-	<input type="hidden" name="page" value="racketmanager-players" />
-	<input type="hidden" name="view" value="requests" />
-	<div class="col-auto">
+    <input type="hidden" name="page" value="racketmanager-players" />
+    <input type="hidden" name="view" value="requests" />
+    <div class="col-auto">
         <label for="club"></label>
         <select class="select" name="club" id="club">
-			<option value="all"><?php esc_html_e( 'All clubs', 'racketmanager' ); ?></option>
-			<?php
-			foreach ( $clubs as $club ) {
-				?>
-				<option value="<?php echo esc_html( $club->id ); ?>" <?php echo intval( $club->id ) === $club_id ? 'selected' : ''; ?>><?php echo esc_html( $club->shortcode ); ?></option>
-				<?php
-			}
-			?>
-		</select>
+            <option value="all"><?php esc_html_e( 'All clubs', 'racketmanager' ); ?></option>
+            <?php
+            foreach ( $clubs as $club ) {
+                ?>
+                <option value="<?php echo esc_html( $club->id ); ?>" <?php echo intval( $club->id ) === $club_id ? 'selected' : ''; ?>><?php echo esc_html( $club->shortcode ); ?></option>
+                <?php
+            }
+            ?>
+        </select>
         <label for="status"></label><select class="select" name="status" id="status">
-			<option value="all" <?php echo 'all' === $status ? 'selected' : ''; ?>><?php esc_html_e( 'All', 'racketmanager' ); ?></option>
-			<option value="outstanding" <?php echo 'outstanding' === $status ? 'selected' : ''; ?>><?php esc_html_e( 'Outstanding', 'racketmanager' ); ?></option>
-		</select>
-		<button class="btn btn-primary"><?php esc_html_e( 'Filter', 'racketmanager' ); ?></button>
-	</div>
+            <option value="all" <?php echo 'all' === $status ? 'selected' : ''; ?>><?php esc_html_e( 'All', 'racketmanager' ); ?></option>
+            <option value="outstanding" <?php echo 'outstanding' === $status ? 'selected' : ''; ?>><?php esc_html_e( 'Outstanding', 'racketmanager' ); ?></option>
+        </select>
+        <button class="btn btn-primary"><?php esc_html_e( 'Filter', 'racketmanager' ); ?></button>
+    </div>
 </form>
 
 <form id="club-player-request-filter" method="post" action="" class="form-control">
-	<?php wp_nonce_field( 'club-player-request-bulk' ); ?>
+    <?php wp_nonce_field( 'club-player-request-bulk' ); ?>
 
-	<div class="mb-3">
-		<!-- Bulk Actions -->
+    <div class="mb-3">
+        <!-- Bulk Actions -->
         <label>
             <select name="action" id="action" class="form-control">
                 <option value="-1" selected="selected"><?php esc_html_e( 'Bulk Actions', 'racketmanager' ); ?></option>
@@ -49,42 +49,42 @@ namespace Racketmanager;
                 <option value="delete"><?php esc_html_e( 'Delete', 'racketmanager' ); ?></option>
             </select>
         </label>
-		<input type="submit" value="<?php esc_html_e( 'Apply', 'racketmanager' ); ?>" name="doPlayerRequest" id="doPlayerRequest" class="btn btn-secondary action" />
-	</div>
-	<table class="table table-striped">
-		<thead class="table-dark">
-			<tr>
-				<th class="check-column"><label for="checkALL"></label><input type="checkbox" name="checkAll" id="checkALL" onclick="Racketmanager.checkAll(document.getElementById('club-player-request-filter'));" /></th>
-				<th><?php esc_html_e( 'ID', 'racketmanager' ); ?></th>
-				<th><?php esc_html_e( 'Club', 'racketmanager' ); ?></th>
-				<th><?php esc_html_e( 'First Name', 'racketmanager' ); ?></th>
-				<th><?php esc_html_e( 'Surname', 'racketmanager' ); ?></th>
-				<th><?php esc_html_e( 'Gender', 'racketmanager' ); ?></th>
-				<th><?php esc_html_e( 'LTA Tennis Number', 'racketmanager' ); ?></th>
-				<th><?php esc_html_e( 'Requested', 'racketmanager' ); ?></th>
-				<th><?php esc_html_e( 'Completed', 'racketmanager' ); ?></th>
+        <input type="submit" value="<?php esc_html_e( 'Apply', 'racketmanager' ); ?>" name="doPlayerRequest" id="doPlayerRequest" class="btn btn-secondary action" />
+    </div>
+    <table class="table table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th class="check-column"><label for="checkALL"></label><input type="checkbox" name="checkAll" id="checkALL" onclick="Racketmanager.checkAll(document.getElementById('club-player-request-filter'));" /></th>
+                <th><?php esc_html_e( 'ID', 'racketmanager' ); ?></th>
+                <th><?php esc_html_e( 'Club', 'racketmanager' ); ?></th>
+                <th><?php esc_html_e( 'First Name', 'racketmanager' ); ?></th>
+                <th><?php esc_html_e( 'Surname', 'racketmanager' ); ?></th>
+                <th><?php esc_html_e( 'Gender', 'racketmanager' ); ?></th>
+                <th><?php esc_html_e( 'LTA Tennis Number', 'racketmanager' ); ?></th>
+                <th><?php esc_html_e( 'Requested', 'racketmanager' ); ?></th>
+                <th><?php esc_html_e( 'Completed', 'racketmanager' ); ?></th>
                 <th><?php esc_html_e( 'Removed', 'racketmanager' ); ?></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			foreach ( $player_requests as $request ) {
-				?>
-				<tr>
-					<td class="check-column"><label for="playerRequest-<?php echo esc_html( $request->id ); ?>"></label><input type="checkbox" value="<?php echo esc_html( $request->id ); ?>" name="playerRequest[<?php echo esc_html( $request->id ); ?>]" id="playerRequest-<?php echo esc_html( $request->id ); ?>" /></<td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ( $player_requests as $request ) {
+                ?>
+                <tr>
+                    <td class="check-column"><label for="playerRequest-<?php echo esc_html( $request->id ); ?>"></label><input type="checkbox" value="<?php echo esc_html( $request->id ); ?>" name="playerRequest[<?php echo esc_html( $request->id ); ?>]" id="playerRequest-<?php echo esc_html( $request->id ); ?>" /></<td>
                     <td><?php echo esc_html( $request->id ); ?></<td>
-					<td><?php echo esc_html( $request->club->shortcode ); ?></<td>
-					<td><?php echo esc_html( $request->player->firstname ); ?></<td>
-					<td><?php echo esc_html( $request->player->surname ); ?></<td>
-					<td><?php echo esc_html( $request->player->gender ); ?></<td>
-					<td><?php echo esc_html( $request->player->btm ); ?></<td>
-					<td <?php echo empty( $request->requested_user ) ? null : 'title="' . esc_html__( 'Requested by', 'racketmanager' ) . ' ' . esc_html( $request->requested_user_name ) . '"'; ?>><?php echo esc_html( $request->requested_date ); ?></<td>
-					<td <?php echo empty( $request->created_user ) ? null : 'title="' . esc_html__( 'Created by', 'racketmanager' ) . ' ' . esc_html( $request->created_user_name ) . '"'; ?>><?php echo esc_html( $request->created_date ); ?></<td>
+                    <td><?php echo esc_html( $request->club->shortcode ); ?></<td>
+                    <td><?php echo esc_html( $request->player->firstname ); ?></<td>
+                    <td><?php echo esc_html( $request->player->surname ); ?></<td>
+                    <td><?php echo esc_html( $request->player->gender ); ?></<td>
+                    <td><?php echo esc_html( $request->player->btm ); ?></<td>
+                    <td <?php echo empty( $request->requested_user ) ? null : 'title="' . esc_html__( 'Requested by', 'racketmanager' ) . ' ' . esc_html( $request->requested_user_name ) . '"'; ?>><?php echo esc_html( $request->requested_date ); ?></<td>
+                    <td <?php echo empty( $request->created_user ) ? null : 'title="' . esc_html__( 'Created by', 'racketmanager' ) . ' ' . esc_html( $request->created_user_name ) . '"'; ?>><?php echo esc_html( $request->created_date ); ?></<td>
                     <td <?php echo empty( $request->removed_user ) ? null : 'title="' . esc_html__( 'Removed by', 'racketmanager' ) . ' ' . esc_html( $request->removed_user_name ) . '"'; ?>><?php echo esc_html( $request->removed_date ); ?></<td>
-				</tr>
-				<?php
-			}
-			?>
-		</tbody>
-	</table>
+                </tr>
+                <?php
+            }
+            ?>
+        </tbody>
+    </table>
 </form>

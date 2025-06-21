4a1574780cd30401bf4b52638db88dd97b535167
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 12:23:09 +0100

    A form label must be associated with a control - fix

--- admin/competition/include/settings/events.php
+++ admin/competition/include/settings/events.php
@@ -33,7 +33,7 @@ $events   = $competition->get_events();
         <table class="table table-striped table-borderless">
             <thead class="table-dark">
                 <tr>
-                    <th class="check-column"><input type="checkbox" id="event-select-all" onclick="Racketmanager.checkAll(document.getElementById('events-action'));" /><label for="event-select-all"></label></th>
+                    <th class="check-column"><input type="checkbox" id="event-select-all" onclick="Racketmanager.checkAll(document.getElementById('events-action'));" /><label for="event-select-all" class="visually-hidden"><?php esc_html_e( 'Check all', 'racketmanager' ); ?></label></th>
                     <th class=""><?php esc_html_e( 'Name', 'racketmanager' ); ?></th>
                     <th class=""><?php esc_html_e( 'Status', 'racketmanager' ); ?></th>
                     <th class=""><?php esc_html_e( 'Age', 'racketmanager' ); ?></th>
@@ -47,7 +47,7 @@ $events   = $competition->get_events();
                 foreach( $events as $event ) {
                     ?>
                     <tr>
-                        <td class="check-column"><input type="checkbox" id="event-select-<?php echo esc_attr( $event->id ); ?>" value="<?php echo esc_html( $event->id ); ?>" name="event[<?php echo esc_html( $event->id ); ?>]" /><label for="event-select-<?php echo esc_attr( $event->id ); ?>"></label></td>
+                        <td class="check-column"><input type="checkbox" id="event-select-<?php echo esc_attr( $event->id ); ?>" value="<?php echo esc_html( $event->id ); ?>" name="event[<?php echo esc_html( $event->id ); ?>]" /><label for="event-select-<?php echo esc_attr( $event->id ); ?>" class="visually-hidden"><?php esc_html_e( 'Check', 'racketmanager' ); ?></label></td>
                         <td class=""><a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=event&amp;event_id=<?php echo esc_html( $event->id ); ?>&competition_id=<?php echo esc_attr( $competition->id ); ?><?php echo esc_attr( $add_link ); ?>"><?php echo esc_html( $event->name ); ?></a></td>
                         <td class=""><?php echo esc_html( Racketmanager_Util::get_event_type( $event->type ) ); ?></td>
                         <td class=""><?php echo esc_html( Racketmanager_Util::get_age_limit( $event->age_limit ) ); ?></td>

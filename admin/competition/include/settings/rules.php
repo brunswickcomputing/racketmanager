03efc058ca710e37aaf332e71017c3ef9a301a68
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 12:26:32 +0100

    replace tab character with spaces

--- admin/competition/include/settings/rules.php
+++ admin/competition/include/settings/rules.php
@@ -11,14 +11,14 @@ namespace Racketmanager;
 ksort( $rules_options );
 ?>
 <div class="form-control">
-	<?php
-	foreach ( $rules_options as $option => $value ) {
-		?>
-		<div class="form-check">
-			<input type="checkbox" class="form-check-input" name="rules[<?php echo esc_html( $option ); ?>]" id="rules-<?php echo esc_html( $option ); ?>" value="1" <?php checked( 1, empty( $competition->config->rules[ $option ] ) ? null : $competition->config->rules[ $option ] ); ?> <?php echo empty( $value ) ? 'disabled' : null; ?> />
-			<label class="form-check-label" for="rules-<?php echo esc_html( $option ); ?>"><?php echo esc_html( ucfirst( $option ) ); ?></label>
-		</div>
-		<?php
-	}
-	?>
+    <?php
+    foreach ( $rules_options as $option => $value ) {
+        ?>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" name="rules[<?php echo esc_html( $option ); ?>]" id="rules-<?php echo esc_html( $option ); ?>" value="1" <?php checked( 1, empty( $competition->config->rules[ $option ] ) ? null : $competition->config->rules[ $option ] ); ?> <?php echo empty( $value ) ? 'disabled' : null; ?> />
+            <label class="form-check-label" for="rules-<?php echo esc_html( $option ); ?>"><?php echo esc_html( ucfirst( $option ) ); ?></label>
+        </div>
+        <?php
+    }
+    ?>
 </div>

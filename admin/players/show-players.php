2f237b98f13e600a8cd092f77790de4ff9d9b05a
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 14:30:26 +0100

    Replace "require" with "require_once".

--- admin/players/show-players.php
+++ admin/players/show-players.php
@@ -9,12 +9,12 @@ namespace Racketmanager;
 
 ?>
 <div class="container">
-    <?php require 'nav-tabs.php'; ?>
+    <?php require_once 'nav-tabs.php'; ?>
     <div class="row">
         <!-- Tab panes -->
         <div class="tab-content">
             <div class="tab-pane active" id="players" role="tabpanel">
-                <?php require 'players.php'; ?>
+                <?php require_once 'players.php'; ?>
             </div>
         </div>
     </div>

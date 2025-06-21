91cdcab16368b3f4b9a207a91cb4c53174a7432f
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 14:27:00 +0100

    replace tab character with spaces

--- admin/results/postponed.php
+++ admin/results/postponed.php
@@ -11,14 +11,14 @@ global $racketmanager;
 $racketmanager_match_args            = array();
 $racketmanager_match_args['status']  = 'D';
 $racketmanager_match_args['orderby'] = array(
-	'updated' => 'ASC',
-	'id'      => 'ASC',
+    'updated' => 'ASC',
+    'id'      => 'ASC',
 );
 $racketmanager_options        = $racketmanager->get_options( 'league' );
 $racketmanager_result_pending = '';
 if ( isset( $racketmanager_options['resultPending'] ) ) {
-	$racketmanager_result_pending              = $racketmanager_options['resultPending'];
-	$racketmanager_match_args['resultPending'] = $racketmanager_result_pending;
+    $racketmanager_result_pending              = $racketmanager_options['resultPending'];
+    $racketmanager_match_args['resultPending'] = $racketmanager_result_pending;
 }
 $racketmanager_matches     = $racketmanager->get_matches( $racketmanager_match_args );
 $racketmanager_prev_league = 0;
@@ -32,37 +32,37 @@ $racketmanager_prev_league = 0;
     </tr>
     </thead>
     <tbody>
-	<?php
-	if ( $racketmanager_matches ) {
-		foreach ( $racketmanager_matches as $racketmanager_match ) {
-			$tooltip                     = '';
-			$racketmanager_match         = get_match( $racketmanager_match );
-			if ( $racketmanager_result_pending ) {
-				$racketmanager_now          = date_create();
-				$racketmanager_date_overdue = date_create( $racketmanager_match->result_overdue_date );
-				if ( $racketmanager_date_overdue < $racketmanager_now ) {
-					/* translators: %d: days overdue  */
-					$tooltip =  sprintf( __( 'Result overdue by %d days', 'racketmanager' ), intval( ceil( $racketmanager_match->overdue_time ) ) );
-				}
-			}
-			if ( $racketmanager_prev_league !== $racketmanager_match->league_id ) {
-				$racketmanager_prev_league = $racketmanager_match->league_id;
-				?>
+    <?php
+    if ( $racketmanager_matches ) {
+        foreach ( $racketmanager_matches as $racketmanager_match ) {
+            $tooltip                     = '';
+            $racketmanager_match         = get_match( $racketmanager_match );
+            if ( $racketmanager_result_pending ) {
+                $racketmanager_now          = date_create();
+                $racketmanager_date_overdue = date_create( $racketmanager_match->result_overdue_date );
+                if ( $racketmanager_date_overdue < $racketmanager_now ) {
+                    /* translators: %d: days overdue  */
+                    $tooltip =  sprintf( __( 'Result overdue by %d days', 'racketmanager' ), intval( ceil( $racketmanager_match->overdue_time ) ) );
+                }
+            }
+            if ( $racketmanager_prev_league !== $racketmanager_match->league_id ) {
+                $racketmanager_prev_league = $racketmanager_match->league_id;
+                ?>
                 <tr><td colspan="3" class="fw-bold fst-italic"><?php echo esc_html( $racketmanager_match->league->title ); ?></td></tr>
-				<?php
-			}
-			?>
+                <?php
+            }
+            ?>
             <tr data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="<?php echo esc_html( $tooltip ); ?>">
                 <td class=""><?php echo esc_html( mysql2date( 'Y-m-d', $racketmanager_match->date ) ); ?></td>
                 <td class="match-title"><a href="<?php echo esc_html( $racketmanager_match->link ); ?>?referrer=postponed; ?>"><?php echo esc_html( $racketmanager_match->match_title ); ?></a></td>
                 <td class=""><a href="<?php echo esc_html( $racketmanager_match->link ); ?>result/?referrer=postponed" class="btn btn-primary"><?php esc_html_e( 'Enter result', 'racketmanager' ); ?></a>                </tr>
-			<?php
-		}
-	} else {
-		?>
+            <?php
+        }
+    } else {
+        ?>
         <tr><td colspan="3"><?php esc_html_e( 'No matches found for criteria', 'racketmanager' ); ?></td></tr>
-		<?php
-	}
-	?>
+        <?php
+    }
+    ?>
     </tbody>
 </table>

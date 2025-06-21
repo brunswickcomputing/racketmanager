f3fc1d61d2c330e4b415727adc0cfa7e3f48e86b
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 12:20:36 +0100

    replace tab character with spaces

--- admin/competition/include/settings/events.php
+++ admin/competition/include/settings/events.php
@@ -13,12 +13,12 @@ namespace Racketmanager;
 $events   = $competition->get_events();
 ?>
 <div class="mb-3 form-control">
-	<form id='events-action' method='post' action='' class='form-control mb-3'>
-		<?php wp_nonce_field( 'racketmanager__events-bulk', 'racketmanager_event_nonce' ); ?>
-		<input type="hidden" name="competition_id" value="<?php echo esc_html( $competition_id ); ?>" />
-		<div class="row gx-3 mb-3 align-items-center">
-			<!-- Bulk Actions -->
-			<div class="col-auto">
+    <form id='events-action' method='post' action='' class='form-control mb-3'>
+        <?php wp_nonce_field( 'racketmanager__events-bulk', 'racketmanager_event_nonce' ); ?>
+        <input type="hidden" name="competition_id" value="<?php echo esc_html( $competition_id ); ?>" />
+        <div class="row gx-3 mb-3 align-items-center">
+            <!-- Bulk Actions -->
+            <div class="col-auto">
                 <label>
                     <select class="form-select" name="action">
                         <option value="-1" selected="selected"><?php esc_html_e( 'Bulk Actions', 'racketmanager' ); ?></option>
@@ -26,43 +26,43 @@ $events   = $competition->get_events();
                     </select>
                 </label>
             </div>
-			<div class="col-auto">
-				<button name="doActionEvent" id="doActionEvent" class="btn btn-secondary"><?php esc_html_e( 'Apply', 'racketmanager' ); ?></button>
-			</div>
-		</div>
-		<table class="table table-striped table-borderless">
-			<thead class="table-dark">
-				<tr>
-					<th class="check-column"><input type="checkbox" id="event-select-all" onclick="Racketmanager.checkAll(document.getElementById('events-action'));" /><label for="event-select-all"></label></th>
-					<th class=""><?php esc_html_e( 'Name', 'racketmanager' ); ?></th>
-					<th class=""><?php esc_html_e( 'Status', 'racketmanager' ); ?></th>
-					<th class=""><?php esc_html_e( 'Age', 'racketmanager' ); ?></th>
-				</tr>
-			</thead>
-			<?php
-			if ( $events ) {
-				?>
-				<tbody>
-				<?php
-				foreach( $events as $event ) {
-					?>
-					<tr>
-						<td class="check-column"><input type="checkbox" id="event-select-<?php echo esc_attr( $event->id ); ?>" value="<?php echo esc_html( $event->id ); ?>" name="event[<?php echo esc_html( $event->id ); ?>]" /><label for="event-select-<?php echo esc_attr( $event->id ); ?>"></label></td>
-						<td class=""><a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=event&amp;event_id=<?php echo esc_html( $event->id ); ?>&competition_id=<?php echo esc_attr( $competition->id ); ?><?php echo esc_attr( $add_link ); ?>"><?php echo esc_html( $event->name ); ?></a></td>
-						<td class=""><?php echo esc_html( Racketmanager_Util::get_event_type( $event->type ) ); ?></td>
-						<td class=""><?php echo esc_html( Racketmanager_Util::get_age_limit( $event->age_limit ) ); ?></td>
-					</tr>
-					<?php
-				}
-				?>
-				</tbody>
-				<?php
-			}
-			?>
-		</table>
-	</form>
+            <div class="col-auto">
+                <button name="doActionEvent" id="doActionEvent" class="btn btn-secondary"><?php esc_html_e( 'Apply', 'racketmanager' ); ?></button>
+            </div>
+        </div>
+        <table class="table table-striped table-borderless">
+            <thead class="table-dark">
+                <tr>
+                    <th class="check-column"><input type="checkbox" id="event-select-all" onclick="Racketmanager.checkAll(document.getElementById('events-action'));" /><label for="event-select-all"></label></th>
+                    <th class=""><?php esc_html_e( 'Name', 'racketmanager' ); ?></th>
+                    <th class=""><?php esc_html_e( 'Status', 'racketmanager' ); ?></th>
+                    <th class=""><?php esc_html_e( 'Age', 'racketmanager' ); ?></th>
+                </tr>
+            </thead>
+            <?php
+            if ( $events ) {
+                ?>
+                <tbody>
+                <?php
+                foreach( $events as $event ) {
+                    ?>
+                    <tr>
+                        <td class="check-column"><input type="checkbox" id="event-select-<?php echo esc_attr( $event->id ); ?>" value="<?php echo esc_html( $event->id ); ?>" name="event[<?php echo esc_html( $event->id ); ?>]" /><label for="event-select-<?php echo esc_attr( $event->id ); ?>"></label></td>
+                        <td class=""><a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=event&amp;event_id=<?php echo esc_html( $event->id ); ?>&competition_id=<?php echo esc_attr( $competition->id ); ?><?php echo esc_attr( $add_link ); ?>"><?php echo esc_html( $event->name ); ?></a></td>
+                        <td class=""><?php echo esc_html( Racketmanager_Util::get_event_type( $event->type ) ); ?></td>
+                        <td class=""><?php echo esc_html( Racketmanager_Util::get_age_limit( $event->age_limit ) ); ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+                </tbody>
+                <?php
+            }
+            ?>
+        </table>
+    </form>
 </div>
 <div class="mb-3">
-	<!-- Add New Event -->
-	<a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=event&competition_id=<?php echo esc_attr( $competition->id ); ?><?php echo esc_attr( $add_link ); ?>" class="btn btn-primary submit"><?php esc_html_e( 'Add Event', 'racketmanager' ); ?></a>
+    <!-- Add New Event -->
+    <a href="/wp-admin/admin.php?page=racketmanager-<?php echo esc_attr( $competition->type ); ?>s&amp;view=event&competition_id=<?php echo esc_attr( $competition->id ); ?><?php echo esc_attr( $add_link ); ?>" class="btn btn-primary submit"><?php esc_html_e( 'Add Event', 'racketmanager' ); ?></a>
 </div>

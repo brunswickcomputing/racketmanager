ea3214749c59d367a7d28d3315e78b85041780bc
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 12:28:44 +0100

    replace tab character with spaces

--- admin/tournament/player-list.php
+++ admin/tournament/player-list.php
@@ -11,36 +11,36 @@ namespace Racketmanager;
 /** @var bool   $entered */
 /** @var object $tournament */
 foreach ( $player_list as $players ) {
-	foreach ( $players as $player ) {
-		?>
-		<tr>
-			<td>
-				<?php
-				if ( $entered || ! empty( $player->user_email ) ) {
-					?>
-				<a href="/tournament/entry-form/<?php echo esc_attr( seo_url( $tournament->name ) ); ?>/player/<?php echo esc_attr( seo_url( $player->display_name ) ); ?>/">
-					<?php
-				}
-				?>
-				<?php echo esc_html( $player->display_name ); ?>
-				<?php
-				if ( $entered ) {
-					?>
-					</a>
-					<?php
-				}
-				?>
-				<?php
-				$rating         = $player->wtn;
-				$match_types    = Racketmanager_Util::get_match_types();
-				$rating_display = '';
-				foreach ( $match_types as $match_type => $description ) {
-					$rating_display .= '[' . $match_type . ' - ' . $rating[ $match_type ] . ']';
-				}
-				echo ' ' . esc_html( $rating_display );
-				?>
-			</td>
-		</tr>
-		<?php
-	}
+    foreach ( $players as $player ) {
+        ?>
+        <tr>
+            <td>
+                <?php
+                if ( $entered || ! empty( $player->user_email ) ) {
+                    ?>
+                <a href="/tournament/entry-form/<?php echo esc_attr( seo_url( $tournament->name ) ); ?>/player/<?php echo esc_attr( seo_url( $player->display_name ) ); ?>/">
+                    <?php
+                }
+                ?>
+                <?php echo esc_html( $player->display_name ); ?>
+                <?php
+                if ( $entered ) {
+                    ?>
+                    </a>
+                    <?php
+                }
+                ?>
+                <?php
+                $rating         = $player->wtn;
+                $match_types    = Racketmanager_Util::get_match_types();
+                $rating_display = '';
+                foreach ( $match_types as $match_type => $description ) {
+                    $rating_display .= '[' . $match_type . ' - ' . $rating[ $match_type ] . ']';
+                }
+                echo ' ' . esc_html( $rating_display );
+                ?>
+            </td>
+        </tr>
+        <?php
+    }
 }

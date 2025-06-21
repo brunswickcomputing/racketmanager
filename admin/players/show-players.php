17ad084ecadb616d42637504d4cb1efc097ccefe
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 14:30:07 +0100

    replace tab character with spaces

--- admin/players/show-players.php
+++ admin/players/show-players.php
@@ -9,13 +9,13 @@ namespace Racketmanager;
 
 ?>
 <div class="container">
-	<?php require 'nav-tabs.php'; ?>
-	<div class="row">
-		<!-- Tab panes -->
-		<div class="tab-content">
-			<div class="tab-pane active" id="players" role="tabpanel">
-				<?php require 'players.php'; ?>
-			</div>
-		</div>
-	</div>
+    <?php require 'nav-tabs.php'; ?>
+    <div class="row">
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div class="tab-pane active" id="players" role="tabpanel">
+                <?php require 'players.php'; ?>
+            </div>
+        </div>
+    </div>
 </div>

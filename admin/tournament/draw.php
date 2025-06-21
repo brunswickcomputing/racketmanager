aab01b9ed45e2a3cf1f789ee2aea4b0d7745e3a0
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-21 13:00:10 +0100

    replace tab character with spaces

--- admin/tournament/draw.php
+++ admin/tournament/draw.php
@@ -13,15 +13,15 @@ namespace Racketmanager;
 ?>
 <script type='text/javascript'>
 jQuery(document).ready(function(){
-	activaTab('<?php echo esc_html( $tab ); ?>');
+    activaTab('<?php echo esc_html( $tab ); ?>');
 });
 </script>
 <div class="container">
-	<div class='row justify-content-end'>
-		<div class='col-auto racketmanager_breadcrumb'>
-			<a href="/wp-admin/admin.php?page=racketmanager-tournaments"><?php esc_html_e( 'RacketManager Tournaments', 'racketmanager' ); ?></a> &raquo; <a href="/wp-admin/admin.php?page=racketmanager-tournaments&amp;view=tournament&amp;tournament=<?php echo esc_attr( $tournament->id ); ?>&amp;season=<?php echo esc_attr( $tournament->season ); ?>"><?php echo esc_html( $tournament->name ); ?></a> &raquo; <?php echo esc_html( $league->title ); ?>
-		</div>
-	</div>
-	<h1><?php echo esc_html( $league->title ); ?> - <?php echo esc_html( $tournament->name ); ?></h1>
-	<?php require RACKETMANAGER_PATH . 'admin/includes/championship-tabs.php'; ?>
+    <div class='row justify-content-end'>
+        <div class='col-auto racketmanager_breadcrumb'>
+            <a href="/wp-admin/admin.php?page=racketmanager-tournaments"><?php esc_html_e( 'RacketManager Tournaments', 'racketmanager' ); ?></a> &raquo; <a href="/wp-admin/admin.php?page=racketmanager-tournaments&amp;view=tournament&amp;tournament=<?php echo esc_attr( $tournament->id ); ?>&amp;season=<?php echo esc_attr( $tournament->season ); ?>"><?php echo esc_html( $tournament->name ); ?></a> &raquo; <?php echo esc_html( $league->title ); ?>
+        </div>
+    </div>
+    <h1><?php echo esc_html( $league->title ); ?> - <?php echo esc_html( $tournament->name ); ?></h1>
+    <?php require RACKETMANAGER_PATH . 'admin/includes/championship-tabs.php'; ?>
 </div>

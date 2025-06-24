5c7c5eea26b37a14c618771c5eb80956a0017443
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-06-24 16:53:35 +0100

    fix sonar errors

--- templates/includes/nav-pills.php
+++ templates/includes/nav-pills.php
@@ -8,23 +8,23 @@
 namespace Racketmanager;
 
 ?>
-					<div class="d-none d-md-block module mt-3">
-						<ul class="nav nav-pills justify-content-center">
-							<li class="nav-item">
-								<a class="nav-link active" onclick="Racketmanager.switchTab(this)" id="tab-list" data-tabid="tab-list">
-									<svg width="16" height="16" class="icon icon-list">
-										<use xlink:href="<?php echo esc_url( RACKETMANAGER_URL . 'images/bootstrap-icons.svg#list' ); ?>"></use>
-									</svg>
-									<?php esc_html_e( 'List view', 'racketmanager' ); ?>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" onclick="Racketmanager.switchTab(this)" id="tab-grid" data-tabid="tab-grid">
-									<svg width="16" height="16" class="icon icon-grid">
-										<use xlink:href="<?php echo esc_url( RACKETMANAGER_URL . 'images/bootstrap-icons.svg#grid' ); ?>"></use>
-									</svg>
-									<?php esc_html_e( 'Grid view', 'racketmanager' ); ?>
-								</a>
-							</li>
-						</ul>
-					</div>
+                    <div class="d-none d-md-block module mt-3">
+                        <ul class="nav nav-pills justify-content-center">
+                            <li class="nav-item">
+                                <button class="nav-link active" onclick="Racketmanager.switchTab(this)" id="tab-list" data-tabid="tab-list">
+                                    <svg width="16" height="16" class="icon icon-list">
+                                        <use xlink:href="<?php echo esc_url( RACKETMANAGER_URL . 'images/bootstrap-icons.svg#list' ); ?>"></use>
+                                    </svg>
+                                    <?php esc_html_e( 'List view', 'racketmanager' ); ?>
+                                </button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link" onclick="Racketmanager.switchTab(this)" id="tab-grid" data-tabid="tab-grid">
+                                    <svg width="16" height="16" class="icon icon-grid">
+                                        <use xlink:href="<?php echo esc_url( RACKETMANAGER_URL . 'images/bootstrap-icons.svg#grid' ); ?>"></use>
+                                    </svg>
+                                    <?php esc_html_e( 'Grid view', 'racketmanager' ); ?>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>

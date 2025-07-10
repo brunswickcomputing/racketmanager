f844a628475e78036bbc2afdd1c0b5137bb669bb
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-10 19:33:18 +0100

    add missing ampersand

--- admin/includes/teams/team.php
+++ admin/includes/teams/team.php
@@ -47,7 +47,7 @@ namespace Racketmanager;
     } else {
         $action_form = 'admin.php?page=racketmanager-clubs&amp;view=teams';
         if ( $club_id ) {
-            $action_form .= 'club_id=' . $club_id;
+            $action_form .= '&amp;club_id=' . $club_id;
         }
     }
     ?>

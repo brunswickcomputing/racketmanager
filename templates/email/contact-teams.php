8916d083fcd0f1a3c60d0e041ba1a36a9b34689d
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-10 19:34:26 +0100

    Fix Replace tab character with spaces

--- templates/email/contact-teams.php
+++ templates/email/contact-teams.php
@@ -15,24 +15,24 @@ require 'email-header.php';
 $title_align = 'center';
 require 'components/title.php';
 if ( isset( $tournament ) ) {
-	$paragraph_text = __( 'Dear player', 'racketmanager' );
+    $paragraph_text = __( 'Dear player', 'racketmanager' );
 } else {
-	$paragraph_text = __( 'Dear captain', 'racketmanager' );
+    $paragraph_text = __( 'Dear captain', 'racketmanager' );
 }
 require 'components/paragraph.php';
 if ( $intro ) {
-	$paragraph_text = $intro;
-	require 'components/paragraph.php';
+    $paragraph_text = $intro;
+    require 'components/paragraph.php';
 }
 foreach ( $body as $i => $body_entry ) {
-	if ( $body_entry ) {
-		$paragraph_text = $body_entry;
-		require 'components/paragraph.php';
-	}
+    if ( $body_entry ) {
+        $paragraph_text = $body_entry;
+        require 'components/paragraph.php';
+    }
 }
 if ( $closing ) {
-	$paragraph_text = $closing;
-	require 'components/paragraph.php';
+    $paragraph_text = $closing;
+    require 'components/paragraph.php';
 }
 require 'components/closing.php';
 require 'email-footer.php';

d35c044bce02539c4561f69aa4cf0103d13dc59d
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-10 19:27:58 +0100

    Fix Replace tab character with spaces Replace "require" with "require_once"

--- templates/email/team-withdrawn.php
+++ templates/email/team-withdrawn.php
@@ -9,25 +9,25 @@ namespace Racketmanager;
 
 /** @var object $team */
 /** @var object $league */
-require 'email-header.php';
+require_once 'email-header.php';
 ?>
-			<?php
-			$paragraph_text = __( 'Dear Captain', 'racketmanager' );
-			require 'components/paragraph.php';
-			/* translators: %1$s: team name %2$s: league name */
-			$paragraph_text = sprintf( __( 'Unfortunately %1$s have withdrawn from the %2$s league.', 'racketmanager' ), $team->title, $league->title );
-			require 'components/paragraph.php';
-			/* translators: %s: team name */
-			$paragraph_text = sprintf( __( 'All unplayed matches involving %s have been cancelled.', 'racketmanager' ), $team->title );
-			require 'components/paragraph.php';
-			/* translators: %s: team name */
-			$paragraph_text = sprintf( __( 'All points from any completed match involving %s have been removed.', 'racketmanager' ), $team->title );
-			require 'components/paragraph.php';
-			if ( ! empty( $email_from ) ) {
-				$contact_email = $email_from;
-				require 'components/contact.php';
-			}
-			require 'components/closing.php';
-			?>
+            <?php
+            $paragraph_text = __( 'Dear Captain', 'racketmanager' );
+            require 'components/paragraph.php';
+            /* translators: %1$s: team name %2$s: league name */
+            $paragraph_text = sprintf( __( 'Unfortunately %1$s have withdrawn from the %2$s league.', 'racketmanager' ), $team->title, $league->title );
+            require 'components/paragraph.php';
+            /* translators: %s: team name */
+            $paragraph_text = sprintf( __( 'All unplayed matches involving %s have been cancelled.', 'racketmanager' ), $team->title );
+            require 'components/paragraph.php';
+            /* translators: %s: team name */
+            $paragraph_text = sprintf( __( 'All points from any completed match involving %s have been removed.', 'racketmanager' ), $team->title );
+            require 'components/paragraph.php';
+            if ( ! empty( $email_from ) ) {
+                $contact_email = $email_from;
+                require_once 'components/contact.php';
+            }
+            require_once 'components/closing.php';
+            ?>
 <?php
-require 'email-footer.php';
+require_once 'email-footer.php';

64d38fea21af5181dab620709698236e54f0d1d8
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-25 18:08:32 +0100

    Fix Define a constant instead of duplicating this literal "components/paragraph.php" 4 times.

--- templates/email/wtn-report.php
+++ templates/email/wtn-report.php
@@ -10,20 +10,22 @@ namespace Racketmanager;
 /** @var object $club */
 /** @var array $players */
 /** @var array $messages */
+/** @var string $paragraph */
 require 'email-header.php';
 $paragraph_text = __( 'A WTN update process has completed.', 'racketmanager' );
-require 'components/paragraph.php';
+/** @var TYPE_NAME $paragraph */
+require $paragraph;
 if ( $club ) {
     /* translators: %s: club name */
     $paragraph_text = sprintf( __( 'The process was run for %s.', 'racketmanager' ), $club->shortcode );
-    require 'components/paragraph.php';
+    require $paragraph;
 }
 /* translators: %s: number of players */
 $paragraph_text = sprintf( __( '%s players were processed.', 'racketmanager' ), count( $players ) );
-require 'components/paragraph.php';
+require $paragraph;
 /* translators: %s: number of errors */
 $paragraph_text = sprintf( __( 'There were %s errors.', 'racketmanager' ), count( $messages ) );
-require 'components/paragraph.php';
-require 'components/closing.php';
+require $paragraph;
+require $paragraph;
 ?>
 <?php require 'email-footer.php'; ?>

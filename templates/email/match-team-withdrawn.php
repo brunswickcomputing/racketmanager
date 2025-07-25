b7cacceeeab371c89c6116efcdf7d0cb1e00a56f
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-25 18:26:15 +0100

    Fix Define a constant instead of duplicating this literal "components/paragraph.php" 4 times.

--- templates/email/match-team-withdrawn.php
+++ templates/email/match-team-withdrawn.php
@@ -9,6 +9,10 @@ namespace Racketmanager;
 
 /** @var string $event */
 /** @var string $round */
+/** @var string $paragraph */
+/** @var string $contact */
+/** @var string $closing */
+/** @var string $title */
 require 'email-header.php';
 $title_text = sprintf(
 /* translators: %1$s: league name %2$s: round name */
@@ -18,31 +22,31 @@ $title_text = sprintf(
 );
 $title_level = '1';
 $title_align = 'center';
-require 'components/title.php';
+require $title;
 $title_align = '';
 $title_text = __( 'Match Update', 'racketmanager' );
 $title_level = '2';
 $title_align = 'center';
-require 'components/title.php';
+require $title;
 $title_align = '';
 if ( isset( $is_tournament ) ) {
     $paragraph_text = __( 'Dear player', 'racketmanager' );
 } else {
     $paragraph_text = __( 'Dear captain', 'racketmanager' );
 }
-require 'components/paragraph.php';
+require $paragraph;
 /* translators: %1$s: team name %2$s: league name */
 $paragraph_text = __( 'Unfortunately your opponent has withdrawn from the event.', 'racketmanager' );
-require 'components/paragraph.php';
+require $paragraph;
 /* translators: %s: team name */
 $paragraph_text = __( 'You have been advanced to the next round.', 'racketmanager' );
-require 'components/paragraph.php';
+require $paragraph;
 /* translators: %s: team name */
 $paragraph_text = __( 'You will receive a notification of your next match in due course.', 'racketmanager' );
-require 'components/paragraph.php';
+require $paragraph;
 if ( ! empty( $email_from ) ) {
     $contact_email = $email_from;
-    require 'components/contact.php';
+    require $contact;
 }
-require 'components/closing.php';
+require $closing;
 require 'email-footer.php';

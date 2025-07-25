56be4b38bb1639bdde812d1d6408084dcd2fee00
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-25 18:18:22 +0100

    Fix Define a constant instead of duplicating this literal "components/paragraph.php" 7 times.

--- templates/email/tournament-entry-open.php
+++ templates/email/tournament-entry-open.php
@@ -11,13 +11,17 @@ namespace Racketmanager;
 /** @var string $type */
 /** @var object $tournament */
 /** @var string $account_link */
+/** @var string $salutation */
+/** @var string $paragraph */
+/** @var string $contact */
+/** @var string $closing */
 require 'email-header.php';
 $salutation_link = $addressee;
-require 'components/salutation.php';
+require $salutation;
 if ( 'club' === $type ) {
     $paragraph_format = 'bold';
     $paragraph_text = __( 'Please pass this information on to your members.', 'racketmanager' );
-    require 'components/paragraph.php';
+    require $paragraph;
     $paragraph_format = null;
 }
 if ( empty( $days_remaining ) ) {
@@ -26,29 +30,29 @@ if ( empty( $days_remaining ) ) {
 } else {
     /* translators: %1$s: days remaining %2$s: competition name %3$s: closing date */
     $paragraph_text = sprintf( __( 'There are now less than %1$s days left before the %2$s Tournament closes on %3$s.', 'racketmanager' ), $days_remaining, ucfirst( $tournament->name ), $tournament->date_closing_display );
-    require 'components/paragraph.php';
+    require $paragraph;
     $paragraph_text = __( 'You have played in a previous tournament but you have not yet entered this one.', 'racketmanager' );
 }
-require 'components/paragraph.php';
+require $paragraph;
 if ( ! empty( $tournament->date_start_display ) && ! empty( $tournament->date_display ) ) {
     /* translators: %1$s: start date %2$s: end date */
     $paragraph_text = sprintf( __( 'The tournament will run from %1$s to %2$s.', 'racketmanager' ), $tournament->date_start_display, $tournament->date_display );
-    require 'components/paragraph.php';
+    require $paragraph;
 }
 if ( ! empty( $tournament->venue_name ) ) {
     /* translators: %s Venue */
     $paragraph_text = sprintf( __( 'Finals day will held at %s.', 'racketmanager' ), $tournament->venue_name );
-    require 'components/paragraph.php';
+    require $paragraph;
 }
 $paragraph_text = __( 'Click the button below to take you directly to the entry form.', 'racketmanager' );
-require 'components/paragraph.php';
+require $paragraph;
 $action_link_text = __( 'Entry Form', 'racketmanager' );
 require 'components/action-link.php';
 if ( ! empty( $from_email ) ) {
     $contact_email = $from_email;
-    require 'components/contact.php';
+    require $contact;
 }
-require 'components/closing.php';
+require $closing;
 switch ( $type ) {
     case 'player':
     case 'reminder':
@@ -59,7 +63,7 @@ switch ( $type ) {
         );
         $paragraph_format = 'italic-small';
         $paragraph_imbed  = true;
-        require 'components/paragraph.php';
+        require $paragraph;
         $paragraph_imbed  = false;
         $paragraph_format = null;
         break;

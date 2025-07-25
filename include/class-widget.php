b5c47bc979afd23fb22591e3713737c8d091b5a4
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-25 17:26:40 +0100

    Fix Merge this if statement with the enclosing one. Refactor this function to reduce its Cognitive Complexity from 26 to the 15 allowed.

--- include/class-widget.php
+++ include/class-widget.php
@@ -9,6 +9,7 @@
 
 namespace Racketmanager;
 
+use stdClass;
 use WP_Widget;
 
 /**
@@ -54,30 +55,55 @@ class Widget extends WP_Widget {
             echo esc_html( $cache[ $args['widget_id'] ] );
             return;
         }
-
-        ob_start();
         $before_widget = $args['before_widget'];
         $before_title  = $args['before_title'];
         $after_title   = $args['after_title'];
-        wp_enqueue_script( 'racketmanager_widget_js', plugins_url( '/js/widget.js', __DIR__ ), array(), RACKETMANAGER_VERSION, array( 'in_footer' => true ) );
-
-        $title     = apply_filters( 'widget_title', $instance['title'] );
-        $club_name = isset( $instance['club_name'] ) ? esc_html( $instance['club_name'] ) : '';
-        $club_link = isset( $instance['club_link'] ) ? esc_html( $instance['club_link'] ) : '';
-        $num_items = empty( $instance['num_items'] ) ? 999999 : intval( $instance['num_items'] );
-        $club_type = $instance['club_type'];
-        $orderby   = $instance['orderby'];
-
-        $clubs = $racketmanager->get_clubs(
+        $after_widget  = $args['after_widget'];
+        $title         = apply_filters( 'widget_title', $instance['title'] );
+        $club_name     = isset( $instance['club_name'] ) ? esc_html( $instance['club_name'] ) : '';
+        $club_link     = isset( $instance['club_link'] ) ? esc_html( $instance['club_link'] ) : '';
+        $num_items     = empty( $instance['num_items'] ) ? 999999 : intval( $instance['num_items'] );
+        $club_type     = $instance['club_type'];
+        $orderby       = $instance['orderby'];
+        $clubs         = $racketmanager->get_clubs(
             array(
                 'type'    => $club_type,
                 'limit'   => $num_items,
                 'orderby' => $orderby,
             )
         );
+        $data = new stdClass();
+        $data->clubs         = $clubs;
+        $data->club_name     = $club_name;
+        $data->club_link     = $club_link;
+        $data->title         = $title;
+        $data->before_widget = $before_widget;
+        $data->before_title  = $before_title;
+        $data->after_title   = $after_title;
+        $data->after_widget  = $after_widget;
+        $data->widget_id     = $args['widget_id'];
+        $this->show_widget( $data );
+    }
 
+    /**
+     * Function to output widget data
+     *
+     * @param object $data
+     *
+     * @return void
+     */
+    private function show_widget( object $data ): void {
+        $clubs         = $data->clubs;
+        $club_name     = $data->club_name;
+        $club_link     = $data->club_link;
+        $title         = $data->title;
+        $before_widget = $data->before_widget;
+        $before_title  = $data->before_title;
+        $after_title   = $data->after_title;
+        $after_widget  = $data->after_widget;
+        ob_start();
+        wp_enqueue_script( 'racketmanager_widget_js', plugins_url( '/js/widget.js', __DIR__ ), array(), RACKETMANAGER_VERSION, array( 'in_footer' => true ) );
         echo esc_html( $before_widget );
-
         if ( $clubs ) {
             ?>
             <?php
@@ -91,18 +117,18 @@ class Widget extends WP_Widget {
                     ?>
                     <div class="club-item">
                         <div class="club-content">
-                            <?php if ( '1' === $club_name ) { ?>
+                            <?php
+                            if ( '1' === $club_name ) {
+                                ?>
                                 <div class="">
                                     <h4 class=""><a href="<?php echo esc_html( '/clubs/' . sanitize_title( $club->shortcode ) . '/' ); ?>"><?php echo esc_html( $club->name ); ?></a></h4>
                                     <p><?php echo esc_html( $club->facilities ); ?></p>
                                     <p><?php echo esc_html( $club->address ); ?></p>
                                     <?php
-                                    if ( '1' === $club_link ) {
-                                        if ( null !== $club->website ) {
-                                            ?>
-                                            <p><a href="<?php echo esc_url( $club->website ); ?>" target="_blank"><?php echo esc_html( $club->website ); ?></a></p>
-                                            <?php
-                                        }
+                                    if ( '1' === $club_link && null !== $club->website ) {
+                                        ?>
+                                        <p><a href="<?php echo esc_url( $club->website ); ?>" target="_blank"><?php echo esc_html( $club->website ); ?></a></p>
+                                        <?php
                                     }
                                     ?>
                                 </div>
@@ -121,15 +147,14 @@ class Widget extends WP_Widget {
             <?php
         }
         wp_reset_postdata();
-        echo esc_html( $args['after_widget'] );
-        if ( ! $this->is_preview() ) {
-            $cache[ $args['widget_id'] ] = ob_get_flush();
-            wp_cache_set( 'racketmanager', $cache, 'widget' );
-        } else {
+        echo esc_html( $after_widget );
+        if ( $this->is_preview() ) {
             ob_end_flush();
+        } else {
+            $cache[ $data->widget_id ] = ob_get_flush();
+            wp_cache_set( 'racketmanager', $cache, 'widget' );
         }
     }
-
     /**
      * Save settings
      *
@@ -151,7 +176,7 @@ class Widget extends WP_Widget {
         $club_name = isset( $instance['club_name'] ) ? esc_attr( $instance['club_name'] ) : '';
         $club_link = isset( $instance['club_link'] ) ? esc_attr( $instance['club_link'] ) : '';
         $orderby   = isset( $instance['orderby'] ) ? esc_attr( $instance['orderby'] ) : '';
-        $num_items = isset( $instance['num_items '] ) ? esc_attr( $instance['num_items '] ) : -1;
+        $num_items = isset( $instance['num_items'] ) ? esc_attr( $instance['num_items'] ) : -1;
         $club_type = isset( $instance['club_type'] ) ? esc_attr( $instance['club_type'] ) : '';
         ?>
         <div>
@@ -169,8 +194,8 @@ class Widget extends WP_Widget {
             </select>
         </div>
         <div>
-            <label for="<?php echo esc_html( $this->get_field_id( 'num_items ' ) ); ?>"><?php esc_html_e( 'How many to show?', 'racketmanager' ); ?></label>
-            <input id="<?php echo esc_html( $this->get_field_id( 'num_items ' ) ); ?>" name="<?php echo esc_html( $this->get_field_name( 'num_items ' ) ); ?>" type="text" value="<?php echo esc_html( $num_items  ); ?>" style="float:right; width:56%;" />
+            <label for="<?php echo esc_html( $this->get_field_id( 'num_items' ) ); ?>"><?php esc_html_e( 'How many to show?', 'racketmanager' ); ?></label>
+            <input id="<?php echo esc_html( $this->get_field_id( 'num_items' ) ); ?>" name="<?php echo esc_html( $this->get_field_name( 'num_items' ) ); ?>" type="text" value="<?php echo esc_html( $num_items ); ?>" style="float:right; width:56%;" />
         </div>
         <div>
             <label for="<?php echo esc_html( $this->get_field_id( 'club_name' ) ); ?>"><?php esc_html_e( 'Show club name?', 'racketmanager' ); ?></label>

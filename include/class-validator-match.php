58549f1434ea5d60fd8d7aaee9ec8bafa30375f5
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-09-09 14:31:47 +0100

    tidy code

--- include/class-validator-match.php
+++ include/class-validator-match.php
@@ -285,7 +285,7 @@ final class Validator_Match extends Validator {
                         $set_status = $match_status;
                         break;
                 }
-                $set = $this->validate_set( $set, $set_prefix, $set_info, $set_status );
+                $set           = $this->validate_set( $set, $set_prefix, $set_info, $set_status );
                 $set_player_1  = is_null( $set['player1'] ) ? null : strtoupper( $set['player1'] );
                 $set_player_2  = is_null( $set['player2'] ) ? null : strtoupper( $set['player2'] );
                 $set_completed = $set['completed'];

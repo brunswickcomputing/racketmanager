e61a6c2dbc560dceb0b379a8e9c667a084e99328
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-09-15 16:23:19 +0100

    use table for layout

--- admin/event/matches.php
+++ admin/event/matches.php
@@ -7,51 +7,45 @@
 
 namespace Racketmanager;
 
-/** @var object $event */
+/** @var array $matches */
 ?>
 <div class="container">
-    <div class="row table-header">
-        <div class="d-none d-lg-block col-1"><?php esc_html_e( 'Match day', 'racketmanager' ); ?></div>
-        <div class="col-4 col-lg-2"><?php esc_html_e( 'Match date', 'racketmanager' ); ?></div>
-        <div class="col-8 col-lg-5"><?php esc_html_e( 'Match', 'racketmanager' ); ?></div>
-        <div class="d-none d-lg-block col-3"><?php esc_html_e( 'League', 'racketmanager' ); ?></div>
-    </div>
-
+    <table class="table table-striped">
+        <thead class="table-dark">
+        <tr>
+            <th><?php esc_html_e( 'Round', 'racketmanager' ); ?></th>
+            <th><?php esc_html_e( 'Date', 'racketmanager' ); ?></th>
+            <th><?php esc_html_e( 'Match', 'racketmanager' ); ?></th>
+            <th><?php esc_html_e( 'League', 'racketmanager' ); ?></th>
+        </tr>
     <?php
-    if ( $event->get_season_event() ) {
-        $matches = $this->get_matches(
-            array(
-                'event_id' => $event->id,
-                'season'   => $event->get_season_event()['name'],
-                'orderby'  => array(
-                    'match_day' => 'ASC',
-                    'date'      => 'ASC',
-                    'league_id' => 'ASC',
-                    'home_team' => 'ASC',
-                ),
-            )
-        );
-        if ( $matches ) {
-            $match_day = '';
-            foreach ( $matches as $match ) {
-                if ( $match->match_day !== $match_day ) {
-                    $match_day = $match->match_day;
-                    ?>
-                    <div class="row table-row">
-                        <div class="col-12"><?php echo esc_html_e( 'Match Day', 'racketmanager' ) . ' ' . esc_html( $match_day ); ?></div>
-                    </div>
-                    <?php
-                }
+    if ( $matches ) {
+        ?>
+        <tbody class="table table-striped">
+        <?php
+        $match_day = '';
+        foreach ( $matches as $match ) {
+            if ( $match->match_day !== $match_day ) {
+                $match_day = $match->match_day;
                 ?>
-                <div class="row table-row">
-                    <div class="d-none d-lg-block col-1"></div>
-                    <div class="col-4 col-lg-2"><?php echo esc_html( $match->date ); ?></div>
-                    <div class="col-8 col-lg-5"><?php echo esc_html( $match->match_title ); ?></div>
-                    <div class="d-none d-lg-block col-3"><?php echo esc_html( $match->league->title ); ?></div>
-                </div>
+                <tr>
+                    <td colspan="4"><?php echo esc_html_e( 'Match Day', 'racketmanager' ) . ' ' . esc_html( $match_day ); ?></td>
+                </tr>
                 <?php
             }
+            ?>
+            <tr>
+                <td></td>
+                <td><?php echo esc_html( $match->date ); ?></td>
+                <td><?php echo esc_html( $match->match_title ); ?></td>
+                <td><?php echo esc_html( $match->league->title ); ?></td>
+            </tr>
+            <?php
         }
+        ?>
+        </tbody>
+        <?php
     }
     ?>
+    </table>
 </div>

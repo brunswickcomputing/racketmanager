6c9ba006c35b0a2a7e09ef5b1d6c9da7ab974221
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-10-30 11:28:10 +0000

    add missing util namespace use

--- include/admin/class-admin-club.php
+++ include/admin/class-admin-club.php
@@ -10,6 +10,7 @@
 namespace Racketmanager\admin;
 
 use Racketmanager\models\Club;
+use Racketmanager\util\Util_Lookup;
 use Racketmanager\validator\Validator;
 use Racketmanager\validator\Validator_Club;
 use stdClass;

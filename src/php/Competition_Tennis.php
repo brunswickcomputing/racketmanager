e316409892bd4a33c307e8105de0bdc7969b6640
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-10-31 10:20:18 +0000

    delete redundant class shims

--- src/php/Competition_Tennis.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * PSR-4 bridge for Racketmanager\\Competition_Tennis
- * Loads the implementation from src/php/sports/Competition_Tennis.php.
- */
-
-require_once RACKETMANAGER_PATH . 'src/php/sports/Competition_Tennis.php';
--- src/php/League_Tennis.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * PSR-4 bridge for Racketmanager\\League_Tennis
- * Loads the implementation from src/php/sports/League_Tennis.php.
- */
-
-require_once RACKETMANAGER_PATH . 'src/php/sports/League_Tennis.php';

6df9cdf75ae7ace0b98a8f185f7bc815502b75f4
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-11-30 21:46:22 +0000

    add nonce name change field names

--- templates/admin/club/show-club-players.php
+++ templates/admin/club/show-club-players.php
@@ -48,7 +48,7 @@ use Racketmanager\Util\Util_Lookup;
             <button class="btn btn-secondary"><?php esc_html_e( 'Filter', 'racketmanager' ); ?></button>
         </form>
         <form id="players-action" method="post" action="" class="form-control">
-            <?php wp_nonce_field( 'club-players-bulk' ); ?>
+            <?php wp_nonce_field( 'racketmanager_club-players-bulk', 'racketmanager_nonce' ); ?>
             <div class="row gx-3 mb-3 align-items-center">
                 <!-- Bulk Actions -->
                 <div class="col-auto">
@@ -84,20 +84,20 @@ use Racketmanager\Util\Util_Lookup;
                         <tr class="">
                             <td class="col-1 col-md-1 check-column">
                                 <?php
-                                if ( ! isset( $player->removed_date ) ) {
+                                if ( ! isset( $player->removal_date ) ) {
                                     ?>
-                                    <label for="clubPlayer-<?php echo esc_html( $player->roster_id ); ?>" class="visually-hidden"><?php esc_html_e( 'Check', 'racketmanager' ); ?></label><input type="checkbox" value="<?php echo esc_html( $player->roster_id ); ?>" name="clubPlayer[<?php echo esc_html( $player->roster_id ); ?>]" id="clubPlayer-<?php echo esc_html( $player->roster_id ); ?>" />
+                                    <label for="clubPlayer-<?php echo esc_html( $player->registration_id ); ?>" class="visually-hidden"><?php esc_html_e( 'Check', 'racketmanager' ); ?></label><input type="checkbox" value="<?php echo esc_html( $player->registration_id ); ?>" name="clubPlayer[<?php echo esc_html( $player->registration_id ); ?>]" id="clubPlayer-<?php echo esc_html( $player->registration_id ); ?>" />
                                     <?php
                                 }
                                 ?>
                             </td>
                             <td class="col-4 col-md-2">
                                 <?php
-                                if ( ! isset( $player->removed_date ) ) {
-                                    echo '<a href="/wp-admin/admin.php?page=racketmanager-clubs&amp;view=player&amp;club_id=' . esc_html( $club->id ) . '&amp;player_id=' . esc_html( $player->player_id ) . '">';
+                                if ( ! isset( $player->removal_date ) ) {
+                                    echo '<a href="/wp-admin/admin.php?page=racketmanager-clubs&amp;view=player&amp;club_id=' . esc_html( $club->id ) . '&amp;player_id=' . esc_html( $player->user_id ) . '">';
                                 }
-                                echo esc_html( $player->fullname );
-                                if ( ! isset( $player->removed_date ) ) {
+                                echo esc_html( $player->display_name );
+                                if ( ! isset( $player->removal_date ) ) {
                                     echo '</a>';
                                 }
                                 ?>
@@ -105,7 +105,7 @@ use Racketmanager\Util\Util_Lookup;
                             <td class="col-4 col-md-2">
                                 <?php
                                 $match_types    = Util_Lookup::get_match_types();
-                                $wtn            = $player->wtn;
+                                $wtn            = $player->wtn ?? array();
                                 $wtn_display = '';
                                 foreach ( $match_types as $match_type => $description ) {
                                     if ( ! empty( $wtn_display ) ) {

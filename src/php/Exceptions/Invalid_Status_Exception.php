96abf29bcfe2c3ade3cb74185d8482f0f5775524
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-12-04 18:44:20 +0000

    add new exceptions

---
+++ src/php/Exceptions/Invalid_Status_Exception.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Racketmanager\Exceptions;
+
+use RuntimeException;
+
+class Invalid_Status_Exception extends RuntimeException {
+
+}
---
+++ src/php/Exceptions/Role_Assignment_Not_Found_Exception.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Racketmanager\Exceptions;
+
+use RuntimeException;
+
+class Role_Assignment_Not_Found_Exception extends RuntimeException {
+
+}

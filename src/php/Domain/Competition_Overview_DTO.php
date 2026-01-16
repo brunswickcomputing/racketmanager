c65dc5a1c2e42535c60847bc42b2a82d749c397c
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2026-01-16 12:22:12 +0000

    add new competition overview DTO

---
+++ src/php/Domain/Competition_Overview_DTO.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Competition_Overview_DTO API: Competition_Overview_DTO class
+ *
+ * @author Paul Moffat
+ * @package RacketManager
+ * @subpackage Domain
+ */
+
+namespace Racketmanager\Domain;
+
+/**
+ * Class to implement the Competition Overview Data Transfer Object
+ */
+class Competition_Overview_DTO {
+    public int $id;
+    public string $name;
+    public int $num_events;
+    public int $num_teams;
+    public int $num_clubs;
+    public int $num_players;
+    public ?string $competition_code;
+    public ?string $grade;
+
+    public function __construct( $data ) {
+        $settings               = json_decode( $data->settings, true );
+        $this->id               = (int) $data->id;
+        $this->name             = $data->name;
+        $this->competition_code = $settings['competition_code'] ?? null;
+        $this->grade            = $settings['grade'] ?? null;
+        $this->num_events       = (int) $data->num_events;
+        $this->num_teams        = (int) $data->num_teams;
+        $this->num_clubs        = (int) $data->num_clubs;
+        $this->num_players      = (int) $data->num_players;
+    }
+
+}

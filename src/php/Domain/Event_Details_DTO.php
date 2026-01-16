8c8ae1989bdb59a04e034b17a55f017325c8fb52
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2026-01-16 12:22:58 +0000

    add new event details DTO

---
+++ src/php/Domain/Event_Details_DTO.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Event_Details_DTO API: Event_Details_DTO class
+ *
+ * @author Paul Moffat
+ * @package RacketManager
+ * @subpackage Domain
+ */
+
+namespace Racketmanager\Domain;
+
+/**
+ * Class to implement the Event Details Data Transfer Object
+ */
+class Event_Details_DTO {
+    public int $event_id;
+    public string $event_name;
+    public string $format;
+    public int $num_leagues;
+    public int $num_teams;
+    public int $num_clubs;
+    public int $num_players;
+    public mixed $age_limit;
+    public mixed $age_offset;
+
+    public function __construct( $data ) {
+        $settings          = maybe_unserialize( $data->settings );
+        $this->event_id    = (int) $data->event_id;
+        $this->event_name  = $data->event_name;
+        $this->format      = $data->format;
+        $this->age_limit   = $settings['age_limit'] ?? null;
+        $this->age_offset  = $settings['age_offset'] ?? null;
+        $this->num_leagues = (int) $data->num_leagues;
+        $this->num_teams   = (int) $data->num_teams;
+        $this->num_clubs   = (int) $data->num_clubs;
+        $this->num_players = (int) $data->num_players;
+    }
+
+}
---
+++ src/php/Exceptions/Clubs_Not_Found_Exception.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Racketmanager\Exceptions;
+
+class Clubs_Not_Found_Exception extends Plugin_Exception {
+
+}

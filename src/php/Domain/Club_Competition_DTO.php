300276a81424af4ec4a184e960722bce0ee60b96
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2026-01-16 12:21:11 +0000

    add new club competition DTO

---
+++ src/php/Domain/Club_Competition_DTO.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Club_Competition_DTO API: Club_Competition_DTO class
+ *
+ * @author Paul Moffat
+ * @package RacketManager
+ * @subpackage Domain
+ */
+
+namespace Racketmanager\Domain;
+
+/**
+ * Class to implement the Club Competition Data Transfer Object
+ */
+class Club_Competition_DTO {
+    public int $id;
+    public string $name;
+    public string $shortcode;
+    public int $num_teams;
+    public int $num_players;
+
+    public function __construct( $data ) {
+        $this->id          = (int) $data->club_id;
+        $this->name        = $data->club_name;
+        $this->shortcode   = $data->club_shortcode;
+        $this->num_teams   = (int) $data->num_teams;
+        $this->num_players = (int) $data->num_players;
+    }
+
+}

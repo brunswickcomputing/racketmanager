219174320860b4cbdf67aee68f64735403b44897
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2026-01-16 12:23:47 +0000

    add new players list DTO

---
+++ src/php/Domain/Players_List_DTO.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Players_List_DTO API: Players_List_DTO class
+ *
+ * @author Paul Moffat
+ * @package RacketManager
+ * @subpackage Domain
+ */
+
+namespace Racketmanager\Domain;
+
+/**
+ * Class to implement the Players List Data Transfer Object
+ */
+class Players_List_DTO {
+    public int $id;
+    public string $firstname;
+    public string $surname;
+    public string $display_name;
+    public string $index;
+
+    public function __construct( $data ) {
+        $this->id           = (int) $data->playerId;
+        $this->display_name = $data->display_name;
+        $this->firstname    = $data->firstName ? : '';
+        $this->surname      = $data->surname ? : '';
+    }
+
+}

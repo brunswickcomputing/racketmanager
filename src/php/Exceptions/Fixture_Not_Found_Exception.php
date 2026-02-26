eb4c03d314b7cb8ffa5e584c97180f8c501c4738
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2026-02-26 18:15:55 +0000

    add new exceptions for tournament and fixture not found

---
+++ src/php/Exceptions/Fixture_Not_Found_Exception.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Racketmanager\Exceptions;
+
+class Fixture_Not_Found_Exception extends Plugin_Exception {
+
+}
---
+++ src/php/Exceptions/Tournament_Entry_Not_Found_Exception.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Racketmanager\Exceptions;
+
+class Tournament_Entry_Not_Found_Exception extends Plugin_Exception {
+
+}

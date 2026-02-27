648d9202ca071529cf7acfc91399b71bfd99fc5c
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2026-02-27 16:33:36 +0000

    match dates property is passed in separately

--- templates/includes/tournament-date-selection.php
+++ templates/includes/tournament-date-selection.php
@@ -17,7 +17,7 @@ namespace Racketmanager;
             <input type="hidden" name="tournament_id" id="tournament_id" value="<?php echo esc_html( $tournament->name ); ?>" />
             <label class="visually-hidden" for="match_date"><?php esc_html__( 'Date', 'racketmanager') ?></label><select class="form-select" name="match_date" id="match_date">
                 <?php
-                foreach ( $tournament->match_dates as $match_date ) {
+                foreach ( $match_dates as $match_date ) {
                     ?>
                     <option value="<?php echo esc_html( $match_date ); ?>" <?php selected( $current_match_date, $match_date ); ?>>
                         <?php echo esc_html( mysql2date( 'D j M', $match_date ) ); ?>

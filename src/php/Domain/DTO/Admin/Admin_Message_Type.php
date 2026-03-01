7faa93f14a8de4a3a228dcce6c258cc56d3bd811
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2026-03-01 14:03:21 +0000

    Add DTOs (enum + action result + draw request)

---
+++ src/php/Domain/DTO/Admin/Admin_Message_Type.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin message types
+ *
+ * @package RacketManager
+ * @subpackage Domain/DTO/Admin
+ */
+
+namespace Racketmanager\Domain\DTO\Admin;
+
+enum Admin_Message_Type: string {
+    case SUCCESS = 'success';
+    case WARNING = 'warning';
+    case ERROR   = 'error';
